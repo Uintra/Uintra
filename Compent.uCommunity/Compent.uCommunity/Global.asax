752f52112a77878c9dd0014f92a3c0f94afd082d
Denis Prodan <dpr@compent.net>
2017-02-03 13:10:38 +0200

    add Compent.uCommunity web application to repository

---
+++ Compent.uCommunity/Compent.uCommunity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Compent.uCommunity.MvcApplication" Language="C#" %>
---
+++ Compent.uCommunity/Compent.uCommunity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Compent.uCommunity.Startup))]
+namespace Compent.uCommunity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
