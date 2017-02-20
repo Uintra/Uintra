15c6bae24596737b2a657241821d3e2c85297bf6
Maksim Didinov <mdi@compent.net>
2017-02-20 16:06:51 +0200

    Added assembly info files

---
+++ src/GlobalAssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyProduct("Compent.uCommunity")]
+
+[assembly: AssemblyCompany("compent.dk")]
+[assembly: AssemblyCopyright("Copyright © compent.dk 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+ 
+
+[assembly: AssemblyVersion("1.0.1.0")]
+//Change MAJOR version before release
+//Change MINOR version when you add functionality in a backwards-compatible manner
+//Change PATCH version when you make backwards-compatible bugfixes
--- src/uCommunity.BaseControls/Properties/AssemblyInfo.cs
+++ src/uCommunity.BaseControls/Properties/AssemblyInfo.cs
@@ -1,17 +1,9 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("BaseControls")]
+[assembly: AssemblyTitle("uCommunity.BaseControls")]
 [assembly: AssemblyDescription("Base activity package.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("compent.dk")]
-[assembly: AssemblyProduct("BaseActivity")]
-[assembly: AssemblyCopyright("Copyright © compent.dk 2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("2e4a8b55-37f6-46a8-b108-30e346870720")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
