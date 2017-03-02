3ffcc8450002f1c07feb8a666c2af766c00244bc
Maksim Didinov <mdi@compent.net>
2017-03-02 11:43:52 +0200

    Core version upd

--- src/uCommunity.Core/Properties/AssemblyInfo.cs
+++ src/uCommunity.Core/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uCommunity.Core")]
 [assembly: AssemblyDescription("Core activity package.")]
 
-[assembly: AssemblyVersion("1.0.2.1")]
+[assembly: AssemblyVersion("1.0.2.2")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3f8c785a-48d0-41f5-9257-6f73afca41eb")]
 
