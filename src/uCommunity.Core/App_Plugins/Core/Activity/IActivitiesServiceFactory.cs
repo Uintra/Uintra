f3ac22bdac8a67a959507aada778718407bd936b
Maksim Didinov <mdi@compent.net>
2017-03-02 12:16:18 +0200

    Merge fix

---
+++ src/uCommunity.Core/App_Plugins/Core/Activity/IActivitiesServiceFactory.cs
@@ -0,0 +1,7 @@
+﻿﻿namespace uCommunity.Core.App_Plugins.Core.Activity
+{
+    public interface IActivitiesServiceFactory
+    {
+        IIntranetActivityItemServiceBase GetService(IntranetActivityTypeEnum type);
+    }
+}
