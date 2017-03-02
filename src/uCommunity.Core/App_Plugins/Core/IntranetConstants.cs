8a05ff7672de2c7db847bdd3536e5282764eb177
Dmitry Bondar <dbo@compent.net>
2017-03-02 11:39:10 +0200

    Add date common constants.

--- src/uCommunity.Core/App_Plugins/Core/IntranetConstants.cs
+++ src/uCommunity.Core/App_Plugins/Core/IntranetConstants.cs
@@ -2,5 +2,16 @@
 {
     public static class IntranetConstants
     {
+        public static class Common
+        {
+#warning this two formats related
+            public const string DefaultTimeFormat = "HH:mm";
+            public const string DefaultDateFormat = "dd.MM.yyyy";
+            public const string DefaultDateTimeFormat = "dd.MM.yyyy HH:mm";
+            public const string DefaultDateTimeValuePickerFormat = "yyyy-MM-ddTHH:mm";
+            public const string DefaultDatePickerFormat = "d.m.Y";
+            public const string DefaultDateTimePickerFormat = "d.m.Y H:i";
+#warning this two formats related
+        }
     }
 }
