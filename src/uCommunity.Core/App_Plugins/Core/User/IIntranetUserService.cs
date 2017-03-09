ab4155d80e09d770ac164525624a31132b13f592
Sergey Shepel <sse@compent.net>
2017-03-09 15:40:14 +0200

    Refactor IIntranetUserService

--- src/uCommunity.Core/App_Plugins/Core/User/IIntranetUserService.cs
+++ src/uCommunity.Core/App_Plugins/Core/User/IIntranetUserService.cs
@@ -5,17 +5,16 @@ namespace uCommunity.Core.App_Plugins.Core.User
 {
     public interface IIntranetUserService
     {
-        IEnumerable<string> GetFullNamesByIds(IEnumerable<Guid> ids);
-        IEnumerable<ListItemModel<Guid>> GetByIds(IEnumerable<Guid> ids);
         Guid GetCurrentUserId();
     }
 
     public interface IIntranetUserService<TModel> : IIntranetUserService
         where TModel : IntranetUserBase
     {
-        IEnumerable<TModel> GetAll();
-        TModel GetActivityUser(int umbracoId);
+        TModel Get(int umbracoId);
         TModel GetCurrentUser();
+        IEnumerable<TModel> GetMany(IEnumerable<Guid> ids);
+        IEnumerable<TModel> GetAll();
         void FillCreator(IHasCreator<TModel> model);
     }
 }
