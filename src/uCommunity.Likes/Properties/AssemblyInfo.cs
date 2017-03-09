02c91a6f54fbcd271e81c276682ed5017021d3ab
Maksim Didinov <mdi@compent.net>
2017-03-09 16:53:32 +0200

    Likes. Remove TD ApiClient dependency

--- src/uCommunity.Likes/Properties/AssemblyInfo.cs
+++ src/uCommunity.Likes/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uCommunity.Likes")]
 [assembly: AssemblyDescription("Likes activity package.")]
 
-[assembly: AssemblyVersion("1.0.2.3")]
+[assembly: AssemblyVersion("1.0.2.4")]
 [assembly: ComVisible(false)]
 [assembly: Guid("b99f0f05-598c-4f4a-a972-7282dcf9fed8")]
