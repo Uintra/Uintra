eb13c5e8f855208230ecfd6b5f865d18cd005ba9
Maksim Didinov <mdi@compent.net>
2017-03-13 17:38:57 +0200

    CentralFeed init

---
+++ src/uCommunity.CentralFeed/Core/Models/CentralFeedOverviewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace uCommunity.CentralFeed.Models
+{
+    public class CentralFeedOverviewModel
+    {
+        public IEnumerable<CentralFeedTypeModel> Types { get; set; }
+        public CentralFeedOverviewModel()
+        {
+            Types = Enumerable.Empty<CentralFeedTypeModel>();
+        }
+    }
+}
