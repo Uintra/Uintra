0984d63cb1c53d77af5585e01756ca517a3192ce
Dmitry Bondar <dbo@compent.net>
2017-03-14 15:32:50 +0200

    Add GetCurrentUser method to HttpContextExtentions.

--- src/uCommunity.Core/App_Plugins/Core/Extentions/HttpContextExtentions.cs
+++ src/uCommunity.Core/App_Plugins/Core/Extentions/HttpContextExtentions.cs
@@ -1,5 +1,7 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Mvc;
+using uCommunity.Core.User;
 
 namespace uCommunity.Core.Extentions
 {
@@ -23,6 +25,18 @@ namespace uCommunity.Core.Extentions
         public static string GetBackLink(this HttpContextBase context, string defaultLink)
         {
             return context.Request.UrlReferrer?.AbsoluteUri ?? defaultLink;
+        }
+
+        public static IntranetUser GetCurrentUser(this HttpContext context)
+        {
+            var currentUser = context.Session?[IntranetConstants.Session.LoggedUserSessionKey] as IntranetUser;
+
+            if (currentUser == null)
+            {
+                throw new Exception("Can't get current user from session!");
+            }
+
+            return currentUser;
         }
     }
 }
