21e969ea93b92b9f50d70be0b0d1c960d468241c
Dmitry Bondar <dbo@compent.net>
2017-03-14 11:34:10 +0200

    Add XPathHelper and IntranetUser model.

---
+++ src/uCommunity.Core/App_Plugins/Core/XPathHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace uCommunity.Core
+{
+    public static class XPathHelper
+    {
+        public static string GetXpath(params string[] documentTypesAliases)
+        {
+            var result = "root/";
+            foreach (var alias in documentTypesAliases)
+            {
+                result += $"{alias}[@isDoc]/";
+            }
+
+            return result.TrimEnd('/');
+        }
+    }
+}
