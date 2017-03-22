198e06f9684051efbc658c4f937466e0aec5cfad
Denys Dzhelomanov <dje@compent.net>
2017-03-22 16:43:07 +0200

    Install navigation uPackage to uCommunity test project

---
+++ src/Compent.uCommunity/App_Plugins/Navigation/Dashboard/navigation-controller.js
@@ -0,0 +1,39 @@
+﻿(function (angular) {
+    'use strict';
+
+    var uNavigationController = function ($scope, uNavigationService) {
+        var self = this;
+
+        self.isLoaded = false;
+        self.isDocumentTypesAlreadyExists = undefined;
+        self.parentIdOrAlias = "";
+
+        self.create = function () {
+            uNavigationService.createNavigationCompositions({ parentIdOrAlias: self.parentIdOrAlias }).then(function (response) {
+                self.isDocumentTypesAlreadyExists = response.data.isDocumentTypesAlreadyExists;
+
+                if (response.data.isUnknownParent) {
+                    alert("Unknown parent id or alias");
+                }
+            });
+        }
+
+        self.delete = function () {
+            uNavigationService.deleteNavigationCompositions().then(function (response) {
+                self.isDocumentTypesAlreadyExists = response.data.isDocumentTypesAlreadyExists;
+            });
+        }
+
+        function init() {
+            uNavigationService.getInitialState().then(function (response) {
+                self.isLoaded = true;
+                self.isDocumentTypesAlreadyExists = response.data.isDocumentTypesAlreadyExists;
+            });
+        }
+
+        init();
+    }
+
+    uNavigationController.$inject = ["$scope", "uNavigationService"];
+    angular.module('umbraco').controller('uNavigationController', uNavigationController);
+})(angular);
---
+++ src/Compent.uCommunity/App_Plugins/Navigation/NavigationAuthorizationController.cs
@@ -0,0 +1,52 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+using uCommunity.Core.User;
+using Umbraco.Core.Services;
+using Umbraco.Web.Mvc;
+
+namespace uCommunity.Navigation.Plugin
+{
+    public class NavigationAuthorizationController : SurfaceController
+    {
+        private readonly IIntranetUserService<IntranetUserBase> _intranetUserService;
+        private readonly IUserService _userService;
+
+        public NavigationAuthorizationController(
+            IIntranetUserService<IntranetUserBase> intranetUserService,
+            IUserService userService)
+        {
+            _intranetUserService = intranetUserService;
+            _userService = userService;
+        }
+
+        public ActionResult LoginToUmbraco()
+        {
+            var currentUser = _intranetUserService.GetCurrentUser();
+            if (!currentUser.UmbracoId.HasValue)
+            {
+                return Redirect("/");
+            }
+
+            var umbracoUser = _userService.GetUserById(currentUser.UmbracoId.Value);
+            if (umbracoUser == null
+                || umbracoUser.IsLockedOut
+                || !umbracoUser.IsApproved)
+            {
+                return Redirect("/");
+            }
+
+            UmbracoContext.Security.PerformLogin(umbracoUser.Id);
+
+            return Redirect("/umbraco");
+        }
+
+        public ActionResult Logout()
+        {
+            UmbracoContext.Security.ClearCurrentLogin();
+            FormsAuthentication.SignOut();
+            Session.Abandon();
+
+            return Redirect(FormsAuthentication.LoginUrl);
+        }
+    }
+}
