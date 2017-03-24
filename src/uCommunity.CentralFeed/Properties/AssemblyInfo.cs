e199d153a2aaf67584d0ddcb375a4962f1a8567a
Dmitry Bondar <dbo@compent.net>
2017-03-24 14:23:25 +0200

    Update package versions

--- src/uCommunity.CentralFeed/Properties/AssemblyInfo.cs
+++ src/uCommunity.CentralFeed/Properties/AssemblyInfo.cs
@@ -1,11 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("uCommunity.CentralFeed")]
 [assembly: AssemblyDescription("Central feed activity package.")]
 
-[assembly: AssemblyVersion("1.0.3.1")]
+[assembly: AssemblyVersion("1.0.3.2")]
 [assembly: ComVisible(false)]
 [assembly: Guid("0b1998e4-8fa0-4328-9cd2-bbba92fc4e98")]
 
