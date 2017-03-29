fb4512547f39b41fd70944548258395fbbfb205d
Maksim Didinov <mdi@compent.net>
2017-03-29 15:14:59 +0300

    Added controllers

---
+++ src/Compent.uCommunity/Controllers/CommentsPageController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+
+namespace Compent.uCommunity.Controllers
+{
+    public class CommentsPageController : RenderMvcController
+    {
+        public override ActionResult Index(RenderModel model)
+        { 
+            return View(model);
+        }
+    }
+}
