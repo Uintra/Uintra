5660c6814c94ab1945820f1c96f5d0f5e592b82a
Dmitriy Kavchuk <dka@compent.net>
2017-03-30 15:45:03 +0300

    Small fixes

--- src/BuildNugets.ps1
+++ src/BuildNugets.ps1
@@ -1,9 +1,8 @@
-Get-ChildItem *.csproj -Recurse | Where-Object {$_.Name -match "uCommunity\.([A-Za-z]*)\.csproj"} | ForEach-Object { 
+Get-ChildItem *.nuspec -Recurse | Where-Object {$_.Name -match "uCommunity\.([A-Za-z]*)\.nuspec"} | ForEach-Object { 
     # $assemblyInfo = (Get-Content ($_.Directory.FullName + "\Properties\AssemblyInfo.cs"));
     # $version = (($assemblyInfo -match 'AssemblyVersion\(".*"\)') -split ('"'))[1];
     # $title = (($assemblyInfo -match 'AssemblyTitle\(".*"\)') -split ('"'))[1];
     $pathToProjFile = $_.Directory.FullName + "\" + $_.Name;
-    Invoke-Expression ("msbuild " + $pathToProjFile + " /p:Configuration=Release");
     Invoke-Expression (".nuget\nuget pack " + $pathToProjFile)
 }
 
--- src/uCommunity.Users/Properties/AssemblyInfo.cs
+++ src/uCommunity.Users/Properties/AssemblyInfo.cs
@@ -10,4 +10,3 @@ using System.Runtime.InteropServices;
 [assembly: Guid("3e868f15-ac47-4962-a6a5-c9084752dde9")]
 
 [assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
