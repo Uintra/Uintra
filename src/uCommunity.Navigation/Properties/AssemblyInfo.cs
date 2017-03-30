05ad32497737ea4408952d2a63afd007c1071d71
Dmitriy Kavchuk <dka@compent.net>
2017-03-30 14:52:50 +0300

    Add navigation controller to project as compiled file

--- src/uCommunity.Navigation/Properties/AssemblyInfo.cs
+++ src/uCommunity.Navigation/Properties/AssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.2.6")]
+[assembly: AssemblyVersion("1.0.2.7")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
