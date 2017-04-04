2873d9a34a75588c7e0bc799bf337719e48d4ed5
Dmitry Bondar <dbo@compent.net>
2017-04-04 13:32:45 +0300

    Delete IHaveCreator in IntranetActivityViewModelBase. Add HeaderInfo property.

--- src/uCommunity.Core/Core/Activity/Models/IntranetActivityViewModelBase.cs
+++ src/uCommunity.Core/Core/Activity/Models/IntranetActivityViewModelBase.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using uCommunity.Core.User;
 
 namespace uCommunity.Core.Activity.Models
 {
-    public abstract class IntranetActivityViewModelBase : IHaveCreator
+    public abstract class IntranetActivityViewModelBase
     {
         public Guid Id { get; set; }
-        public IntranetActivityTypeEnum Type { get; set; }
-        public string Title { get; set; }
         public bool CanEdit { get; set; }
-        public int? UmbracoCreatorId { get; set; }
-        public Guid CreatorId { get; set; }
-        public IIntranetUser Creator { get; set; }
+
+        public IntranetActivityHeaderBase HeaderInfo { get; set; }
     }
 }
