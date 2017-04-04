3cba2bac92590f7063c00b8f07000f1377542e41
Dmitry Bondar <dbo@compent.net>
2017-04-04 10:47:46 +0300

    Add activity header views and models.

---
+++ src/uCommunity.Core/Core/Activity/Models/Header/IntranetActivityHeaderBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using uCommunity.Core.User;
+
+namespace uCommunity.Core.Activity.Models
+{
+    public class IntranetActivityHeaderBase
+    {
+        public IntranetActivityHeaderBase()
+        {
+            Dates = new List<string>();
+        }
+
+        public string Title { get; set; }
+        public IntranetActivityTypeEnum Type { get; set; }
+        public IIntranetUser Creator { get; set; }
+        public IEnumerable<string> Dates { get; set; }
+    }
+}
---
+++ src/uCommunity.Core/Core/Activity/Models/Header/IntranetActivityHeaderModel.cs
@@ -0,0 +1,7 @@
+﻿namespace uCommunity.Core.Activity.Models
+{
+    public class IntranetActivityHeaderModel : IntranetActivityHeaderBase
+    {
+        public string DetailsPageUrl { get; set; }
+    }
+}
