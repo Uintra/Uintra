2324e2af6dcd314e02a3c6de697711c143394d4e
Dmitry Bondar <dbo@compent.net>
2017-04-07 11:07:09 +0300

    Revert "Comment event code"
    This reverts commit 6d1893b039cc73ed32fc05ad1124e21d8cef01ac.

--- src/Compent.uCommunity/Core/Events/Entities/Event.cs
+++ src/Compent.uCommunity/Core/Events/Entities/Event.cs
@@ -1,14 +1,23 @@
-﻿namespace Compent.uCommunity.Core.Events
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using uCommunity.CentralFeed;
+using uCommunity.Comments;
+using uCommunity.Events;
+using uCommunity.Likes;
+using uCommunity.Subscribe;
+
+namespace Compent.uCommunity.Core.Events
 {
-    //public class Event : EventModelBase, ICentralFeedItem, ICommentable, ILikeable, ISubscribable
-    //{
-    //    [JsonIgnore]
-    //    public DateTime SortDate => PublishDate;
-    //    [JsonIgnore]
-    //    public IEnumerable<LikeModel> Likes { get; set; }
-    //    [JsonIgnore]
-    //    public IEnumerable<Comment> Comments { get; set; }
-    //    [JsonIgnore]
-    //    public IEnumerable<global::uCommunity.Subscribe.Subscribe> Subscribers { get; set; }
-    //}
+    public class Event : EventModelBase, ICentralFeedItem, ICommentable, ILikeable, ISubscribable
+    {
+        [JsonIgnore]
+        public DateTime SortDate => PublishDate;
+        [JsonIgnore]
+        public IEnumerable<LikeModel> Likes { get; set; }
+        [JsonIgnore]
+        public IEnumerable<Comment> Comments { get; set; }
+        [JsonIgnore]
+        public IEnumerable<global::uCommunity.Subscribe.Subscribe> Subscribers { get; set; }
+    }
 }
--- src/Compent.uCommunity/Core/Events/Models/EventOverviewItemModel2.cs
+++ src/Compent.uCommunity/Core/Events/Models/EventOverviewItemModel2.cs
@@ -1,10 +1,10 @@
-﻿//using uCommunity.Events;
-//using uCommunity.Likes;
+﻿using uCommunity.Events;
+using uCommunity.Likes;
 
-//namespace Compent.uCommunity.Core.Events
-//{
-//    public class EventOverviewItemModel2 : EventsOverviewItemModel
-//    {
-//        public ILikeable LikesInfo { get; set; }
-//    }
-//}
+namespace Compent.uCommunity.Core.Events
+{
+    public class EventOverviewItemModel2 : EventsOverviewItemModel
+    {
+        public ILikeable LikesInfo { get; set; }
+    }
+}
