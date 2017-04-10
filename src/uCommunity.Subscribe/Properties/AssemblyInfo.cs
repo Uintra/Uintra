b99f804bf8a2300199b626ee3da4e9957c3aa35e
Sergey Shepel <sse@compent.net>
2017-04-10 16:19:27 +0300

    Change return type of Subscribe in ISubscribableService

--- src/uCommunity.Subscribe/Properties/AssemblyInfo.cs
+++ src/uCommunity.Subscribe/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uCommunity.Subscribe")]
 [assembly: AssemblyDescription("Subscribe activity package.")]
 
-[assembly: AssemblyVersion("1.0.6.4")]
+[assembly: AssemblyVersion("1.0.7.0")]
 [assembly: ComVisible(false)]
 [assembly: Guid("dd7c6b81-0f9f-4196-89fe-4db666cd0367")]
