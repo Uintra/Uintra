0365fa483653b6f892e0738825da0249e5481cd0
Dmitriy Kavchuk <dka@compent.net>
2017-04-10 18:50:35 +0300

    Update news package

---
+++ src/Compent.uCommunity/Controllers/NewsController.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using uCommunity.CentralFeed;
+using uCommunity.Core.Activity;
+using uCommunity.Core.Media;
+using uCommunity.Core.User;
+using uCommunity.Core.User.Permissions;
+using uCommunity.News;
+using uCommunity.News.Web;
+
+namespace Compent.uCommunity.Controllers
+{
+    public class NewsController : NewsControllerBase
+    {
+        public NewsController(IIntranetUserService intranetUserService, INewsService<NewsBase, NewsModelBase> newsService, IMediaHelper mediaHelper) 
+            : base(intranetUserService, newsService, mediaHelper)
+        {
+        }
+
+        public ActionResult CentralFeedItem(ICentralFeedItem item)
+        {
+            FillLinks();
+            var activity = item as NewsModelBase;
+
+            var model = GetOverviewItems(Enumerable.Repeat(activity, 1)).Single();
+            return PartialView("~/App_Plugins/News/List/ItemView.cshtml", model);
+        }
+
+    }
+}
