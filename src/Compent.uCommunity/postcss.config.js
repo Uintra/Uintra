bda3f239edac166b88dddee88174b7d1b68f6fd8
Denys Dzhelomanov <dje@compent.net>
2017-04-11 17:47:21 +0300

    change webpack

--- src/Compent.uCommunity/postcss.config.js
+++ src/Compent.uCommunity/postcss.config.js
@@ -1,23 +1,27 @@
 module.exports = {
     plugins: [
         require("postcss-import"),
-        require('precss'),
-        require('autoprefixer')({ browsers: ['last 2 versions'] }),
-        require('postcss-css-variables')({
-            variables: {
-                '--color-white': '#fff',
-                '--color-red': '#dd0a2d',
-                '--color-black': '#000',
-                '--color-light-gray': '#ccc',
-                '--color-light-gray-2': '#eee',
-                '--color-light-gray-3': '#c7c7c7',
-                '--color-light-gray-4': '#aaa',
-                '--text-color-light': '#8f8f8f',
-                '--text-color-dark': '#333',
-                '--header-bg': '#373737',
-                '--font-custom': 'opensans, Arial, Helvetica, sans-serif;',
-                '--font-general': 'opensans, Arial, Helvetica, sans-serif;'
+        require('postcss-cssnext')({
+            browsers: ['last 2 version'],
+            features: {
+                customProperties: {
+                    variables: {
+                        '--color-white': '#fff',
+                        '--color-red': '#dd0a2d',
+                        '--color-black': '#000',
+                        '--color-light-gray': '#ccc',
+                        '--color-light-gray-2': '#eee',
+                        '--color-light-gray-3': '#c7c7c7',
+                        '--color-light-gray-4': '#aaa',
+                        '--text-color-light': '#8f8f8f',
+                        '--text-color-dark': '#333',
+                        '--header-bg': '#373737',
+                        '--font-custom': 'opensans, Arial, Helvetica, sans-serif',
+                        '--font-general': 'opensans, Arial, Helvetica, sans-serif'
+                    }
+                }
             }
-        })
+        }),
+        require('precss')
     ]
 };
