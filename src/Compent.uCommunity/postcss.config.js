b322fe242545fde34634325b224a43f5304022e9
Denys Dzhelomanov <dje@compent.net>
2017-04-11 14:42:04 +0300

    testing new type of variables declarations

--- src/Compent.uCommunity/postcss.config.js
+++ src/Compent.uCommunity/postcss.config.js
@@ -3,6 +3,21 @@ module.exports = {
         require("postcss-import"),
         require('precss'),
         require('autoprefixer')({ browsers: ['last 2 versions'] }),
-        require('postcss-css-variables')
+        require('postcss-css-variables')({
+            variables: {
+                '--color-white': '#fff',
+                '--color-red': '#dd0a2d',
+                '--color-black': '#000',
+                '--color-light-gray': '#ccc',
+                '--color-light-gray-2': '#eee',
+                '--color-light-gray-3': '#c7c7c7',
+                '--color-light-gray-4': '#aaa',
+                '--text-color-light': '#8f8f8f',
+                '--text-color-dark': '#333',
+                '--header-bg': '#373737',
+                '--font-custom': 'opensans, Arial, Helvetica, sans-serif;',
+                '--font-general': 'opensans, Arial, Helvetica, sans-serif;'
+            }
+        })
     ]
 };
