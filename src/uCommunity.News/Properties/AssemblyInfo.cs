ec32c66629add5112746894cc8837a543ba79a5f
Dmitry Bondar <dbo@compent.net>
2017-04-12 11:22:57 +0300

    Update Events and News assembly versions.

--- src/uCommunity.News/Properties/AssemblyInfo.cs
+++ src/uCommunity.News/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uCommunity.News")]
 [assembly: AssemblyDescription("News activity package")]
 
-[assembly: AssemblyVersion("1.0.6.6")]
+[assembly: AssemblyVersion("1.0.6.7")]
 [assembly: ComVisible(false)]
 [assembly: Guid("53386df1-d6a7-484b-8b07-c4fddebf4b83")]
