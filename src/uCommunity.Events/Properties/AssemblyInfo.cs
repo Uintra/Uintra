e729ca06fd9e4d0e59cf08c3f773e6a6f47888c2
Dmitriy Kavchuk <dka@compent.net>
2017-04-14 14:51:36 +0300

    Fix base controls plugin

--- src/uCommunity.Events/Properties/AssemblyInfo.cs
+++ src/uCommunity.Events/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uCommunity.Events")]
 [assembly: AssemblyDescription("Events activity package")]
 
-[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.0.3.1")]
 [assembly: ComVisible(false)]
 [assembly: Guid("85aa3a0a-738e-4e0d-9448-ea34aa3a537c")]
