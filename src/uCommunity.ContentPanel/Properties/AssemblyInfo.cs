8df5b242bf24ec55af55b923729e92f8c259ff1a
Dmitry Bondar <dbo@compent.net>
2017-04-18 15:48:57 +0300

    Add ContentPanel project.

---
+++ src/uCommunity.ContentPanel/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("uCommunity.ContentPanel")]
+[assembly: AssemblyDescription("Content panel package")]
+
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d5590777-b149-467f-829a-e901a9462e72")]
