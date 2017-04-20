7b2fdabefb87bcef6d925ab5ae0a92915e21da47
Denys Dzhelomanov <dje@compent.net>
2017-04-20 15:10:26 +0300

    CentralFeed,  News changes

--- src/Compent.uCommunity/App_Plugins/CentralFeed/CentralFeed.js
+++ src/Compent.uCommunity/App_Plugins/CentralFeed/CentralFeed.js
@@ -4,20 +4,112 @@ import umbracoAjaxForm from "./../Core/Content/scripts/UmbracoAjaxForm";
 import lightbox from "./../Core/Controls/LightboxGallery/LightboxGallery";
 import subscribe from "./../Subscribe/Subscribe";
 
-require("./centralFeed.css");
-
 var infinityScroll = helpers.infiniteScrollFactory;
 var scrollTo = helpers.scrollTo;
 var localStorage = helpers.localStorage;
-
 var centralFeedTabEvent = new CustomEvent("cfTabChanged");
 
+var holder;
+var state;
+var formController;
+
+function showLoadingStatus() {
+    var loadingElem = document.querySelector(".js-loading-status");
+    loadingElem && (loadingElem.style.display = "block");
+}
+
+function hideLoadingStatus() {
+    var loadingElem = document.querySelector(".js-loading-status");
+    loadingElem && (loadingElem.style.display = "none");
+}
+
+function initCustomControls(data) {
+    if (!data) {
+        return;
+    }
+
+    lightbox.init();
+    subscribe.initOnLoad();
+}
+
+function scrollPrevented() {
+    return !!parseInt(holder.querySelector('input[name="preventScrolling"]').value) | false;
+}
+
+function attachEventFilter() {
+    var showSubscribedElem = formController.form.querySelector('input[name="showSubscribed"]');
+    if (showSubscribedElem) {
+        showSubscribedElem.addEventListener('change', function () {
+            reload();
+        });
+    }
+}
+
+function reload(useVersion, skipLoadingStatus) {
+    if (!useVersion) {
+        holder.querySelector('input[name="version"]').value = null;
+    }
+
+    !skipLoadingStatus && showLoadingStatus();
+
+    saveState();
+    var promise = formController.reload();
+    promise.then(attachEventFilter);
+    promise.then(hideLoadingStatus);
+    promise.then(initCustomControls);
+    promise.catch(hideLoadingStatus);
+    return promise;
+}
+
+function saveState() {
+    localStorage.setItem(state.storageName, { page: state.page });
+}
+
+function restoreState() {
+    var hash = (window.location.hash || "").replace("#", "");
+    if (hash) {
+        var savedState = localStorage.getItem(state.storageName);
+        state.page = (savedState || {}).page || 1;
+        reload().then(function () {
+            var elem = document.querySelector('[data-anchor="' + hash + '"]');
+
+            if(elem){
+                scrollTo(document.body, elem.offsetTop, 300);
+                window.history.pushState("", document.title, window.location.pathname);
+            }
+        });
+    } else {
+        localStorage.removeItem(state.storageName);
+    }
+}
+
+function onScroll(done) {
+    if (scrollPrevented()) {
+        done();
+        return;
+    }
+    state.page++;
+    var promise = reload();
+    promise.then(done, done);
+}
+
+function tabClickEventHandler(e) {
+    if (!$(e.target).hasClass('_active') && !$(e.target).closest('._active').length > 0) {
+        e.preventDefault();
+        window.history.replaceState({}, "", e.currentTarget.dataset["pageUrl"]);
+        state.tab = e.currentTarget.dataset['type'];
+
+        $(e.target).closest('.tabset').removeClass('_expanded');
+    }
+}
+
 appInitializer.add(function () {
-    var holder = document.querySelector('.js-feed-overview');
+    holder = document.querySelector('.js-feed-overview');
     if (!holder) return;
+    formController = umbracoAjaxForm()(holder.querySelector("form.js-ajax-form"));
     var tabs = holder.querySelectorAll('.js-feed-links .js-feed-type');
-    var formController = umbracoAjaxForm()(holder.querySelector("form.js-ajax-form"));
-    var state = {
+    
+    state = {
         get tab() {
             return holder.querySelector('.js-feed-links .js-feed-type._active').dataset['type'];
         },
@@ -47,104 +139,9 @@ appInitializer.add(function () {
         }
     }
 
-    function attachEventFilter() {
-        var showSubscribedElem = formController.form.querySelector('input[name="showSubscribed"]');
-        if (showSubscribedElem) {
-            showSubscribedElem.addEventListener('change',
-                function () {
-                    reload();
-                });
-        }
-    }
-
-    function scrollPrevented() {
-        return !!parseInt(holder.querySelector('input[name="preventScrolling"]').value) | false;
-    }
-
-    function reload(useVersion, skipLoadingStatus) {
-        if (!useVersion) {
-            holder.querySelector('input[name="version"]').value = null;
-        }
-
-        !skipLoadingStatus && showLoadingStatus();
-
-        saveState();
-        var promise = formController.reload();
-        promise.then(attachEventFilter);
-        promise.then(hideLoadingStatus);
-        promise.then(initCustomControls);
-        promise.catch(hideLoadingStatus);
-        return promise;
-    }
-
-    function tabClickEventHandler(e) {
-        if (!$(e.target).hasClass('_active') && !$(e.target).closest('._active').length > 0) {
-            e.preventDefault();
-            window.history.replaceState({}, "", e.currentTarget.dataset["pageUrl"]);
-            state.tab = e.currentTarget.dataset['type'];
-
-            if ($(e.target).closest('.tabset').hasClass('_expanded')) {
-                $(e.target).closest('.tabset').removeClass('_expanded');
-            }
-        }
-    }
-
     for (var i = 0; i < tabs.length; i++) {
         var tab = tabs[i];
         tab.addEventListener('click', tabClickEventHandler);
-    }
-
-    function onScroll(done) {
-        if (scrollPrevented()) {
-            done();
-            return;
-        }
-        state.page++;
-        var promise = reload();
-        promise.then(done, done);
-    }
-
-    function saveState() {
-        localStorage.setItem(state.storageName, { page: state.page });
-    }
-
-    function restoreState() {
-        var hash = (window.location.hash || "").replace("#", "");
-        if (hash) {
-            var savedState = localStorage.getItem(state.storageName);
-            state.page = (savedState || {}).page || 1;
-            reload().then(function () {
-                var elem = document.querySelector('[data-anchor="' + hash + '"]');
-
-                if(elem){
-                    scrollTo(document.body, elem.offsetTop, 300);
-                    window.history.pushState("", document.title, window.location.pathname);
-                }
-            });
-        } else {
-            localStorage.removeItem(state.storageName);
-        }
-
-    }
-
-    function showLoadingStatus() {
-        var loadingElem = document.querySelector(".js-loading-status");
-        loadingElem && (loadingElem.style.display = "block");
-    }
-
-    function hideLoadingStatus() {
-        var loadingElem = document.querySelector(".js-loading-status");
-        loadingElem && (loadingElem.style.display = "none");
-    }
-
-    function initCustomControls(data) {
-        if (!data) {
-            return;
-        }
-
-        lightbox.init();
-
-      subscribe.initOnLoad();
     }
 
     restoreState();
