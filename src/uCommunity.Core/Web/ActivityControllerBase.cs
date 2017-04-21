86c47199298285fd20000f400f2e5798811b8b2b
Maksim Didinov <mdi@compent.net>
2017-04-21 13:50:17 +0300

    Core fixes

--- src/uCommunity.Core/Web/ActivityControllerBase.cs
+++ src/uCommunity.Core/Web/ActivityControllerBase.cs
@@ -4,17 +4,17 @@ using Umbraco.Web.Mvc;
 
 namespace uCommunity.Core
 {
-    public class ActivityControllerBase : SurfaceController
+    public abstract class ActivityControllerBase : SurfaceController
     {
         protected virtual string DetailsHeaderViewPath { get; } = "~/App_Plugins/Core/Activity/ActivityDetailsHeader.cshtml";
         protected virtual string ItemHeaderViewPath { get; } = "~/App_Plugins/Core/Activity/ActivityItemHeader.cshtml";
 
-        public ActionResult ActivityDetailsHeader(IntranetActivityDetailsHeaderViewModel header)
+        public virtual ActionResult ActivityDetailsHeader(IntranetActivityDetailsHeaderViewModel header)
         {
             return PartialView(DetailsHeaderViewPath, header);
         }
 
-        public ActionResult ActivityItemHeader(IntranetActivityItemHeaderViewModel header)
+        public virtual ActionResult ActivityItemHeader(IntranetActivityItemHeaderViewModel header)
         {
             return PartialView(ItemHeaderViewPath, header);
         }
