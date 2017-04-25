d12ce91b603a8423274f73fc16189b4641329f4f
Denys Dzhelomanov <dje@compent.net>
2017-04-25 11:43:00 +0300

    Events

--- src/uCommunity.Events/App_Plugins/Events/Edit/edit-events.js
+++ src/uCommunity.Events/App_Plugins/Events/Edit/edit-events.js
@@ -18,165 +18,159 @@ require('./../../Core/Content/scripts/ValidationExtensions');
 require('flatpickr/dist/flatpickr.min.css');
 require('dropzone/dist/min/dropzone.min.css');
 
-var EditEventsController = (function () {
-    var holder;
-    var userSelect;
-    var editor;
-    var form;
+var holder;
+var userSelect;
+var editor;
+var form;
 
-    var initUserSelect = function () {
-        userSelect = holder.find('#js-user-select').select2({});
-    }
+var initUserSelect = function () {
+    userSelect = holder.find('#js-user-select').select2({});
+}
 
-    var initDatePicker = function (containerSelector, valueSelector) {
-        var dateElem = holder.find(containerSelector);
-        var dateFormat = dateElem.data('dateFormat');
-        var dateElemValue = holder.find(valueSelector);
-        var defaultDate = new Date(dateElem.data('defaultDate'));
+var initDatePicker = function (containerSelector, valueSelector) {
+    var dateElem = holder.find(containerSelector);
+    var dateFormat = dateElem.data('dateFormat');
+    var dateElemValue = holder.find(valueSelector);
+    var defaultDate = new Date(dateElem.data('defaultDate'));
 
-        var datePicker = new Flatpickr(dateElem[0], {
-            enableTime: true,
-            time_24hr: true,
-            allowInput: false,
-            weekNumbers: true,
-            dateFormat: dateFormat,
-            locale: FlatpickrLang.da,
-            onChange: function (selectedDates) {
-                if (selectedDates.length === 0) {
-                    dateElemValue.val('');
-                    return;
-                }
-
-                dateElemValue.val(selectedDates[0].toISOString());
-            }
-        });
-
-        defaultDate = helpers.removeOffset(defaultDate);
-        datePicker.setDate(defaultDate, true);
-        var minDate = new Date();
-        if (defaultDate < minDate) {
-            minDate = defaultDate;
-        }
-
-        datePicker.set('minDate', minDate.setHours(0));
-    }
-
-    var continueSubmit = function (value) {
-        $('#notifyAllSubscribers').val(value);
-        form.submit();
-    }
-
-    var initSubmitButton = function () {
-        form = holder.find('#editForm');
-        var btn = holder.find('.form__btn._submit');
-        var descriptionElem = holder.find('#description');
-
-        btn.click(function (event) {
-            if (!form.valid()) {
-                event.preventDefault();
+    var datePicker = new Flatpickr(dateElem[0], {
+        enableTime: true,
+        time_24hr: true,
+        allowInput: false,
+        weekNumbers: true,
+        dateFormat: dateFormat,
+        locale: FlatpickrLang.da,
+        onChange: function (selectedDates) {
+            if (selectedDates.length === 0) {
+                dateElemValue.val('');
                 return;
             }
 
-            if (editor.getLength() <= 1) {
-                event.preventDefault();
-                descriptionElem.addClass('input-validation-error');
-            }
+            dateElemValue.val(selectedDates[0].toISOString());
+        }
+    });
 
-            descriptionElem.removeClass('input-validation-error');
+    defaultDate = helpers.removeOffset(defaultDate);
+    datePicker.setDate(defaultDate, true);
+    var minDate = new Date();
+    if (defaultDate < minDate) {
+        minDate = defaultDate;
+    }
+
+    datePicker.set('minDate', minDate.setHours(0));
+}
+
+var continueSubmit = function (value) {
+    $('#notifyAllSubscribers').val(value);
+    form.submit();
+}
+
+var initSubmitButton = function () {
+    form = holder.find('#editForm');
+    var btn = holder.find('.form__btn._submit');
+    var descriptionElem = holder.find('#description');
+
+    btn.click(function (event) {
+        if (!form.valid()) {
             event.preventDefault();
-
-            var data = umbracoAjaxForm()(form[0]).serialize();
-            $.post('/umbraco/surface/Events/HasConfirmation', data, function (result) {
-                
-                if (result && !result.HasConfirmation) {
-                    continueSubmit(false);
-                    return;
-                }
-
-                var callbacks = [
-                         function () {
-                             continueSubmit(true);
-                         }, function () {
-                             continueSubmit(false);
-                         },
-                         function () {
-                             return true;
-                         }
-                ];
-
-                var textQuestion = btn.data('text');
-                var textYes = btn.data('yes');
-                var textNo = btn.data('no');
-                var textCancel = btn.data('cancel');
-                Alertify.defaults.glossary.cancel = textCancel;
-                Alertify.defaults.glossary.yes = textYes;
-                Alertify.defaults.glossary.no = textNo;
-
-                window.App.Confirm.showDialog(textQuestion, callbacks, App.Confirm.defaultSettings);
-            });
-        });
-    }
-
-    var initDescriptionControl = function () {
-
-        var dataStorage = holder.find('#js-hidden-description-container');
-        if (!dataStorage) {
-            throw new Error("EditNews: Hiden input field missing");
+            return;
         }
-        var descriptionElem = holder.find('#description');
 
-        editor = helpers.initQuill(descriptionElem[0], dataStorage[0], { theme: 'snow' });
+        if (editor.getLength() <= 1) {
+            event.preventDefault();
+            descriptionElem.addClass('input-validation-error');
+        }
 
-        editor.on('text-change', function () {
-            dataStorage.val(editor.container.firstChild.innerHTML);
+        descriptionElem.removeClass('input-validation-error');
+        event.preventDefault();
 
-            if (editor.getLength() > 1 && descriptionElem.hasClass('input-validation-error')) {
-                descriptionElem.removeClass('input-validation-error');
-            }
-        });
-    }
-
-    var initHideControl = function () {
-        var hideControl = holder.find('.js-event-hide');
-
-        var text = hideControl.data('text');
-        var textOk = hideControl.data('ok');
-        var textCancel = hideControl.data('cancel');
-        Alertify.defaults.glossary.cancel = textCancel;
-        Alertify.defaults.glossary.ok = textOk;
-
-        hideControl.on('click', function () {
-            window.App.Confirm.showConfirm(text,
-                 function () {
-                     $.post('/umbraco/surface/Events/Hide?id=' + hideControl.data('id'),
-                         function (data) {
-                             window.location.href = data.Url;
-                         });
-                 }, function () { }, App.Confirm.defaultSettings);
-
-            return false;
-        });
-    }
-
-    var controller = {
-        init: function () {
-            holder = $('#js-events-edit-page');
-            if (!holder.length) {
+        var data = umbracoAjaxForm()(form[0]).serialize();
+        $.post('/umbraco/surface/Events/HasConfirmation', data, function (result) {
+                
+            if (result && !result.HasConfirmation) {
+                continueSubmit(false);
                 return;
             }
 
-            initUserSelect();
-            initDatePicker('#js-start-date', '#js-start-date-value');
-            initDatePicker('#js-end-date', '#js-end-date-value');
-            initDescriptionControl();
-            initSubmitButton();
-            initHideControl();
-            fileUploadController.init(holder);
-        }
-    }
-    appInitializer.add(controller.init);
-    return controller;
-})();
+            var callbacks = [
+                     function () {
+                         continueSubmit(true);
+                     }, function () {
+                         continueSubmit(false);
+                     },
+                     function () {
+                         return true;
+                     }
+            ];
 
-window.App = window.App || {};
-window.App.EditEventsController = EditEventsController;
+            var textQuestion = btn.data('text');
+            var textYes = btn.data('yes');
+            var textNo = btn.data('no');
+            var textCancel = btn.data('cancel');
+            Alertify.defaults.glossary.cancel = textCancel;
+            Alertify.defaults.glossary.yes = textYes;
+            Alertify.defaults.glossary.no = textNo;
+
+            window.App.Confirm.showDialog(textQuestion, callbacks, App.Confirm.defaultSettings);
+        });
+    });
+}
+
+var initDescriptionControl = function () {
+    var dataStorage = holder.find('#js-hidden-description-container');
+    if (!dataStorage) {
+        throw new Error("EditNews: Hiden input field missing");
+    }
+    var descriptionElem = holder.find('#description');
+
+    editor = helpers.initQuill(descriptionElem[0], dataStorage[0], { theme: 'snow' });
+
+    editor.on('text-change', function () {
+        dataStorage.val(editor.container.firstChild.innerHTML);
+
+        if (editor.getLength() > 1 && descriptionElem.hasClass('input-validation-error')) {
+            descriptionElem.removeClass('input-validation-error');
+        }
+    });
+}
+
+var initHideControl = function () {
+    var hideControl = holder.find('.js-event-hide');
+
+    var text = hideControl.data('text');
+    var textOk = hideControl.data('ok');
+    var textCancel = hideControl.data('cancel');
+    Alertify.defaults.glossary.cancel = textCancel;
+    Alertify.defaults.glossary.ok = textOk;
+
+    hideControl.on('click', function () {
+        window.App.Confirm.showConfirm(text,
+             function () {
+                 $.post('/umbraco/surface/Events/Hide?id=' + hideControl.data('id'),
+                     function (data) {
+                         window.location.href = data.Url;
+                     });
+             }, function () { }, App.Confirm.defaultSettings);
+
+        return false;
+    });
+}
+
+var controller = {
+    init: function () {
+        holder = $('#js-events-edit-page');
+        if (!holder.length) {
+            return;
+        }
+
+        initUserSelect();
+        initDatePicker('#js-start-date', '#js-start-date-value');
+        initDatePicker('#js-end-date', '#js-end-date-value');
+        initDescriptionControl();
+        initSubmitButton();
+        initHideControl();
+        fileUploadController.init(holder);
+    }
+}
+
+appInitializer.add(controller.init);
