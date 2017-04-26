cf551f0f207479f28fea76d88b21c71b03a69d89
Denys Dzhelomanov <dje@compent.net>
2017-04-26 15:05:03 +0300

    subscribe, comments refactoring

--- src/Compent.uCommunity/Content/main.js
+++ src/Compent.uCommunity/Content/main.js
@@ -2,13 +2,8 @@
 require('./../App_Plugins/Navigation/navigation');
 require('./../App_Plugins/News/news');
 require('./../App_Plugins/CentralFeed/centralFeed');
-
-import commentOverview from "./../App_Plugins/Comments/comment";
-import subscribe from "./../App_Plugins/Subscribe/subscribe";
-
-window.subscribe = subscribe;
-window.CommentOverview = commentOverview;
-
+require("./../App_Plugins/Comments/comment");
+require("./../App_Plugins/Subscribe/subscribe");
 require('./../App_Plugins/Subscribe/subscribeList');
 require('./../App_Plugins/Events/events');
 require('./../App_Plugins/Notification/notification');
