31edc8ebf2f62cc363df434aa3fd9bd1d8d6e6f3
Dmitry Bondar <dbo@compent.net>
2017-04-26 16:28:55 +0300

    Don't show description on CentalFeed for Events.

--- src/uCommunity.Events/Core/Models/EventsOverviewItemViewModel.cs
+++ src/uCommunity.Events/Core/Models/EventsOverviewItemViewModel.cs
@@ -11,8 +11,6 @@ namespace uCommunity.Events
 
         public string Teaser { get; set; }
 
-        public string Description { get; set; }
-
         public DateTime StartDate { get; set; }
 
         public DateTime EndDate { get; set; }
