a2f28db0725904b4204b46bfe80f55141c9ce1ad
Dmitriy Kavchuk <dka@compent.net>
2017-05-03 17:41:22 +0300

    Small fix

--- src/uCommunity.Core/Core/Media/IMediaHelper.cs
+++ src/uCommunity.Core/Core/Media/IMediaHelper.cs
@@ -1,9 +1,12 @@
 using System.Collections.Generic;
+using uCommunity.Core.Controls.FileUpload;
+using Umbraco.Core.Models;
 
 namespace uCommunity.Core.Media
 {
     public interface IMediaHelper
     {
         IEnumerable<int> CreateMedia(IContentWithMediaCreateEditModel model);
+        IMedia CreateMedia(TempFile file, IMedia rootMedia);
     }
 }
