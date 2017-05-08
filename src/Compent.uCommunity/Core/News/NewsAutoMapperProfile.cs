00102ebfd7e571477d0e2281c35e8864b37662ed
Dmitry Bondar <dbo@compent.net>
2017-05-08 16:38:08 +0300

    Update packages.

--- src/Compent.uCommunity/Core/News/NewsAutoMapperProfile.cs
+++ src/Compent.uCommunity/Core/News/NewsAutoMapperProfile.cs
@@ -3,6 +3,7 @@ using Compent.uCommunity.Core.News.Entities;
 using Compent.uCommunity.Core.News.Models;
 using uCommunity.Core.Activity.Models;
 using uCommunity.News;
+using uCommunity.News.Dashboard;
 
 namespace Compent.uCommunity.Core.News
 {
@@ -10,7 +11,7 @@ namespace Compent.uCommunity.Core.News
     {
         protected override void Configure()
         {
-            Mapper.CreateMap<Entities.NewsEntity, NewsExtendedViewModel>()
+            Mapper.CreateMap<NewsEntity, NewsExtendedViewModel>()
                 .IncludeBase<NewsBase, NewsViewModel>()
                 .ForMember(dst => dst.LikesInfo, o => o.MapFrom(el => el))
                 .ForMember(dst => dst.CommentsInfo, o => o.MapFrom(el => el));
@@ -18,6 +19,9 @@ namespace Compent.uCommunity.Core.News
             Mapper.CreateMap<NewsEntity, NewsOverviewItemExtendedViewModel>()
                 .IncludeBase<NewsBase, NewsOverviewItemViewModel>()
                 .ForMember(dst => dst.LikesInfo, o => o.MapFrom(el => el));
+
+            Mapper.CreateMap<NewsEntity, NewsBackofficeViewModel>()
+              .IncludeBase<NewsBase, NewsBackofficeViewModel>();
 
             Mapper.CreateMap<NewsEntity, IntranetActivityItemHeaderViewModel>()
                  .IncludeBase<NewsBase, IntranetActivityItemHeaderViewModel>();
