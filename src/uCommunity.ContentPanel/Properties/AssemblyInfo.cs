69ee9f01212ac0e2a8d770c35b1213cf18cd730f
Dmitriy Kavchuk <dka@compent.net>
2017-05-11 12:40:40 +0300

    Index removed files

--- src/uCommunity.ContentPanel/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("uCommunity.Panels")]
-[assembly: AssemblyDescription("Panels package")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: ComVisible(false)]
-[assembly: Guid("009e188c-14f8-4bb8-aea0-3d39eb12ab82")]
