203ab1c3eeba3193f4fe39e54bae8984971a758a
Dmitriy Kavchuk <dka@compent.net>
2017-05-11 12:37:26 +0300

    Fix package manifests. Rename Project folder

---
+++ src/uCommunity.Panels/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("uCommunity.Panels")]
+[assembly: AssemblyDescription("Panels package")]
+
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: ComVisible(false)]
+[assembly: Guid("009e188c-14f8-4bb8-aea0-3d39eb12ab82")]
