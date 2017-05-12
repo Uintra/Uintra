524926d597fa33a4561810c52872df25f5dd4645
Julia Shulumba <jsh@compent.net>
2017-05-12 12:59:26 +0300

    UC-232 Likes popup. Design. Show users list in separate lines.

--- src/uCommunity.Likes/Properties/AssemblyInfo.cs
+++ src/uCommunity.Likes/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uCommunity.Likes")]
 [assembly: AssemblyDescription("Likes activity package.")]
 
-[assembly: AssemblyVersion("1.0.7.0")]
+[assembly: AssemblyVersion("1.0.8.0")]
 [assembly: ComVisible(false)]
 [assembly: Guid("b99f0f05-598c-4f4a-a972-7282dcf9fed8")]
