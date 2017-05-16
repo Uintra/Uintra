a79f701e6534e3d0602a18e9d405f58b8f9c453f
Vasil Vasilik <vva@compent.net>
2017-05-16 13:00:18 +0300

    UC-195 - [Design] Update design for Navigation. Fix expand after add link

--- src/uCommunity.Navigation/App_Plugins/Navigation/MyLinks/myLinks.js
+++ src/uCommunity.Navigation/App_Plugins/Navigation/MyLinks/myLinks.js
@@ -5,20 +5,13 @@ var active = "_expand";
 var controller = {
     switchLinkIcon: function() {
         $('#mylinkIcon').find('span').toggleClass('_isLinked');
-        addListeners();
     }
 }
 
-function addListeners() {
+appInitializer.add(function() {
     $(".js-my-links__opener").on("click", function() {
         $(this).toggleClass(active);
     });
-
-    $("#mylinkIcon").on("click", function() {
-        $('#myLinks_addRemove').click();
-    });
-}
-
-appInitializer.add(addListeners);
+});
 
 export default controller;
