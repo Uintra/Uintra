c7354899d69d671eeae7b7e77aae478bb2da81fe
Dmitry Bondar <dbo@compent.net>
2017-05-16 10:11:31 +0300

    Update Taggin in uIntro.

--- src/Compent.uCommunity/Core/News/Models/NewsExtendedCreateModel.cs
+++ src/Compent.uCommunity/Core/News/Models/NewsExtendedCreateModel.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections.Generic;
 using uCommunity.News;
+using uCommunity.Tagging;
 
 namespace Compent.uCommunity.Core.News.Models
 {
-    public class NewsExtendedCreateModel : NewsCreateModel
+    public class NewsExtendedCreateModel : NewsCreateModel, ITagsCreateEditModel
     {
         public IEnumerable<string> Tags { get; set; }
     }
--- src/Compent.uCommunity/Core/News/Models/NewsExtendedEditModel.cs
+++ src/Compent.uCommunity/Core/News/Models/NewsExtendedEditModel.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections.Generic;
 using uCommunity.News;
+using uCommunity.Tagging;
 
 namespace Compent.uCommunity.Core.News.Models
 {
-    public class NewsExtendedEditModel : NewsEditModel
+    public class NewsExtendedEditModel : NewsEditModel, ITagsCreateEditModel
     {
         public IEnumerable<string> Tags { get; set; }
     }
