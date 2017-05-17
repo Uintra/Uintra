0b66c6f6cb3ce3109c59296df88087a39f907a9a
Julia Shulumba <jsh@compent.net>
2017-05-17 11:18:42 +0300

    UC-215 Content. Video. It should be possible to exit from full-size view.

--- src/Compent.uCommunity/Content/head_css.js
+++ src/Compent.uCommunity/Content/head_css.js
@@ -1,3 +1,5 @@
 ﻿require('bootstrap/dist/css/bootstrap.min.css');
+require('photoswipe/dist/photoswipe.css');
+require('photoswipe/dist/default-skin/default-skin.css');
 require('./../App_Plugins/Core/Content/styles/Core.css');
 require('./css/styles.css');
