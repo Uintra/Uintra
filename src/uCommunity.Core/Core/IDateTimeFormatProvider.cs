f88aaede2a144c20189d3e51ff23f7adf7fe2da3
Maksim Didinov <mdi@compent.net>
2017-05-18 15:45:25 +0300

    Added IDateTimeFormatProvider to Core

---
+++ src/uCommunity.Core/Core/IDateTimeFormatProvider.cs
@@ -0,0 +1,12 @@
+﻿namespace Compent.uCommunity.Core
+{
+    public interface IDateTimeFormatProvider
+    {
+        string DefaultTimeFormat { get; set; }
+        string DefaultDateFormat { get; set; }
+        string DefaultDateTimeFormat { get; set; }
+        string DefaultDateTimeValuePickerFormat { get; set; }
+        string DefaultDatePickerFormat { get; set; }
+        string DefaultDateTimePickerFormat { get; set; }
+    }
+}
