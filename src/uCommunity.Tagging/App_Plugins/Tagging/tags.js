9fa8e30ffd94aacd64e2d0aea1a00255b6524345
Dmitry Bondar <dbo@compent.net>
2017-05-18 12:43:03 +0300

    Refactor tagging.

--- src/uCommunity.Tagging/App_Plugins/Tagging/tags.js
+++ src/uCommunity.Tagging/App_Plugins/Tagging/tags.js
@@ -1,52 +1,55 @@
-﻿var Taggle = require("taggle");
+﻿import appInitializer from "./../Core/Content/scripts/AppInitializer";
+
+var Taggle = require("taggle");
 
 require('devbridge-autocomplete');
 
-var TagsController = {
-    init: function () {
-        var activityTagsContainer = $('#activityTags');
+function initTagsControl() {
+    var holder = $('#activityTags');
+    if (!holder.length) return;
 
-        var activityTags = [];
-        var activityTagsProperty = activityTagsContainer.data('activity-tags');
-        if (activityTagsProperty) {
-            if ((activityTagsProperty + '').indexOf(',') !== -1) {
-                activityTags = activityTagsProperty.split(',');
-            } else {
-                activityTags = [activityTagsProperty];
-            }
+    var activityTags = [];
+    var activityTagsProperty = holder.data('activity-tags');
+    if (activityTagsProperty) {
+        if ((activityTagsProperty + '').indexOf(',') !== -1) {
+            activityTags = activityTagsProperty.split(',');
+        } else {
+            activityTags = [activityTagsProperty];
         }
-
-        var tagsControl = new Taggle(activityTagsContainer[0], {
-            preserveCase: true,
-            placeholder : activityTagsContainer.data('placeholder'),
-            hiddenInputName : activityTagsContainer.data('model-name'),
-            tags: activityTags
-        });
-
-        var tagsControlInput = tagsControl.getInput();
-
-        $(tagsControlInput).devbridgeAutocomplete({
-            serviceUrl: '/umbraco/surface/Tags/TagsAutocomplete',
-            paramName: 'query',
-            minChars: 3,
-            dataType: 'json',
-            transformResult: function (response, originalQuery) {
-                var result = {
-                    suggestions: $.map(response.Tags, function (dataItem) {
-                        return { value: dataItem };
-                    })
-                };
-
-                return result;
-            },
-            formatResult: function (suggestion, currentValue) {
-                return "<span>" + suggestion.value + "</span>";
-            },
-            onSelect: function (suggestion) {
-                tagsControl.add(suggestion.value);
-            }
-        });
     }
+
+    var tagsControl = new Taggle(holder[0], {
+        preserveCase: true,
+        placeholder : holder.data('placeholder'),
+        hiddenInputName : holder.data('model-name'),
+        tags: activityTags
+    });
+
+    var tagsControlInput = tagsControl.getInput();
+
+    $(tagsControlInput).devbridgeAutocomplete({
+        serviceUrl: '/umbraco/surface/Tags/TagsAutocomplete',
+        paramName: 'query',
+        minChars: 3,
+        dataType: 'json',
+        transformResult: function (response, originalQuery) {
+            var result = {
+                suggestions: $.map(response.Tags, function (dataItem) {
+                    return { value: dataItem };
+                })
+            };
+
+            return result;
+        },
+        formatResult: function (suggestion, currentValue) {
+            return "<span>" + suggestion.value + "</span>";
+        },
+        onSelect: function (suggestion) {
+            tagsControl.add(suggestion.value);
+        }
+    });
 }
 
-export default TagsController;
+appInitializer.add(function () {
+    initTagsControl();
+});
--- src/uCommunity.Tagging/Properties/AssemblyInfo.cs
+++ src/uCommunity.Tagging/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uCommunity.Tagging")]
 [assembly: AssemblyDescription("Tagging activity package")]
 
-[assembly: AssemblyVersion("0.0.0.6")]
+[assembly: AssemblyVersion("0.0.1.0")]
 [assembly: ComVisible(false)]
 [assembly: Guid("2167c278-86ed-451b-8d2c-01774532b4e8")]
