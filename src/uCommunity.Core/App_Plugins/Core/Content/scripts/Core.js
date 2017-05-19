9a4adeafbd7945192ad3299d3873c08ececa5491
Sergey Shepel <sse@compent.net>
2017-05-19 11:59:15 +0300

    Move init AnchorScroll to appInitializer;

--- src/uCommunity.Core/App_Plugins/Core/Content/scripts/Core.js
+++ src/uCommunity.Core/App_Plugins/Core/Content/scripts/Core.js
@@ -1,7 +1,5 @@
-﻿import initAnchorScroll from "./AnchorScroll"
-import showContent from "./ShowContent"
+﻿import showContent from "./ShowContent"
 
 window.$ = window.jQuery = require('jquery');
-initAnchorScroll();
 
 document.addEventListener('DOMContentLoaded', showContent, false);
