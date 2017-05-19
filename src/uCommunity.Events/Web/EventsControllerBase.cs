277e7475a48f4efafd3593cc173abbcad8a9c2a8
Maksim Didinov <mdi@compent.net>
2017-05-19 12:35:06 +0300

    Fixes

--- src/uCommunity.Events/Web/EventsControllerBase.cs
+++ src/uCommunity.Events/Web/EventsControllerBase.cs
@@ -200,6 +200,7 @@ namespace uCommunity.Events.Web
 
             var mediaSettings = _eventsService.GetMediaSettings();
             ViewData["AllowedMediaExtentions"] = mediaSettings.AllowedMediaExtentions;
+            ViewData.SetDateTimeFormats();
             model.MediaRootId = mediaSettings.MediaRootId;
         }
 
