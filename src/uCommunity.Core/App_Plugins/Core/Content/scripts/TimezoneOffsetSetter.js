82b9604540396cd9e5f130de6bcf1e39be30d106
Maksim Didinov <mdi@compent.net>
2017-05-24 13:41:36 +0300

    Core. TimezoneOffset JS

---
+++ src/uCommunity.Core/App_Plugins/Core/Content/scripts/TimezoneOffsetSetter.js
@@ -0,0 +1,16 @@
+﻿import appInitializer from "./AppInitializer";
+
+function initTimezoneOffsetSetter() {
+    var holder = document.getElementsByClassName("clientTimezoneOffset");
+
+    if (holder.length) {
+        var currentDate = new Date();
+        holder[0].value = currentDate.getTimezoneOffset();
+    }
+}
+
+appInitializer.add(function() {
+    initTimezoneOffsetSetter();
+});
+
+export default initTimezoneOffsetSetter;
