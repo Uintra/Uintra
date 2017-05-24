5a9c67904df950db40e3b1e974a44d2c8e6d17f2
Maksim Didinov <mdi@compent.net>
2017-05-24 11:25:02 +0300

    Fixes

--- src/Compent.uCommunity/Controllers/LoginController.cs
+++ src/Compent.uCommunity/Controllers/LoginController.cs
@@ -1,4 +1,7 @@
-﻿using uCommunity.Users.Web;
+﻿using System;
+using System.Web.Mvc;
+using uCommunity.Core;
+using uCommunity.Users.Web;
 using Umbraco.Core.Services;
 
 namespace Compent.uCommunity.Controllers
@@ -7,9 +10,27 @@ namespace Compent.uCommunity.Controllers
     public class LoginController: LoginControllerBase
     {
         protected override string LoginViewPath => "~/Views/Login/Login.cshtml";
+        private readonly ITimezoneOffsetProvider _timezoneOffsetProvider;
 
-        public LoginController(IMemberService memberService) : base(memberService)
+        public LoginController(IMemberService memberService, ITimezoneOffsetProvider timezoneOffsetProvider) : base(memberService)
         {
+            _timezoneOffsetProvider = timezoneOffsetProvider;
+        }
+
+        [HttpPost]
+        public override ActionResult Login(string login, string password, string returnUrl)
+        {
+            var redirectUrl = returnUrl ?? DefaultRedirectUrl;
+            if (Members.Login(login, password))
+            {
+                redirectUrl = HttpContext.Request.Url?.AbsoluteUri ?? DefaultRedirectUrl;
+                _timezoneOffsetProvider.SetTimezoneOffset(60);
+                var check = _timezoneOffsetProvider.GetTimezoneOffset();
+
+                var serverUtcOffset = TimeZoneInfo.Local.GetUtcOffset(DateTime.UtcNow).Minutes;
+            }
+            return Redirect(redirectUrl);
+
         }
     }
 }
