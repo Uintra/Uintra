e403186f4fcace298535a2703aba6fd84b4528dc
Dmitry Bondar <dbo@compent.net>
2017-05-25 16:43:50 +0300

    Update package assembly versions.

--- src/uIntra.Subscribe/Properties/AssemblyInfo.cs
+++ src/uIntra.Subscribe/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.Subscribe")]
 [assembly: AssemblyDescription("Subscribe activity package.")]
 
-[assembly: AssemblyVersion("1.0.9.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
 [assembly: ComVisible(false)]
 [assembly: Guid("dd7c6b81-0f9f-4196-89fe-4db666cd0367")]
