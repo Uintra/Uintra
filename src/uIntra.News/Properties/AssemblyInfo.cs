101d88f744e775294c3fe922dc4395c9f6eb15e7
Dmitry Bondar <dbo@compent.net>
2017-05-30 14:06:23 +0300

    Delete OrmLite in packages.

--- src/uIntra.News/Properties/AssemblyInfo.cs
+++ src/uIntra.News/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.News")]
 [assembly: AssemblyDescription("News activity package")]
 
-[assembly: AssemblyVersion("1.1.0.5")]
+[assembly: AssemblyVersion("1.1.0.6")]
 [assembly: ComVisible(false)]
 [assembly: Guid("53386df1-d6a7-484b-8b07-c4fddebf4b83")]
