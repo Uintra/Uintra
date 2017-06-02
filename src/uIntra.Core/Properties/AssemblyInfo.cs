3b7e79b740bbcb8abc9b97f4cfd93d7cd7943207
Vasil Vasilik <vva@compent.net>
2017-06-02 18:54:51 +0300

    Revert "UC-378 - Gallery. Mobile. Already show close button, not just after clicking on image."
    This reverts commit 3e2046578b1a5cedb35853766078c368701f655e.

--- src/uIntra.Core/Properties/AssemblyInfo.cs
+++ src/uIntra.Core/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.Core")]
 [assembly: AssemblyDescription("Core activity package.")]
 
-[assembly: AssemblyVersion("1.1.2.19")]
+[assembly: AssemblyVersion("1.1.2.18")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3f8c785a-48d0-41f5-9257-6f73afca41eb")]
