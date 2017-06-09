0496d8da0ba9193c0d32df077567f55dda230fbe
Dmitry Bondar <dbo@compent.net>
2017-06-09 12:47:52 +0300

    Update packages.

--- src/Compent.uIntra/App_Plugins/Core/Content/scripts/Ajax.js
+++ src/Compent.uIntra/App_Plugins/Core/Content/scripts/Ajax.js
@@ -67,6 +67,30 @@ var ajax = {
             jsonRegex: applicationJsonRegex
         };
         return post(url, JSON.stringify(data), onSuccess, onError, options);
+    },
+    Delete: function (url, onSuccess, onError) {
+        if (onSuccess || onError) {
+            console.warn("onSuccess and onError callbacks obsolete. use promise instead");
+        }
+
+        return new Promise(function (resolve, reject) {
+            var request = new XMLHttpRequest();
+            request.open('DELETE', url, true);
+
+            request.onload = () => {
+                if (request.status >= 200 && request.status < 400) {
+                    var responseText = request.responseText;
+                    var resultData = formJsonRegex.test(responseText) ? JSON.parse(responseText) : responseText;
+                    onSuccess && onSuccess(resultData);
+                    resolve(resultData);
+                } else {
+                    onError && onError({ code: request.status, message: request.responseText });
+                    reject({ code: request.status, message: request.responseText });
+                }
+            }
+            request.onerror = onError;
+            request.send();
+        });
     }
 }
 
