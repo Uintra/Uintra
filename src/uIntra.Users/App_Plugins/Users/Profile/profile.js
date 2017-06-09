e5aff3d8650e64b47f6515ed5a9568e643b56b60
Dmitry Bondar <dbo@compent.net>
2017-06-09 12:39:57 +0300

    UC-364: Uintra. Member profile. It shoud be possible delete avatar.

--- src/uIntra.Users/App_Plugins/Users/Profile/profile.js
+++ src/uIntra.Users/App_Plugins/Users/Profile/profile.js
@@ -1,18 +1,33 @@
 ﻿import appInitializer from "./../../Core/Content/scripts/AppInitializer";
 import helpers from "./../../Core/Content/scripts/Helpers";
 import fileUploadController from "./../../Core/Controls/FileUpload/file-upload";
+import ajax from "./../../Core/Content/scripts/Ajax";
 
 require("./profile.css");
 
-var holder;
+var initDeleteButton = function (holder) {    
+    var btn = holder.find('.js-delete-btn');  
+
+    btn.click(function (event) { 
+        var confirmMessage = btn.data('confirm-message');
+        if (!confirm(confirmMessage)) {
+            return;
+        }
+
+        ajax.Delete("/umbraco/surface/Profile/DeletePhoto").then(function(response) {
+            location.reload();
+        });;
+    });
+}
 
 var controller = {
     init: function () {
-        holder = $('#js-profile-page');
-
+        var holder = $('#js-profile-page');
         if (!holder.length) {
             return;
         }
+
+        initDeleteButton(holder);
         fileUploadController.init(holder);
     }
 }
