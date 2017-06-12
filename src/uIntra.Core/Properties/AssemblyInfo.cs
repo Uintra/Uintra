c5f95d56c4f02ec168ce83e3fb69ba499c176487
Maksim Didinov <mdi@compent.net>
2017-06-12 18:20:58 +0300

    Refactor PinActivity

--- src/uIntra.Core/Properties/AssemblyInfo.cs
+++ src/uIntra.Core/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.Core")]
 [assembly: AssemblyDescription("Core activity package.")]
 
-[assembly: AssemblyVersion("1.1.8.8")]
+[assembly: AssemblyVersion("1.1.8.9")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3f8c785a-48d0-41f5-9257-6f73afca41eb")]
