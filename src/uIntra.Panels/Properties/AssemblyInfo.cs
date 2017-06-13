827e6d51649f571b9002610a60953ffc65f586f2
Dmitry Bondar <dbo@compent.net>
2017-06-13 11:24:55 +0300

    UC-405: Deleted Global panel should not give crash on UI.

--- src/uIntra.Panels/Properties/AssemblyInfo.cs
+++ src/uIntra.Panels/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.Panels")]
 [assembly: AssemblyDescription("Panels package")]
 
-[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.1.0.1")]
 [assembly: ComVisible(false)]
 [assembly: Guid("009e188c-14f8-4bb8-aea0-3d39eb12ab82")]
