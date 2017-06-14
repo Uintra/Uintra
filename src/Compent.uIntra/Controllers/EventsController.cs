e4ce6c6115fec1e33a6abc28d541cf9f5732e948
aleksandr.chekanyn <alc@compent.net>
2017-06-14 14:58:24 +0300

    Update packages.

--- src/Compent.uIntra/Controllers/EventsController.cs
+++ src/Compent.uIntra/Controllers/EventsController.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using AutoMapper;
 using Compent.uIntra.Core.Events;
@@ -8,6 +10,7 @@ using uIntra.Core.Grid;
 using uIntra.Core.Media;
 using uIntra.Core.User;
 using uIntra.Events;
+using uIntra.Events.Core.Models;
 using uIntra.Events.Web;
 using uIntra.Notification;
 using uIntra.Notification.Configuration;
@@ -25,6 +28,8 @@ namespace Compent.uIntra.Controllers
 
         private readonly IEventsService<Event> _eventsService;
         private readonly IReminderService _reminderService;
+        private readonly IGridHelper _gridHelper;
+        private readonly IIntranetUserService<IIntranetUser> _intranetUserService;
 
         public EventsController(IEventsService<Event> eventsService,
             IMediaHelper mediaHelper,
@@ -36,6 +41,33 @@ namespace Compent.uIntra.Controllers
         {
             _eventsService = eventsService;
             _reminderService = reminderService;
+            _gridHelper = gridHelper;
+            _intranetUserService = intranetUserService;
+        }
+
+        public override ActionResult ComingEvents()
+        {
+            var eventsAmount = _gridHelper.GetContentProperty<int>(CurrentPage, "custom.ComingEvents", "eventsAmount");
+            var title = _gridHelper.GetContentProperty<string>(CurrentPage, "custom.ComingEvents", "displayTitle");
+            var currentDate = DateTime.UtcNow;
+
+            var events = _eventsService.GetComingEvents(currentDate).Take(eventsAmount);
+            var eventsList = events as IList<Event> ?? events.ToList();
+            var creatorsDictionary = _intranetUserService.GetMany(eventsList.Select(e => e.CreatorId)).ToDictionary(c => c.Id);
+            var comingEvents = new List<ComingEventViewModel>();
+            foreach (var e in eventsList)
+            {
+                var viewModel = e.Map<ComingEventViewModel>();
+                viewModel.Creator = creatorsDictionary[e.CreatorId];
+                comingEvents.Add(viewModel);
+            }
+
+            var model = new ComingEventsPanelViewModel()
+            {
+                Title = title,
+                Events = comingEvents
+            };
+            return PartialView(ComingEventsViewPath, model);
         }
 
         public ActionResult CentralFeedItem(ICentralFeedItem item)
