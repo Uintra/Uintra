f9c53ac4155836c6e0764da7e73b9a21b7a01fed
Julia Shulumba <jsh@compent.net>
2017-06-20 14:44:23 +0300

    Mobile. Home page. Add design to bulletin, filters and right menu

--- src/uIntra.Bulletins/Properties/AssemblyInfo.cs
+++ src/uIntra.Bulletins/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.Bulletins")]
 [assembly: AssemblyDescription("Bulletins activity package")]
 
-[assembly: AssemblyVersion("0.0.1.17")]
+[assembly: AssemblyVersion("0.0.1.18")]
 [assembly: ComVisible(false)]
 [assembly: Guid("9cdb39aa-aa5a-4691-b942-8f2ee3297e64")]
