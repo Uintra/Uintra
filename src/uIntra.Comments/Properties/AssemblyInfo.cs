f9aecbf676a6a6e319cb13335248c4d4841a62c6
Julia Shulumba <jsh@compent.net>
2017-06-20 16:30:57 +0300

    UC-419 Comments icon. Design issue

--- src/uIntra.Comments/Properties/AssemblyInfo.cs
+++ src/uIntra.Comments/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.Comments")]
 [assembly: AssemblyDescription("Comments activity package")]
 
-[assembly: AssemblyVersion("1.1.1.8")]
+[assembly: AssemblyVersion("1.1.1.9")]
 [assembly: ComVisible(false)]
 [assembly: Guid("54e782d4-8298-4b78-8d7a-cde398521374")]
