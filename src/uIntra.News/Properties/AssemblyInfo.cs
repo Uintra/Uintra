b8c1ed9519c0dfb04e3b7b28a8a76766a0b6e0df
Maksim Didinov <mdi@compent.net>
2017-06-20 14:19:41 +0300

    Fix versions after merge

--- src/uIntra.News/Properties/AssemblyInfo.cs
+++ src/uIntra.News/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.News")]
 [assembly: AssemblyDescription("News activity package")]
 
-[assembly: AssemblyVersion("1.1.2.15")]
+[assembly: AssemblyVersion("1.1.2.16")]
 [assembly: ComVisible(false)]
 [assembly: Guid("53386df1-d6a7-484b-8b07-c4fddebf4b83")]
