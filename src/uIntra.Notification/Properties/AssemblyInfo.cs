7fd7063c6c62f9ec7f4706966f1d6fd63952a80e
Dmitry Bondar <dbo@compent.net>
2017-06-21 12:29:05 +0300

    Fix view.

--- src/uIntra.Notification/Properties/AssemblyInfo.cs
+++ src/uIntra.Notification/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.Notification")]
 [assembly: AssemblyDescription("Notification activity package")]
 
-[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyVersion("1.1.2.1")]
 [assembly: ComVisible(false)]
 [assembly: Guid("774eba5a-679c-401d-b3f1-130562785c60")]
