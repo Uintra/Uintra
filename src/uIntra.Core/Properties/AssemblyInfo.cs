736667d62a4fc30b40df5bffeea56326cbcd5955
aleksandr.chekanyn <alc@compent.net>
2017-06-24 13:57:06 +0300

    Update packages versions: Users, Notification, Core

--- src/uIntra.Core/Properties/AssemblyInfo.cs
+++ src/uIntra.Core/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.Core")]
 [assembly: AssemblyDescription("Core activity package.")]
 
-[assembly: AssemblyVersion("1.1.12.12")]
+[assembly: AssemblyVersion("1.1.13.0")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("3f8c785a-48d0-41f5-9257-6f73afca41eb")]
