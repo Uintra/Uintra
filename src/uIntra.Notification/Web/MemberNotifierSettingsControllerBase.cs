5735e0698db8dbcb7bf2cf88d59612d94ba1c6ed
aleksandr.chekanyn <alc@compent.net>
2017-06-24 13:52:42 +0300

    Update Notification package.

---
+++ src/uIntra.Notification/Web/MemberNotifierSettingsControllerBase.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Http;
+using uIntra.Core.User;
+using uIntra.Notification;
+using uIntra.Notification.Configuration;
+using Umbraco.Web.WebApi;
+
+namespace Compent.uIntra.Controllers
+{
+    public class MemberNotifierSettingsController : UmbracoApiController
+    {
+        private readonly IMemberNotifiersSettingsService _memberNotifiersSettingsService;
+        private readonly IIntranetUserService<IIntranetUser> _intranetUserService;
+
+        public MemberNotifierSettingsController(
+            IMemberNotifiersSettingsService memberNotifiersSettingsService,
+            IIntranetUserService<IIntranetUser> intranetUserService)
+        {
+            _memberNotifiersSettingsService = memberNotifiersSettingsService;
+            _intranetUserService = intranetUserService;
+        }
+
+        [HttpPost]
+
+        public void Update(NotifierTypeEnum type, bool isEnabled)
+        {
+            var currentUser = _intranetUserService.GetCurrentUser();
+            _memberNotifiersSettingsService.SetForMember(currentUser.Id, type, isEnabled);
+        }
+    }
+}
