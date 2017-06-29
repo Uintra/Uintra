9a728f9ea8cd06d39054894a0d167d62f50884d1
Julia Shulumba <jsh@compent.net>
2017-06-29 16:24:13 +0300

    UC-454 Refactor ccs Core

--- src/Compent.uIntra/Content/head_css.js
+++ src/Compent.uIntra/Content/head_css.js
@@ -2,4 +2,7 @@
 require('photoswipe/dist/photoswipe.css');
 require('photoswipe/dist/default-skin/default-skin.css');
 require('./../App_Plugins/Core/Content/styles/Core.css');
+require('./../App_Plugins/Core/Content/styles/libs.css');
+require('./../App_Plugins/Core/Content/styles/forms.css');
 require('./css/styles.css');
+require('./../App_Plugins/Core/Content/styles/custom.css');
