e8ace5f01cc80895594616476d6df6f0385c4f65
Julia Shulumba <jsh@compent.net>
2017-06-29 11:50:27 +0300

    UC-481 Save state of sort order of the My links Navigation

--- src/uIntra.Navigation/App_Plugins/Navigation/MyLinks/myLinks.js
+++ src/uIntra.Navigation/App_Plugins/Navigation/MyLinks/myLinks.js
@@ -13,8 +13,26 @@ var controller = {
         var className = '_disabled';
 
         var sortable = Sortable.create(container, {
-            onUpdate: function (evt) {
+            group: "localStorage-example",
+            store: {
+                /**
+                 * Get the order of elements. Called once during initialization.
+                 * @param   {Sortable}  sortable
+                 * @returns {Array}
+                 */
+                get: function (sortable) {
+                    var order = localStorage.getItem(sortable.options.group.name);
+                    return order ? order.split('|') : [];
+                },
 
+                /**
+                 * Save the order of elements. Called onEnd (when the item is dropped).
+                 * @param {Sortable}  sortable
+                 */
+                set: function (sortable) {
+                    var order = sortable.toArray();
+                    localStorage.setItem(sortable.options.group.name, order.join('|'));
+                }
             }
         });
 
