54c189940e2deb399f6b9a9e342089f4ac8cc24b
Dmitry Bondar <dbo@compent.net>
2017-06-30 10:33:52 +0300

    Update Search assembly version.

--- src/uIntra.Search/Properties/AssemblyInfo.cs
+++ src/uIntra.Search/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.Search")]
 [assembly: AssemblyDescription("Search package")]
 
-[assembly: AssemblyVersion("0.0.2.2")]
+[assembly: AssemblyVersion("0.0.2.3")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4d9f910e-24e0-43b3-bccb-fef527d5ff2f")]
