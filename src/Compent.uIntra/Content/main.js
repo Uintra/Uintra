1a77b3f9424bc39a3f2d38891f23a36188a9f37d
Vasil Vasilik <vva@compent.net>
2017-07-03 13:05:05 +0300

    UC-455 - Refactor JS. ConfirmOnBeforeUnload imports by Core

--- src/Compent.uIntra/Content/main.js
+++ src/Compent.uIntra/Content/main.js
@@ -6,7 +6,6 @@ require('./../App_Plugins/Events/events');
 require('./../App_Plugins/Likes/likes');
 require('./../App_Plugins/Panels/ContentPanel/contentPanel');
 require('./../App_Plugins/Bulletins/bulletins');
-require('../App_Plugins/Core/Content/scripts/ConfirmOnBeforeUnload');
 
 import initCore from './../App_Plugins/Core/Content/Scripts/Core';
 import initSearch from './../App_Plugins/Search/search';
