24243e5ace918d4134c36abb2b0fba48f431b36f
Vasil Vasilik <vva@compent.net>
2017-07-03 12:16:10 +0300

    UC-455 - Refactor JS. Tagging.Apply

--- src/Compent.uIntra/App_Plugins/Tagging/tagsView.js
+++ src/Compent.uIntra/App_Plugins/Tagging/tagsView.js
@@ -1,10 +1,8 @@
-﻿import appInitializer from "./../Core/Content/scripts/AppInitializer";
-
-require("./taggle.css");
+﻿require("./taggle.css");
 require("./tags.css");
 
 var controller = {
     init: function () {}
-}
+};
 
 export default controller;
