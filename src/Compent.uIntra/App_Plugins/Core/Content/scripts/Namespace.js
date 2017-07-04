1525f97b82eb5bf2db3d0ac84489c7a64e9fb087
Vasil Vasilik <vva@compent.net>
2017-07-04 14:05:24 +0300

    UC-455 - Refactor JS. Core. Use namespace. Apply

---
+++ src/Compent.uIntra/App_Plugins/Core/Content/scripts/Namespace.js
@@ -0,0 +1,24 @@
+﻿const uIntra = window.uIntra = {
+    methods: {
+        add: function (name, method) {
+            this[name] = method;
+            this.list.push({
+                [name]: method
+            })
+        },
+
+        list: []
+    },
+    variables: {
+        add: function (name, value) {
+            this[name] = value;
+            this.list.push({
+                [name]: value
+            })
+        },
+
+        list: []
+    }
+};
+
+export {uIntra};
