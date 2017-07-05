92f12b77b9a71e79c74ce9d92cc98ade1fb37938
Dmitry Bondar <dbo@compent.net>
2017-07-05 14:20:22 +0300

    UC-496: Incorrect Search result

--- src/uIntra.Search/Properties/AssemblyInfo.cs
+++ src/uIntra.Search/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.Search")]
 [assembly: AssemblyDescription("Search package")]
 
-[assembly: AssemblyVersion("0.0.3.6")]
+[assembly: AssemblyVersion("0.0.3.7")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4d9f910e-24e0-43b3-bccb-fef527d5ff2f")]
