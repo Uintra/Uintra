18f0ce1c343dfa801e17c88ecdf10cc1c15e8fb2
Julia Shulumba <jsh@compent.net>
2017-07-06 15:02:21 +0300

    UC-531 iPhone. Service bar. Open windows overlap each other

--- src/uIntra.Search/Properties/AssemblyInfo.cs
+++ src/uIntra.Search/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.Search")]
 [assembly: AssemblyDescription("Search package")]
 
-[assembly: AssemblyVersion("0.0.3.10")]
+[assembly: AssemblyVersion("0.0.3.11")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4d9f910e-24e0-43b3-bccb-fef527d5ff2f")]
