ad397d6dbe29eabe67b1d26ce399b3958fe7342e
Vasil Vasilik <vva@compent.net>
2017-07-06 11:09:23 +0300

    UC-489 - Send empty string if no user image

--- src/uIntra.Users/Properties/AssemblyInfo.cs
+++ src/uIntra.Users/Properties/AssemblyInfo.cs
@@ -5,6 +5,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("uIntra.Users")]
 [assembly: AssemblyConfiguration("")]
 
-[assembly: AssemblyVersion("1.1.3.3")]
+[assembly: AssemblyVersion("1.1.3.4")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3e868f15-ac47-4962-a6a5-c9084752dde9")]
