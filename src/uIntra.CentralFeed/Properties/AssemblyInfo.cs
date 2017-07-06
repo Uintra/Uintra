42819509a0c3c05717b12802cae686cfc83eda42
Vasil Vasilik <vva@compent.net>
2017-07-06 13:45:40 +0300

    UC-509 - Central feed. Filters. It should be possible to expand/collapse filters. Style fix

--- src/uIntra.CentralFeed/Properties/AssemblyInfo.cs
+++ src/uIntra.CentralFeed/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.CentralFeed")]
 [assembly: AssemblyDescription("Central feed activity package.")]
 
-[assembly: AssemblyVersion("1.1.7.1")]
+[assembly: AssemblyVersion("1.1.7.2")]
 [assembly: ComVisible(false)]
 [assembly: Guid("0b1998e4-8fa0-4328-9cd2-bbba92fc4e98")]
