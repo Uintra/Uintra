615a5e1cc11af78626d5bd0d81e3b8998dee1193
Julia Shulumba <jsh@compent.net>
2017-07-12 14:50:04 +0300

    Fix design

--- src/uIntra.Bulletins/Properties/AssemblyInfo.cs
+++ src/uIntra.Bulletins/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.Bulletins")]
 [assembly: AssemblyDescription("Bulletins activity package")]
 
-[assembly: AssemblyVersion("0.0.6.0")]
+[assembly: AssemblyVersion("0.0.6.1")]
 [assembly: ComVisible(false)]
 [assembly: Guid("9cdb39aa-aa5a-4691-b942-8f2ee3297e64")]
