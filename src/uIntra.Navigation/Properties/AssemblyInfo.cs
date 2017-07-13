46a73541e57482fae26eb2c4d53fcd53859940a3
Julia Shulumba <jsh@compent.net>
2017-07-13 15:39:40 +0300

    Fix design for small screens.

--- src/uIntra.Navigation/Properties/AssemblyInfo.cs
+++ src/uIntra.Navigation/Properties/AssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.Navigation")]
 [assembly: AssemblyDescription("Left Navigation, Sub Navigation Top Navigation")]
 
-[assembly: AssemblyVersion("1.1.5.7")]
+[assembly: AssemblyVersion("1.1.5.8")]
 [assembly: ComVisible(false)]
