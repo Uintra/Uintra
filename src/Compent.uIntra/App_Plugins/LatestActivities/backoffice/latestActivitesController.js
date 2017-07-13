c263a5bceb866d8ee82d1988736ecb4da7482cfe
m.kilianovskyi <mki@compent.net>
2017-07-13 17:00:05 +0300

    [FTFAINTRA-339] : Created panel; Added view, ng-controller, forms and validation

---
+++ src/Compent.uIntra/App_Plugins/LatestActivities/backoffice/latestActivitesController.js
@@ -0,0 +1,56 @@
+﻿(function () {
+
+
+
+    let controller = function ($scope) {
+        $scope.overlay = {
+            show: false,
+            title: "Latest activities",
+            view: "/App_Plugins/LatestActivities/backoffice/latest-activities.overlay.html",
+            close: function () {
+                $scope.overlay.show = false;
+                $scope.control.value = $scope.backupModel;
+            },
+            submit: function () {
+                if (isValidModel($scope.control.value)) {
+                    $scope.overlay.show = false;
+                    $scope.control.validationMessage = null;
+                }
+            }
+        };
+
+        $scope.open = function () {
+            $scope.overlay.show = true;
+            $scope.backupModel = angular.copy($scope.control.value);
+        };
+
+        function isValidModel(model) {
+
+            if (!model) { return false;}
+                
+
+            if (!isValidTitle(model)) {
+                $scope.control.validationMessage = "Title is required";
+                return false;
+            }
+
+            if (!isValidNumberOfActivities(model)) {
+                $scope.control.validationMessage = "Number of activities is invalid";
+                return false;
+            }
+
+            return true;
+
+            function isValidNumberOfActivities(model) {
+                return !isNaN(model.nubmerOfActivities) && parseInt(model.nubmerOfActivities) > 0; 
+            }
+
+            function isValidTitle(model) {
+                return model.displayTitle && model.displayTitle.length > 0;
+            }
+        }
+
+    };
+    controller.$inject = ["$scope"];
+    angular.module('umbraco').controller('latestActivititesController', controller);
+})();
