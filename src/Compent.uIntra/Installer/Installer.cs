df4bf741d79959ce38da74cc22b65aa4625cd0fc
Dmitry Bondar <dbo@compent.net>
2017-07-14 14:22:26 +0300

    Update Core and update UIntraConfigurationStatus

--- src/Compent.uIntra/Installer/Installer.cs
+++ src/Compent.uIntra/Installer/Installer.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Configuration;
+using System.Web.Configuration;
 using Compent.uIntra.SetupMigrations;
 using uIntra.Bulletins.Installer;
+using uIntra.Core.Extentions;
 using uIntra.Core.Installer;
 using uIntra.Events.Installer;
 using uIntra.Navigation.Installer;
@@ -14,9 +13,21 @@ using Umbraco.Core;
 
 namespace Compent.uIntra.Installer
 {
-    public class Installer: ApplicationEventHandler
+    public class Installer : ApplicationEventHandler
     {
+        private const string UIntraVersion = "1.0.0";
+
         protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            var uIntraConfigurationStatus = ConfigurationManager.AppSettings[CoreInstallationConstants.AppSettingKey.UIntraConfigurationStatus];
+            if (uIntraConfigurationStatus.IsNullOrEmpty())
+            {
+                InitMigration();
+                UpdateUIntraConfigurationStatus();
+            }
+        }
+
+        private void InitMigration()
         {
             var installer = new IntranetInstaller();
             var umbracoContentMigration = new UmbracoContentMigration();
@@ -46,7 +57,13 @@ namespace Compent.uIntra.Installer
 
             CoreInstallationStep.InheritCompositionForPage(EventsInstallationConstants.DocumentTypeAliases.EventsOverviewPage, homeNav);
             CoreInstallationStep.InheritCompositionForPage(EventsInstallationConstants.DocumentTypeAliases.EventsOverviewPage, nav);
+        }
 
+        private void UpdateUIntraConfigurationStatus()
+        {
+            var webConfigApp = WebConfigurationManager.OpenWebConfiguration("~");
+            webConfigApp.AppSettings.Settings[CoreInstallationConstants.AppSettingKey.UIntraConfigurationStatus].Value = UIntraVersion;
+            webConfigApp.Save();
         }
     }
 }
