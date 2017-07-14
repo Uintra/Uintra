486e92d5a2ef922b42739fb705c5877d33c1d121
Dmitry Bondar <dbo@compent.net>
2017-07-14 16:14:03 +0300

    merge

--- src/uIntra/Installer/Installer.cs
+++ src/uIntra/Installer/Installer.cs
@@ -1,9 +1,8 @@
-﻿using System.IO;
-using System.Linq;
-using System.Web.Hosting;
+﻿using System.Configuration;
+using System.Web.Configuration;
 using Compent.uIntra.SetupMigrations;
-using Microsoft.Build.Construction;
 using uIntra.Bulletins.Installer;
+using uIntra.Core.Extentions;
 using uIntra.Core.Installer;
 using uIntra.Events.Installer;
 using uIntra.Navigation.Installer;
@@ -16,7 +15,19 @@ namespace Compent.uIntra.Installer
 {
     public class Installer : ApplicationEventHandler
     {
+        private const string UIntraVersion = "1.0.0";
+
         protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            var uIntraConfigurationStatus = ConfigurationManager.AppSettings[CoreInstallationConstants.AppSettingKey.UIntraConfigurationStatus];
+            if (uIntraConfigurationStatus.IsNullOrEmpty())
+            {
+                InitMigration();
+                UpdateUIntraConfigurationStatus();
+            }
+        }
+
+        private void InitMigration()
         {
             var installer = new IntranetInstaller();
             var umbracoContentMigration = new UmbracoContentMigration();
@@ -24,7 +35,6 @@ namespace Compent.uIntra.Installer
             installer.Install();
             InheritNavigationCompositions();
             umbracoContentMigration.Init();
-            //AddWebpackBuildTarget();
         }
 
         private void InheritNavigationCompositions()
@@ -33,50 +43,27 @@ namespace Compent.uIntra.Installer
             var nav = NavigationInstallationConstants.DocumentTypeAliases.NavigationComposition;
 
             CoreInstallationStep.InheritCompositionForPage(CoreInstallationConstants.DocumentTypeAliases.HomePage, nav);
-            CoreInstallationStep.InheritCompositionForPage(CoreInstallationConstants.DocumentTypeAliases.ContentPage,
-                nav);
+            CoreInstallationStep.InheritCompositionForPage(CoreInstallationConstants.DocumentTypeAliases.ContentPage, nav);
             CoreInstallationStep.InheritCompositionForPage(CoreInstallationConstants.DocumentTypeAliases.ErrorPage, nav);
 
-            CoreInstallationStep.InheritCompositionForPage(
-                SearchInstallationConstants.DocumentTypeAliases.SearchResultPage, nav);
-            CoreInstallationStep.InheritCompositionForPage(
-                NotificationInstallationConstants.DocumentTypeAliases.NotificationPage, nav);
+            CoreInstallationStep.InheritCompositionForPage(SearchInstallationConstants.DocumentTypeAliases.SearchResultPage, nav);
+            CoreInstallationStep.InheritCompositionForPage(NotificationInstallationConstants.DocumentTypeAliases.NotificationPage, nav);
 
-            CoreInstallationStep.InheritCompositionForPage(
-                BulletinsInstallationConstants.DocumentTypeAliases.BulletinsOverviewPage, homeNav);
-            CoreInstallationStep.InheritCompositionForPage(
-                BulletinsInstallationConstants.DocumentTypeAliases.BulletinsOverviewPage, nav);
+            CoreInstallationStep.InheritCompositionForPage(BulletinsInstallationConstants.DocumentTypeAliases.BulletinsOverviewPage, homeNav);
+            CoreInstallationStep.InheritCompositionForPage(BulletinsInstallationConstants.DocumentTypeAliases.BulletinsOverviewPage, nav);
 
-            CoreInstallationStep.InheritCompositionForPage(
-                NewsInstallationConstants.DocumentTypeAliases.NewsOverviewPage, homeNav);
-            CoreInstallationStep.InheritCompositionForPage(
-                NewsInstallationConstants.DocumentTypeAliases.NewsOverviewPage, nav);
+            CoreInstallationStep.InheritCompositionForPage(NewsInstallationConstants.DocumentTypeAliases.NewsOverviewPage, homeNav);
+            CoreInstallationStep.InheritCompositionForPage(NewsInstallationConstants.DocumentTypeAliases.NewsOverviewPage, nav);
 
-            CoreInstallationStep.InheritCompositionForPage(
-                EventsInstallationConstants.DocumentTypeAliases.EventsOverviewPage, homeNav);
-            CoreInstallationStep.InheritCompositionForPage(
-                EventsInstallationConstants.DocumentTypeAliases.EventsOverviewPage, nav);
-
+            CoreInstallationStep.InheritCompositionForPage(EventsInstallationConstants.DocumentTypeAliases.EventsOverviewPage, homeNav);
+            CoreInstallationStep.InheritCompositionForPage(EventsInstallationConstants.DocumentTypeAliases.EventsOverviewPage, nav);
         }
 
-        private void AddWebpackBuildTarget()
+        private void UpdateUIntraConfigurationStatus()
         {
-            var projectDirectoryPath = HostingEnvironment.MapPath("~");
-            var fileProjectPath = Directory.GetFiles(projectDirectoryPath, "*.csproj")[0];
-
-            var projectFile = ProjectRootElement.Open(fileProjectPath);
-            if (projectFile.Targets.Any(t => t.Name.Equals("WebpackBuild")))
-            {
-                return;
-            }
-
-            var webpackTarget = projectFile.AddTarget("WebpackBuild");
-            webpackTarget.AfterTargets = "AfterBuild";
-
-            var webpackTask = webpackTarget.AddTask("Exec");
-            webpackTask.SetParameter("Command", "webpack");
-
-            projectFile.Save();
+            var webConfigApp = WebConfigurationManager.OpenWebConfiguration("~");
+            webConfigApp.AppSettings.Settings[CoreInstallationConstants.AppSettingKey.UIntraConfigurationStatus].Value = UIntraVersion;
+            webConfigApp.Save();
         }
     }
 }
