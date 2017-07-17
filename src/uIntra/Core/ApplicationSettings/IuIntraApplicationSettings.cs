a0153e179f385b6bebd84d46bf8e2b2d0c8f2292
Maksim Didinov <mdi@compent.net>
2017-07-17 15:29:10 +0300

    uIntra. All content files now references to Compent.uIntra

--- src/uIntra/Core/ApplicationSettings/IuIntraApplicationSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace uIntra.Core.ApplicationSettings
-{
-    public interface IuIntraApplicationSettings : IApplicationSettings
-    {
-        IEnumerable<string> NotWebMasterRoleDisabledDocumentTypes { get; }
-    }
-}
--- src/uIntra/Core/Helpers/IUmbracoContentHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿using Umbraco.Core.Models;
-
-namespace uIntra.Core.Helpers
-{
-    public interface IUmbracoContentHelper
-    {
-        bool IsContentAvailable(IPublishedContent publishedContent);
-    }
-}
