5076fe05b1f75691ad3fc743c954cac2e47dfa0e
Dmitry Bondar <dbo@compent.net>
2017-07-18 17:03:49 +0300

    UC-592: Rename properties names.

--- src/uIntra.Navigation/Properties/AssemblyInfo.cs
+++ src/uIntra.Navigation/Properties/AssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.Navigation")]
 [assembly: AssemblyDescription("Left Navigation, Sub Navigation Top Navigation")]
 
-[assembly: AssemblyVersion("1.1.5.12")]
+[assembly: AssemblyVersion("1.1.5.13")]
 [assembly: ComVisible(false)]
