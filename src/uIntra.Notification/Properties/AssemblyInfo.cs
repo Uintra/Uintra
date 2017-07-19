1851781f017f4ffbfa6d1a52d4e2bd8e6f6bb904
Vasil Vasilik <vva@compent.net>
2017-07-19 12:47:31 +0300

    UC-594 - Notification page. Fix align

--- src/uIntra.Notification/Properties/AssemblyInfo.cs
+++ src/uIntra.Notification/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.Notification")]
 [assembly: AssemblyDescription("Notification activity package")]
 
-[assembly: AssemblyVersion("1.1.7.17")]
+[assembly: AssemblyVersion("1.1.7.18")]
 [assembly: ComVisible(false)]
 [assembly: Guid("774eba5a-679c-401d-b3f1-130562785c60")]
