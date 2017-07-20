a2b758c94eba0995cda843364dd697944f511f55
m.kilianovskyi <mki@compent.net>
2017-07-20 17:40:36 +0300

    Updating Core package : disabled MaximumUsersCountException

--- src/uIntra.Core/Core/Handlers/MaximumUsersCountVerificator.cs
+++ src/uIntra.Core/Core/Handlers/MaximumUsersCountVerificator.cs
@@ -12,7 +12,7 @@ namespace uIntra.Core.Handlers
             var memberService = ApplicationContext.Current.Services.MemberService;
             var membersCount = memberService.Count();
 
-            if (membersCount > MaxIntranetUsersCount)
+            if (false && membersCount > MaxIntranetUsersCount)
             {
                 throw new MaximumUsersCountException($"This is a beta version of uIntra. As result, this is only possible to have up to {MaxIntranetUsersCount} members." +
                                     $" If you are interested in more than {MaxIntranetUsersCount} members, please don't hesitate and contact us by this email vma@compent.net");
