4ad28794a9c310697e465ade42bbcb1259913dec
Dmitry Bondar <dbo@compent.net>
2017-07-21 13:37:45 +0300

    UC-612: Remove navigation tab from Activities

--- src/Compent.uIntra/Installer/Installer.cs
+++ src/Compent.uIntra/Installer/Installer.cs
@@ -1,11 +1,8 @@
 ﻿using System.Configuration;
 using System.Web.Configuration;
-using uIntra.Bulletins.Installer;
 using uIntra.Core.Extentions;
 using uIntra.Core.Installer;
-using uIntra.Events.Installer;
 using uIntra.Navigation.Installer;
-using uIntra.News.Installer;
 using uIntra.Notification.Installer;
 using uIntra.Search.Installer;
 using uIntra.Users.Installer;
@@ -39,7 +36,6 @@ namespace Compent.uIntra.Installer
 
         private void InheritNavigationCompositions()
         {
-            var homeNav = NavigationInstallationConstants.DocumentTypeAliases.HomeNavigationComposition;
             var nav = NavigationInstallationConstants.DocumentTypeAliases.NavigationComposition;
 
             CoreInstallationStep.InheritCompositionForPage(CoreInstallationConstants.DocumentTypeAliases.HomePage, nav);
@@ -48,15 +44,6 @@ namespace Compent.uIntra.Installer
 
             CoreInstallationStep.InheritCompositionForPage(SearchInstallationConstants.DocumentTypeAliases.SearchResultPage, nav);
             CoreInstallationStep.InheritCompositionForPage(NotificationInstallationConstants.DocumentTypeAliases.NotificationPage, nav);
-
-            CoreInstallationStep.InheritCompositionForPage(BulletinsInstallationConstants.DocumentTypeAliases.BulletinsOverviewPage, homeNav);
-            CoreInstallationStep.InheritCompositionForPage(BulletinsInstallationConstants.DocumentTypeAliases.BulletinsOverviewPage, nav);
-
-            CoreInstallationStep.InheritCompositionForPage(NewsInstallationConstants.DocumentTypeAliases.NewsOverviewPage, homeNav);
-            CoreInstallationStep.InheritCompositionForPage(NewsInstallationConstants.DocumentTypeAliases.NewsOverviewPage, nav);
-
-            CoreInstallationStep.InheritCompositionForPage(EventsInstallationConstants.DocumentTypeAliases.EventsOverviewPage, homeNav);
-            CoreInstallationStep.InheritCompositionForPage(EventsInstallationConstants.DocumentTypeAliases.EventsOverviewPage, nav);
 
             CoreInstallationStep.InheritCompositionForPage(UsersInstallationConstants.DocumentTypeAliases.ProfilePage, nav);
             CoreInstallationStep.InheritCompositionForPage(UsersInstallationConstants.DocumentTypeAliases.ProfileEditPage, nav);
