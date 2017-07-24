0c563b8ef537dea279f70838a0e2f948d32e0684
Sergey Prokhorov <spr@compent.net>
2017-07-24 17:49:25 +0300

    UC-642: Fix NotificationLIst

--- src/uIntra.Notification/Properties/AssemblyInfo.cs
+++ src/uIntra.Notification/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.Notification")]
 [assembly: AssemblyDescription("Notification activity package")]
 
-[assembly: AssemblyVersion("1.1.7.26")]
+[assembly: AssemblyVersion("1.1.7.27")]
 [assembly: ComVisible(false)]
 [assembly: Guid("774eba5a-679c-401d-b3f1-130562785c60")]
