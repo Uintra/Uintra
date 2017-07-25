9472aec3723ea74c218df26a72a0210824a24371
Dmitry Bondar <dbo@compent.net>
2017-07-25 13:55:03 +0300

    Update Notification package.

--- src/Compent.uIntra/Controllers/NotificationController.cs
+++ src/Compent.uIntra/Controllers/NotificationController.cs
@@ -1,16 +1,19 @@
 ﻿using uIntra.Core.User;
 using uIntra.Notification;
 using uIntra.Notification.Web;
-using uIntra.Users;
 
 namespace Compent.uIntra.Controllers
 {
-    public class NotificationController: NotificationControllerBase
+    public class NotificationController : NotificationControllerBase
     {
         protected override int ItemsPerPage { get; } = 10;
 
-        public NotificationController(IUiNotifierService uiNotifierService, IIntranetUserService<IntranetUser> intranetUserService, INotificationHelper notificationHelper) 
-            : base(uiNotifierService, intranetUserService, notificationHelper)
+        public NotificationController(
+            IUiNotifierService uiNotifierService,
+            IIntranetUserService<IIntranetUser> intranetUserService,
+            INotificationHelper notificationHelper,
+            IIntranetUserContentHelper intranetUserContentHelper)
+            : base(uiNotifierService, intranetUserService, notificationHelper, intranetUserContentHelper)
         {
         }
     }
