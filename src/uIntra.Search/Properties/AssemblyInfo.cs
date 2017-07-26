9cef6abce33c7143a2d3eff40148862be3461eaf
Vasil Vasilik <vva@compent.net>
2017-07-26 16:28:44 +0300

    FTFAINTRA-420 - Search page. "Load icon" issue

--- src/uIntra.Search/Properties/AssemblyInfo.cs
+++ src/uIntra.Search/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Search package")]
 
 [assembly: AssemblyVersion("0.0.4.9")]
-[assembly: AssemblyInformationalVersion("0.0.4.10-v1")]
+[assembly: AssemblyInformationalVersion("0.0.4.10-v2")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("4d9f910e-24e0-43b3-bccb-fef527d5ff2f")]
