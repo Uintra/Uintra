364442b56e4dac9404d9ec73a14a4f6166641409
m.kilianovskyi <mki@compent.net>
2017-07-26 12:58:50 +0300

    FTFAINTRA-382 : Updating Tagging package

--- src/uIntra.Tagging/Properties/AssemblyInfo.cs
+++ src/uIntra.Tagging/Properties/AssemblyInfo.cs
@@ -5,6 +5,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Tagging activity package")]
 
 [assembly: AssemblyVersion("1.1.3.6")]
-[assembly: AssemblyInformationalVersion("1.1.3.6-v2")]
+[assembly: AssemblyInformationalVersion("1.1.3.6-v3")]
 [assembly: ComVisible(false)]
 [assembly: Guid("2167c278-86ed-451b-8d2c-01774532b4e8")]
