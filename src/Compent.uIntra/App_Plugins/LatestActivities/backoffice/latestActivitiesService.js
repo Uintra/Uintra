6ba385842b5d62333b7d6ffbbf49e62b0613599f
m.kilianovskyi <mki@compent.net>
2017-07-28 13:48:07 +0300

    FTFAINTRA-490 : added logic for neat moving "All" option to the end

--- src/Compent.uIntra/App_Plugins/LatestActivities/backoffice/latestActivitiesService.js
+++ src/Compent.uIntra/App_Plugins/LatestActivities/backoffice/latestActivitiesService.js
@@ -1,19 +1,42 @@
 ﻿(function (angular) {
     'use strict';
+
+     function findIndex (array, predicate) {
+         for (var i = 0; i < array.length; i++)
+             if (predicate(array[i])) return i;
+        return -1;
+    };
+
+    function moveElementToTheEnd(data, i) {
+        var el = data[i];
+        data.splice(i, 1);
+        data.push(el);
+        return data;
+    };
+
     var service = function (centralFeedService, $q) {
         var self = this;
 
         self.getActivityTypes = function () {
             var deferred = $q.defer();
 
-            centralFeedService.getActivityTypes().then(function (data) {
-                deferred.resolve([]);
+            centralFeedService.getActivityTypes().then(function (response) {
+                var result = moveAllTabTypeToEndIfExists(response.data);
+                deferred.resolve(result);
             }, function (err) {
                 deferred.reject(err);
             });
 
             return deferred.promise;
-        }
+        };
+
+        function moveAllTabTypeToEndIfExists(data) {
+            var i = findIndex(data, function (el) {
+                return el.Name == "All";
+            });
+
+            return i > -1 ? moveElementToTheEnd(data, i) : data;
+        };
     }
 
     service.$inject = ["centralFeedService", "$q"];
