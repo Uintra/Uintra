66f3916191d2ac6dde985aad8b9bf398bc7216f4
m.kilianovskyi <mki@compent.net>
2017-07-31 12:18:43 +0300

    FTFAINTRA-473 : Updating Bulletins, News and Events packages

--- src/uIntra.Events/Properties/AssemblyInfo.cs
+++ src/uIntra.Events/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Events activity package")]
 
 [assembly: AssemblyVersion("1.2.0.2")]
-[assembly: AssemblyInformationalVersion("1.2.0.4-v2")]
+[assembly: AssemblyInformationalVersion("1.2.0.4-v3")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("85aa3a0a-738e-4e0d-9448-ea34aa3a537c")]
