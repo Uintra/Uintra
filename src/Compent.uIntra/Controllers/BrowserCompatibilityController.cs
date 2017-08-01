a09d4111e54d0195fb2b34b9bc11d80140cca9c4
Denis Prodan <dpr@compent.net>
2017-08-01 11:51:57 +0300

    update core package and install browser compatibility

---
+++ src/Compent.uIntra/Controllers/BrowserCompatibilityController.cs
@@ -0,0 +1,13 @@
+﻿using uIntra.Core;
+using uIntra.Core.BrowserCompatibility;
+using uIntra.Core.Web;
+
+namespace Compent.uIntra.Controllers
+{
+    public class BrowserCompatibilityController : BrowserCompatibilityControllerBase
+    {        
+        public BrowserCompatibilityController(IBrowserCompatibilityConfigurationSection browserCompatibilityConfiguration, ICookieProvider cookieProvider) : base(browserCompatibilityConfiguration, cookieProvider)
+        {
+        }
+    }
+}
