188c5061b10f52a76942de92d2e44797ecd38b89
Denis Prodan <dpr@compent.net>
2017-08-01 11:45:04 +0300

    fix core package

--- src/uIntra.Core/Properties/AssemblyInfo.cs
+++ src/uIntra.Core/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Core activity package.")]
 
 [assembly: AssemblyVersion("1.1.21.7")]
-[assembly: AssemblyInformationalVersion("1.1.21.9-v12")]
+[assembly: AssemblyInformationalVersion("1.1.21.9-v13")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("3f8c785a-48d0-41f5-9257-6f73afca41eb")]
