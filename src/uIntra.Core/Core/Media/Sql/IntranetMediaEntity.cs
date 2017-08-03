0e34071f712218842e471ff2f45eb535fc4b6973
Alexander Poddubko <apo@compent.net>
2017-08-03 17:42:46 +0300

    UC-701 - Reimplement saving media for activity

---
+++ src/uIntra.Core/Core/Media/Sql/IntranetMediaEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using uIntra.Core.Persistence;
+
+namespace uIntra.Core.Media
+{
+    [uIntraTable("Media")]
+    public class IntranetMediaEntity : SqlEntity<Guid>
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public override Guid Id { get; set; }
+
+        public Guid EntityId { get; set; }
+
+        public string MediaIds { get; set; } = string.Empty;
+    }
+}
