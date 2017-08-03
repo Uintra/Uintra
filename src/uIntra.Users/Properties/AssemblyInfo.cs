216904d3f8414d3365d53011993f7851692ad324
m.kilianovskyi <mki@compent.net>
2017-08-03 19:10:45 +0300

    Updating Users package

--- src/uIntra.Users/Properties/AssemblyInfo.cs
+++ src/uIntra.Users/Properties/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 
 [assembly: AssemblyVersion("1.3.1.2")]
-[assembly: AssemblyInformationalVersion("1.3.1.2-v1")]
+[assembly: AssemblyInformationalVersion("1.3.1.2-v2")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3e868f15-ac47-4962-a6a5-c9084752dde9")]
