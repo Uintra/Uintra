2ffb9c6460bb8959a68f31b5ccdcec8174d9f264
Julia Shulumba <jsh@compent.net>
2017-08-03 16:12:52 +0300

    UC-709 Auto-suggest field is broken after scrolling

--- src/uIntra.Navigation/Properties/AssemblyInfo.cs
+++ src/uIntra.Navigation/Properties/AssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.Navigation")]
 [assembly: AssemblyDescription("Left Navigation, Sub Navigation Top Navigation")]
 
-[assembly: AssemblyVersion("1.1.7.0")]
+[assembly: AssemblyVersion("1.1.7.1")]
 [assembly: ComVisible(false)]
