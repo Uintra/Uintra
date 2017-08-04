3a6dbda51e3e02311309d79c19e11decb9faf9a8
Serhii Shepel <sse@compent.net>
2017-08-04 13:31:13 +0300

    Update uIntra.Core

---
+++ src/Compent.uIntra/App_Plugins/Core/Content/scripts/QuillExtender.js
@@ -0,0 +1,14 @@
+﻿var Quill = require('quill');
+
+const Link = Quill.import('formats/link');
+class linkType extends Link {
+    static create(value) {
+        let node = super.create(value);
+        value = this.sanitize(value);
+        if (!value.startsWith('https://') && !value.startsWith('http://')) {
+            node.setAttribute('href', 'http://' + value);
+        }
+        return node;
+    }
+}
+Quill.register(linkType);
