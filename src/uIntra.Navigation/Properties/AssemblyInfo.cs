c1c4958c92cc033bb493877592d9195f3830bf8f
Julia Shulumba <jsh@compent.net>
2017-08-07 22:42:48 +0300

    UC-680 Mobile. Right side column icon doesn`t open Right side panels

--- src/uIntra.Navigation/Properties/AssemblyInfo.cs
+++ src/uIntra.Navigation/Properties/AssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.Navigation")]
 [assembly: AssemblyDescription("Left Navigation, Sub Navigation Top Navigation")]
 
-[assembly: AssemblyVersion("1.1.8.3")]
+[assembly: AssemblyVersion("1.1.8.4")]
 [assembly: ComVisible(false)]
