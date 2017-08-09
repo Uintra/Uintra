b76155b6fed4c9d07cf6dac7f785da309fd67f92
Dmitry Bondar <dbo@compent.net>
2017-08-09 10:02:05 +0300

    fix

--- src/uIntra.Users/Core/Entities/IntranetUser.cs
+++ src/uIntra.Users/Core/Entities/IntranetUser.cs
@@ -5,9 +5,14 @@ namespace uIntra.Users
 {
     public class IntranetUser : IIntranetUser
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } 
         public int? UmbracoId { get; set; }
-        public virtual string DisplayedName => $"{FirstName} {LastName}";
+        public virtual string DisplayedName
+        {
+            get { return $"{FirstName} {LastName}"; }
+            set { throw new Exception("Can't set displayed name. You should change first and last name instead."); }
+        }
+
         public virtual string Photo { get; set; }
 
         public string FirstName { get; set; }
