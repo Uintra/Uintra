d31534dcb3cfb2fa7a0a7cd3a5f03d0532fd345f
aleksandr.chekanyn <alc@compent.net>
2017-08-11 16:30:14 +0300

    FTFAINTRA-544 Edit bulletin. Description should not be required field if pictures added

--- src/uIntra.Bulletins/Core/Models/BulletinEditModel.cs
+++ src/uIntra.Bulletins/Core/Models/BulletinEditModel.cs
@@ -2,6 +2,7 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.Web.Mvc;
 using uIntra.Core.Attributes;
+using uIntra.Core.Core.Attributes;
 using uIntra.Core.Media;
 using uIntra.Core.ModelBinders;
 
@@ -12,7 +13,7 @@ namespace uIntra.Bulletins
         [Required]
         public virtual Guid Id { get; set; }
 
-        [RequiredIfEmpty(OtherProperty = nameof(NewMedia)), AllowHtml, StringLength(2000)]
+        [RequiredIfAllEmpty(DependancyProperties = new[] { nameof(NewMedia), nameof(Media) }), AllowHtml, StringLength(2000)]
         public string Description { get; set; }
 
         [PropertyBinder(typeof(DateTimeBinder))]
@@ -23,7 +24,7 @@ namespace uIntra.Bulletins
         [Required]
         public int? MediaRootId { get; set; }
 
-        [RequiredIfEmpty(OtherProperty = nameof(Description))]
+        [RequiredIfAllEmpty(DependancyProperties = new[] { nameof(Description), nameof(Media) })]
         public string NewMedia { get; set; }
 
         public Guid CreatorId { get; set; }
