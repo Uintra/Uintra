b829f5dceab6945983b09666bf50af5c9b53eb1a
Maksim Didinov <mdi@compent.net>
2017-08-11 17:01:50 +0300

    Update packages

--- src/Compent.uIntra/App_Start/NinjectWebCommon.cs
+++ src/Compent.uIntra/App_Start/NinjectWebCommon.cs
@@ -256,6 +256,7 @@ namespace Compent.uIntra
             kernel.Bind<IIntranetRoleTypeProvider>().To<IntranetRoleTypeProvider>().InRequestScope();
 
             kernel.Bind<IDocumentTypeAliasProvider>().To<DocumentTypeProvider>().InRequestScope();
+            kernel.Bind<IImageHelper>().To<ImageHelper>().InRequestScope();
         }
 
         private static void RegisterEntityFrameworkServices(IKernel kernel)
