be06bedc42b301ae41ae79c1e686ceb5e8f29e6d
Maksim Didinov <mdi@compent.net>
2017-08-11 16:37:36 +0300

    Core. Added ImageHelper

---
+++ src/uIntra.Core/Core/Media/IImageHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uIntra.Core.Media
+{
+    public interface IImageHelper
+    {
+        MemoryStream NormalizeOrientation(Stream imageStream, string imageExtension, bool removeExifOrientationTag = true);
+        bool IsFileImage(byte[] fileBytes);
+    }
+}
