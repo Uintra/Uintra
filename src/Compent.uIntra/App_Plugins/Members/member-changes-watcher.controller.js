d55051cd0698cb88c4a26a894b7cd2c7f381010a
aleksandr.chekanyn <alc@compent.net>
2017-08-11 16:39:47 +0300

    Update packages.

---
+++ src/Compent.uIntra/App_Plugins/Members/member-changes-watcher.controller.js
@@ -0,0 +1,17 @@
+﻿(function (angular) {
+    'use strict';
+
+    var controller = function ($scope, $routeParams, memberService) {
+
+        function initFormSubmitEventListener() {
+            $scope.$on("formSubmitted", function (ev, args) {
+                var memberId = $routeParams.id;
+                memberService.memberChanged(memberId);
+            });
+        }
+
+        initFormSubmitEventListener();
+    };
+
+    angular.module("umbraco").controller('memberChangesWatcherController', ['$scope', '$routeParams', 'memberService', controller]);
+})(angular);
