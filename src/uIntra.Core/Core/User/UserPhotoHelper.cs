2fd8a6396e388e723812b4da7fe74bf0724752b1
Dmitry Bondar <dbo@compent.net>
2017-08-12 14:08:13 +0300

    UC-737: Empty avatar image in notifications if avatar is missing

---
+++ src/uIntra.Core/Core/User/UserPhotoHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using uIntra.Core.ApplicationSettings;
+using uIntra.Core.Extentions;
+
+namespace uIntra.Core.User
+{
+    public static class UserPhotoHelper
+    {
+        public static string GetUserPhotoOrDefaultAvatar(string userPhoto)
+        {
+            var applicationSettings = HttpContext.Current.GetService<IApplicationSettings>();
+            return !string.IsNullOrEmpty(userPhoto) ? userPhoto : applicationSettings.DefaultAvatarPath;
+        }
+    }
+}
