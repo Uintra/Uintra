ba81e720f886a4728d6ae75a79881b1ca106c7e7
Julia Shulumba <jsh@compent.net>
2017-08-15 17:19:38 +0300

    FTFAINTRA-553 Bulletin creation form. Erasing all tags should not collapse the form.

--- src/uIntra.Bulletins/Properties/AssemblyInfo.cs
+++ src/uIntra.Bulletins/Properties/AssemblyInfo.cs
@@ -5,6 +5,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Bulletins activity package")]
 
 [assembly: AssemblyVersion("0.1.2.3")]
-[assembly: AssemblyInformationalVersion("0.1.2.2-v5")]
+[assembly: AssemblyInformationalVersion("0.1.2.2-v6")]
 [assembly: ComVisible(false)]
 [assembly: Guid("9cdb39aa-aa5a-4691-b942-8f2ee3297e64")]
