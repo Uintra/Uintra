e8d25c40339cad59684f28d51bdc49fc752f6220
Julia Shulumba <jsh@compent.net>
2017-08-15 14:11:48 +0300

    Reimplement logic for create bulletin form

--- src/uIntra.Bulletins/Properties/AssemblyInfo.cs
+++ src/uIntra.Bulletins/Properties/AssemblyInfo.cs
@@ -5,6 +5,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Bulletins activity package")]
 
 [assembly: AssemblyVersion("0.1.2.3")]
-[assembly: AssemblyInformationalVersion("0.1.2.2-v3")]
+[assembly: AssemblyInformationalVersion("0.1.2.2-v4")]
 [assembly: ComVisible(false)]
 [assembly: Guid("9cdb39aa-aa5a-4691-b942-8f2ee3297e64")]
