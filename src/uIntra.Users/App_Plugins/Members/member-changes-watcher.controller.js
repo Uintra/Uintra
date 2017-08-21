64caab023c260334ed71d159a8063bd7ff143222
Dmitry Bondar <dbo@compent.net>
2017-08-21 14:03:53 +0300

    Reimplement memberChangesWatcherController

--- src/uIntra.Users/App_Plugins/Members/member-changes-watcher.controller.js
+++ src/uIntra.Users/App_Plugins/Members/member-changes-watcher.controller.js
@@ -1,17 +1,27 @@
 ﻿(function (angular) {
     'use strict';
 
-    var controller = function ($scope, $routeParams, memberService) {
+    var controller = function ($scope, memberService) {
 
         function initFormSubmitEventListener() {
             $scope.$on("formSubmitted", function (ev, args) {
-                var memberId = $routeParams.id;
-                memberService.memberChanged(memberId);
+                var memberId = args.scope.content.id !== 0 ? args.scope.content.key : null;
+
+                if (memberId) {
+                    memberService.memberChanged(memberId);
+                    return;
+                }
+
+                args.scope.$watch('page.saveButtonState', function () {
+                    if (args.scope.page.saveButtonState === "success") {
+                        memberService.memberChanged(args.scope.content.key);
+                    }
+                });
             });
         }
 
         initFormSubmitEventListener();
     };
 
-    angular.module("umbraco").controller('memberChangesWatcherController', ['$scope', '$routeParams', 'memberService', controller]);
+    angular.module("umbraco").controller('memberChangesWatcherController', ['$scope', 'memberService', controller]);
 })(angular);
