022289e1a9e66aa1341678e5f893920f411f94bb
m.kilianovskyi <mki@compent.net>
2017-08-23 20:46:04 +0300

    FTFAINTRA-412 : updating Core package

--- src/uIntra.Core/Properties/AssemblyInfo.cs
+++ src/uIntra.Core/Properties/AssemblyInfo.cs
@@ -5,6 +5,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Core activity package.")]
 
 [assembly: AssemblyVersion("1.1.27.2")]
-[assembly: AssemblyInformationalVersion("1.1.27.2-v14")]
+[assembly: AssemblyInformationalVersion("1.1.27.2-v15")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3f8c785a-48d0-41f5-9257-6f73afca41eb")]
