d1e3c93c58ee19ac64fa519ef06c19db75a36c0e
Maksim Didinov <mdi@compent.net>
2017-08-23 16:18:47 +0300

    BaseControls. Fix LinkPicker mailto link

--- src/uIntra.BaseControls/Properties/AssemblyInfo.cs
+++ src/uIntra.BaseControls/Properties/AssemblyInfo.cs
@@ -5,6 +5,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Base activity package.")]
 
 [assembly: AssemblyVersion("2.3.4.0")]
-[assembly: AssemblyInformationalVersion("2.3.4.0-v1")]
+[assembly: AssemblyInformationalVersion("2.3.4.0-v2")]
 [assembly: ComVisible(false)]
 [assembly: Guid("2e4a8b55-37f6-46a8-b108-30e346870720")]
