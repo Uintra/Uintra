f0e6cf274f35c3a8ad0b39836f06a71d4f387324
m.kilianovskyi <mki@compent.net>
2017-08-25 13:28:47 +0300

    Added MemberApiController

--- src/Compent.uIntra/App_Start/NinjectWebCommon.cs
+++ src/Compent.uIntra/App_Start/NinjectWebCommon.cs
@@ -171,6 +171,7 @@ namespace Compent.uIntra
             // Plugin services
             kernel.Bind<IIntranetLocalizationService>().To<Core.LocalizationService>().InRequestScope();
             kernel.Bind(typeof(IIntranetUserService<>)).To<IntranetUserService<IntranetUser>>().InRequestScope();
+            kernel.Bind<ICacheableIntranetUserService>().To<IntranetUserService<IntranetUser>>().InRequestScope();
             kernel.Bind(typeof(INewsService<>)).To<NewsService>().InRequestScope();
             kernel.Bind(typeof(IEventsService<>)).To<EventsService>().InRequestScope();
             kernel.Bind(typeof(IBulletinsService<>)).To<BulletinsService>().InRequestScope();
