9eb9d23bf845f8fa42c7cd8df4b8a7a8f50ab240
Dmitry Bondar <dbo@compent.net>
2017-08-28 11:36:03 +0300

    Move CentralFeedListModel to Models folder.

--- src/uIntra.CentralFeed/Core/Models/CentralFeedListModel.cs
+++ src/uIntra.CentralFeed/Core/Models/CentralFeedListModel.cs
@@ -1,7 +1,7 @@
-﻿namespace uIntra.CentralFeed.App_Plugins.CentralFeed.Models
+﻿namespace uIntra.CentralFeed
 {
     public class CentralFeedListModel
-    {
+    { 
         public int Type { get; set; }
         public long? Version { get; set; }
         public int Page { get; set; } = 1;
