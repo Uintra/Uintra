6e660436c7b57ce33706b0e46701f32c32897404
m.kilianovskyi <mki@compent.net>
2017-08-28 18:27:35 +0300

    FTFAINTRA-95 : updating Search package

--- src/uIntra.Search/Properties/AssemblyInfo.cs
+++ src/uIntra.Search/Properties/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Runtime.InteropServices;
                                " search key words.")]
 
 [assembly: AssemblyVersion("0.0.8.2")]
-[assembly: AssemblyInformationalVersion("0.0.8.2-v1")]
+[assembly: AssemblyInformationalVersion("0.0.8.2-v2")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4d9f910e-24e0-43b3-bccb-fef527d5ff2f")]
