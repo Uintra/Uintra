1a3718d8c7a0e6a9ab9bf9ea2cd31583748fd5a6
Eugene Kunah <eku@compent.net>
2017-08-30 17:23:59 +0300

    Fix create media for non authorized users

--- src/uIntra.Core/Core/Media/IMediaHelper.cs
+++ src/uIntra.Core/Core/Media/IMediaHelper.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using uIntra.Core.Controls.FileUpload;
 using uIntra.Core.TypeProviders;
@@ -7,13 +8,13 @@ namespace uIntra.Core.Media
 {
     public interface IMediaHelper
     {
-        IEnumerable<int> CreateMedia(IContentWithMediaCreateEditModel model);
+        IEnumerable<int> CreateMedia(IContentWithMediaCreateEditModel model, Guid? userId = null);
         void DeleteMedia(int mediaId);
         void DeleteMedia(string mediaPath);
         void DeleteMedia(IEnumerable<int> mediaIds);
         void RestoreMedia(int mediaId);
         void RestoreMedia(IEnumerable<int> mediaIds);
-        IMedia CreateMedia(TempFile file, int rootMediaId);
+        IMedia CreateMedia(TempFile file, int rootMediaId, Guid? userId = null);
         bool IsMediaDeleted(IPublishedContent media);
 
         MediaSettings GetMediaFolderSettings(int mediaFolderType);
