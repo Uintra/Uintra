d7e41c44f1a3c96318161263b80be91dfa96c94f
Julia Shulumba <jsh@compent.net>
2017-09-01 12:19:50 +0300

    FTFAINTRA-639 FTFIntra. Docs panel. list of docs has different indent between items

--- src/uIntra.Panels/Properties/AssemblyInfo.cs
+++ src/uIntra.Panels/Properties/AssemblyInfo.cs
@@ -5,6 +5,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Panels can be added to all pages. They hold information of different types. They can be placed according to a grid layout by the webmaster in Umbraco.")]
 
 [assembly: AssemblyVersion("2.1.4.1")]
-[assembly: AssemblyInformationalVersion("2.1.4.1-v2")]
+[assembly: AssemblyInformationalVersion("2.1.4.1-v3")]
 [assembly: ComVisible(false)]
 [assembly: Guid("009e188c-14f8-4bb8-aea0-3d39eb12ab82")]
