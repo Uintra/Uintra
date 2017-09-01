52ac5feff7a7a2875a57edf5941a6db16cd329bb
aleksandr.chekanyn <alc@compent.net>
2017-09-01 14:54:42 +0300

    UC-819 Should not be possible to set the same umbraco user as related to different members who is webmaster

---
+++ src/uIntra.Users/Core/IMemberServiceHelper.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Umbraco.Core.Models;
+
+namespace uIntra.Users
+{
+   public  interface IMemberServiceHelper
+    {
+        Dictionary<IMember, int?> GetRelatedUserIdsForMembers(IEnumerable<IMember> users);
+    }
+}
---
+++ src/uIntra.Users/Web/UserApiController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Compent.uIntra.Controllers.Api;
+using Umbraco.Core.Services;
+using Umbraco.Web.WebApi;
+
+namespace uIntra.Users.Web
+{
+    public abstract class UserApiControllerBase : UmbracoAuthorizedApiController
+    {
+        private readonly IMemberService _memberService;
+        private readonly IUserService _userService;
+        private readonly IMemberServiceHelper _memberServiceHelper;
+
+        protected UserApiControllerBase(IUserService userService, IMemberService memberService, IMemberServiceHelper memberServiceHelper)
+        {
+            _userService = userService;
+            _memberService = memberService;
+            _memberServiceHelper = memberServiceHelper;
+        }
+
+
+        [HttpGet]
+        public virtual IEnumerable<UserPickerModel> NotAssignedToMemberUsers()
+        {
+            int totalUsersCount;
+            var users = _userService.GetAll(0, int.MaxValue, out totalUsersCount);
+            var relatedUserIdsDictionary = _memberServiceHelper.GetRelatedUserIdsForMembers(_memberService.GetAllMembers());
+            var unassignedUsers = users.Where(u => !relatedUserIdsDictionary.Values.Contains(u.Id));
+            var mappedModels = unassignedUsers.Select(u => new UserPickerModel {Id = u.Id, Name = u.Name});
+            return mappedModels;
+        }
+    }
+}
