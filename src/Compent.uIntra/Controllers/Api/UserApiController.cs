528ae18c271c14973b17fddf1bf809e8826bceb2
aleksandr.chekanyn <alc@compent.net>
2017-09-01 15:06:21 +0300

    Update packages.

---
+++ src/Compent.uIntra/Controllers/Api/UserApiController.cs
@@ -0,0 +1,16 @@
+﻿using uIntra.Users;
+using uIntra.Users.Web;
+using Umbraco.Core.Services;
+
+namespace Compent.uIntra.Controllers.Api
+{
+    public class UserApiController : UserApiControllerBase
+    {
+        public UserApiController(
+            IUserService userService,
+            IMemberService memberService,
+            IMemberServiceHelper memberServiceHelper):base(userService, memberService, memberServiceHelper)
+        {
+        }
+    }
+}
---
+++ src/Compent.uIntra/Core/MemberServiceHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using uIntra.Users;
+using Umbraco.Core.Models;
+using Member = Umbraco.Web.PublishedContentModels.Member;
+
+
+namespace Compent.uIntra.Core
+{
+    public class MemberServiceHelper : IMemberServiceHelper
+    {
+        public Dictionary<IMember, int?> GetRelatedUserIdsForMembers(IEnumerable<IMember> users)
+        {
+            return users.ToDictionary(u => u, u => u.GetValue<int?>(nameof(Member.RelatedUser)));
+        }
+    }
+}
