115038abe13390d924bbbe19068f6999cc02d7e2
aleksandr.chekanyn <alc@compent.net>
2017-09-05 11:22:18 +0300

    Fix models conflicts. Update Uintra package.

--- src/Compent.uIntra/Core/MemberServiceHelper.cs
+++ src/Compent.uIntra/Core/MemberServiceHelper.cs
@@ -2,16 +2,16 @@
 using System.Linq;
 using uIntra.Users;
 using Umbraco.Core.Models;
-using Member = Umbraco.Web.PublishedContentModels.Member;
 
 
 namespace Compent.uIntra.Core
 {
     public class MemberServiceHelper : IMemberServiceHelper
     {
+        private const string RelatedUserPropertyName = "relatedUser";
         public Dictionary<IMember, int?> GetRelatedUserIdsForMembers(IEnumerable<IMember> users)
         {
-            return users.ToDictionary(u => u, u => u.GetValue<int?>(nameof(Member.RelatedUser)));
+            return users.ToDictionary(u => u, u => u.GetValue<int?>(nameof(RelatedUserPropertyName)));
         }
     }
 }
