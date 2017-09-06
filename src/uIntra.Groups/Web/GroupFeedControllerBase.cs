0d668e65937df7f39fe011f1035e0a936ffdea00
m.kilianovskyi <mki@compent.net>
2017-09-06 13:21:43 +0300

    Added references and applyed fixes

---
+++ src/uIntra.Groups/Web/GroupFeedControllerBase.cs
@@ -0,0 +1,15 @@
+﻿using uIntra.CentralFeed;
+using uIntra.CentralFeed.Web;
+using uIntra.Core.Activity;
+using uIntra.Core.User;
+using uIntra.Subscribe;
+
+namespace uIntra.Groups.Web
+{
+    public abstract class GroupFeedControllerBase : FeedControllerBase
+    {
+        public GroupFeedControllerBase(ICentralFeedContentHelper centralFeedContentHelper, ISubscribeService subscribeService, ICentralFeedService centralFeedService, IActivitiesServiceFactory activitiesServiceFactory, IIntranetUserContentHelper intranetUserContentHelper, ICentralFeedTypeProvider centralFeedTypeProvider, IIntranetUserService<IIntranetUser> intranetUserService) : base(centralFeedContentHelper, subscribeService, centralFeedService, activitiesServiceFactory, intranetUserContentHelper, centralFeedTypeProvider, intranetUserService)
+        {
+        }
+    }
+}
