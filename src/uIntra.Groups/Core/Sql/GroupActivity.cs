b8b6803ef09bdc4e0c5acb1c2a4695ff4d863938
Maksim Didinov <mdi@compent.net>
2017-09-12 11:49:33 +0300

    Added group-activity relation table

---
+++ src/uIntra.Groups/Core/Sql/GroupActivity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using uIntra.Core.Persistence;
+
+namespace uIntra.Groups.Sql
+{
+    [uIntraTable(nameof(GroupActivity))]
+    public class GroupActivity : SqlEntity<Guid>
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public override Guid Id { get; set; }
+
+        [Required, Index("UQ_GroupActivity_GroupId_ActivityId", 1, IsUnique = true)]
+        public Guid GroupId { get; set; }
+
+        [Required, Index("UQ_GroupActivity_GroupId_ActivityId", 2, IsUnique = true)]
+        public Guid ActivityId { get; set; } 
+    }
+}
