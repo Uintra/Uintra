ea7c1c4b414602764677827610af999ebc6da092
Dmitry Bondar <dbo@compent.net>
2017-09-15 12:52:18 +0300

    IM-277: Search result page. Add counters to the filter by activity type

--- src/uIntra.Search/Core/Models/SearchResultsOverviewViewModel.cs
+++ src/uIntra.Search/Core/Models/SearchResultsOverviewViewModel.cs
@@ -7,8 +7,12 @@ namespace uIntra.Search
     {
         public IEnumerable<SearchResultViewModel> Results { get; set; } = Enumerable.Empty<SearchResultViewModel>();
 
+        public IEnumerable<SearchFilterItemViewModel> FilterItems { get; set; } = Enumerable.Empty<SearchFilterItemViewModel>();
+
         public string Query { get; set; }
 
         public int ResultsCount { get; set; }
+
+        public string AllTypesPlaceholder { get; set; }
     }
 }
