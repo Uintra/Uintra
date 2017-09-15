91962a98e1189953fba917be8d07534601da6f4a
m.kilianovskyi <mki@compent.net>
2017-09-15 13:49:58 +0300

    Ovverriden .GetHashCode() for IntranetActivity

--- src/uIntra.Core/Core/TypeProviders/IntranetType.cs
+++ src/uIntra.Core/Core/TypeProviders/IntranetType.cs
@@ -4,5 +4,10 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
     }
 }
