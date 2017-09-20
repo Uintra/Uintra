12e85309e1194403df756a9ef5e76254ed27cd37
Alexander Poddubko <apo@compent.net>
2017-09-20 11:03:33 +0300

    IM-304 - Implement monthly email package, related with users tags match

--- src/Compent.uIntra/Controllers/Api/MonthlyEmailApiController.cs
+++ src/Compent.uIntra/Controllers/Api/MonthlyEmailApiController.cs
@@ -7,28 +7,17 @@ namespace Compent.uIntra.Controllers.Api
 {
     public class MonthlyEmailApiController: UmbracoApiController
     {
-        public class MonthlyEmailApiControllerBase : UmbracoApiController
+        private readonly IMonthlyEmailService _monthlyEmailService;
+
+        public MonthlyEmailApiController(IMonthlyEmailService monthlyEmailService)
         {
-            private readonly IMonthlyEmailService _monthlyEmailService;
+            _monthlyEmailService = monthlyEmailService;            
+        }
 
-            private readonly IApplicationSettings _applicationSettings;
-
-            public MonthlyEmailApiControllerBase(IMonthlyEmailService monthlyEmailService,
-                IApplicationSettings applicationSettings)
-            {
-                _monthlyEmailService = monthlyEmailService;
-                _applicationSettings = applicationSettings;
-            }
-
-            [System.Web.Http.HttpGet]
-            public void SendMonthlyEmail()
-            {
-                if (DateTime.Now.Day == _applicationSettings.MonthlyEmailJobDay)
-                {
-                    _monthlyEmailService.SendEmail();
-                }
-
-            }
+        [System.Web.Http.HttpGet]
+        public void SendMonthlyEmail()
+        {
+            _monthlyEmailService.SendEmail();
         }
     }
 }
