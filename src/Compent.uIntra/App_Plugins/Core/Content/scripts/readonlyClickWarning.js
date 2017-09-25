4237daee663d105be4e0ab1d51136fa43e25545c
m.kilianovskyi <mki@compent.net>
2017-09-25 14:52:52 +0300

    Attached event listener for likes and comments

--- src/Compent.uIntra/App_Plugins/Core/Content/scripts/readonlyClickWarning.js
+++ src/Compent.uIntra/App_Plugins/Core/Content/scripts/readonlyClickWarning.js
@@ -1,6 +1,23 @@
-﻿var controller = {
+﻿function readonlyClickEventHandler() {
+    alert()
+}
+
+function isReadonlyItem(el) {
+    return el.dataset.isReadonly.toLowerCase() === "true";
+}
+
+var controller = {
     init: function() {
-        alert(12);
+        let selector = ".js-readonly-click-warning";
+        var c = 0;
+        var items = $(selector);
+        console.log(items)
+        items.each(function(i, el) { debugger
+            if (isReadonlyItem(el)) {
+                el.addEventListener('click', readonlyClickEventHandler);
+            }
+        });
+
     }
 };
 
