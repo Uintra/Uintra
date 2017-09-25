4854c617c4631a8a40617fbae5a32cb4479df8f2
m.kilianovskyi <mki@compent.net>
2017-09-25 19:20:35 +0300

    Added confirm to group unsubsribe

--- src/Compent.uIntra/App_Plugins/Groups/GroupSubscribe.js
+++ src/Compent.uIntra/App_Plugins/Groups/GroupSubscribe.js
@@ -1,8 +1,16 @@
-﻿var controller = {
+﻿import confirm from './../Core/Controls/Confirm/Confirm';
+
+function unsubscribeEventHanlder(e, title, message) {
+    e.preventDefault();
+
+    confirm.showConfirm(title, message, () => e.target.form.submit(), () => {}, confirm.defaultSettings);
+}
+
+var controller = {
     init: function () {
         var selector = ".js-group-subscribe ._unsubscribe";
-        var unsubscribeBtn = $(selector);
-        unsubscribeBtn.click(el => alert(el))
+        var unsubscribeForm = $(selector);
+        unsubscribeForm.on('click', e => unsubscribeEventHanlder(e, "ti", "body"));
     }
 }
 
