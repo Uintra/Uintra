d8f444db446481f73c81f47ae8433123bb9818b2
m.kilianovskyi <mki@compent.net>
2017-09-26 09:08:34 +0300

    Updating CentralFeed and Groups

--- src/uIntra.Groups/App_Plugins/Groups/GroupSubscribe.js
+++ src/uIntra.Groups/App_Plugins/Groups/GroupSubscribe.js
@@ -1,6 +1,17 @@
-﻿var controller = {
-    init: function (selector) {
+﻿import confirm from './../Core/Controls/Confirm/Confirm';
 
+function unsubscribeEventHanlder(e) {
+    e.preventDefault();
+    var title = e.target.dataset.title;
+    var message = e.target.dataset.message;
+    confirm.showConfirm(title, message, () => e.target.form.submit(), () => {}, confirm.defaultSettings);
+}
+
+var controller = {
+    init: function () {
+        var selector = ".js-group-subscribe ._unsubscribe";
+        var unsubscribeForm = $(selector);
+        unsubscribeForm.on('click', e => unsubscribeEventHanlder(e));
     }
 }
 
