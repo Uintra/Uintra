dfa35ba4ede9748936b57a623d7f6c9525e0d61e
m.kilianovskyi <mki@compent.net>
2017-09-27 10:00:28 +0300

    Updating Core package

--- src/uIntra.Core/Properties/AssemblyInfo.cs
+++ src/uIntra.Core/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.Core")]
 [assembly: AssemblyDescription("Core activity package.")]
 
-[assembly: AssemblyVersion("1.3.10.2")]
+[assembly: AssemblyVersion("1.3.10.3")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3f8c785a-48d0-41f5-9257-6f73afca41eb")]
