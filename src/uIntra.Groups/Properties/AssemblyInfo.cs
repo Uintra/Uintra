9c875263632d5a1cd84a364979984a19134d45d3
m.kilianovskyi <mki@compent.net>
2017-10-02 16:53:08 +0300

    Updating package versions

--- src/uIntra.Groups/Properties/AssemblyInfo.cs
+++ src/uIntra.Groups/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.Groups")]
 [assembly: AssemblyDescription("Some kind of group description.")]
 
-[assembly: AssemblyVersion("0.0.1.11")]
+[assembly: AssemblyVersion("0.0.1.12")]
 [assembly: ComVisible(false)]
 [assembly: Guid("85aa3a0a-738e-4e0d-9448-ea34aa3a537c")]
