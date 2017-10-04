8f5144ddaa6c6c02ed603ee467ec2d5aa4419b1d
m.kilianovskyi <mki@compent.net>
2017-10-04 17:58:18 +0300

    Created SearchMediaEventService and subscribed for event

---
+++ src/uIntra.Search/Core/UmbracoEventServices/SearchMediaEventService.cs
@@ -0,0 +1,15 @@
+﻿using uIntra.Core.UmbracoEventServices;
+using Umbraco.Core.Events;
+using Umbraco.Core.Models;
+using Umbraco.Core.Services;
+
+namespace uIntra.Search
+{
+    public class SearchMediaEventService : IUmbracoMediaEventService
+    {
+        public void ProcessMediaSaved(IMediaService sender, SaveEventArgs<IMedia> e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
