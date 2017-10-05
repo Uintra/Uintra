b8bb326cb3e1f30f2ada8721dbe33bdf2c07d9d7
aleksandr.chekanyn <alc@compent.net>
2017-10-05 14:54:21 +0300

    UC-827 Review Comments for content pages

---
+++ src/uIntra.Core/Core/IUmbracoContentHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Umbraco.Core.Models;
+
+namespace uIntra.Core
+{
+    public interface IUmbracoContentHelper
+    {
+        bool IsContentAvailable(IPublishedContent publishedContent);
+        bool IsForContentPage(Guid id);
+    }
+}
