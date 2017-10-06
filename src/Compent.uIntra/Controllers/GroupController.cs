844b839cc55acc01ddc28c56a9b51e2c06795ad9
Maksim Didinov <mdi@compent.net>
2017-10-06 16:13:04 +0300

    Updated Groups

--- src/Compent.uIntra/Controllers/GroupController.cs
+++ src/Compent.uIntra/Controllers/GroupController.cs
@@ -1,24 +1,55 @@
-﻿using uIntra.Core.Links;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Compent.uIntra.Core.Extentions;
+using uIntra.Core;
+using uIntra.Core.Links;
 using uIntra.Core.Media;
 using uIntra.Core.User;
 using uIntra.Groups;
 using uIntra.Groups.Web;
 using Umbraco.Core.Services;
+using Umbraco.Web;
 
 namespace Compent.uIntra.Controllers
 {
     public class GroupController : GroupControllerBase
     {
+        private readonly UmbracoHelper _umbracoHelper;
+        private readonly IDocumentTypeAliasProvider _documentTypeAliasProvider;
+
         public GroupController(IGroupService groupService, 
             IGroupMemberService groupMemberService, 
             IMediaHelper mediaHelper,
             IGroupLinkProvider groupLinkProvider, 
             IUserService userService, 
             IGroupMediaService groupMediaService, 
-            IIntranetUserService<IGroupMember> intranetUserService, IProfileLinkProvider profileLinkProvider) 
+            IIntranetUserService<IGroupMember> intranetUserService, IProfileLinkProvider profileLinkProvider, UmbracoHelper umbracoHelper, IDocumentTypeAliasProvider documentTypeAliasProvider) 
             : base(groupService, groupMemberService, mediaHelper, groupMediaService, intranetUserService, profileLinkProvider, groupLinkProvider)
         {
+            _umbracoHelper = umbracoHelper;
+            _documentTypeAliasProvider = documentTypeAliasProvider;
         }
-        
+
+        public override ActionResult LeftNavigation()
+        {
+            var result = new List<GroupLeftNavigationItemViewModel>();
+            var xpath = XPathHelper.GetXpath(_documentTypeAliasProvider.GetHomePage(), _documentTypeAliasProvider.GetGroupOverviewPage());
+            var groupPage = _umbracoHelper.TypedContentSingleAtXPath(xpath);
+            var children = groupPage.Children;
+
+            foreach (var child in children)
+            {
+                if (child.IsShowPageInSubNavigation())
+                {
+                    result.Add(new GroupLeftNavigationItemViewModel()
+                    {
+                        Name = child.GetNavigationName(),
+                        Url = child.Url
+                    });
+                }
+            }
+
+            return PartialView(LeftNavigationPath, result);
+        }
     }
 }
