9e8797da4dd834958c873901a5bc56010985fd17
aleksandr.chekanyn <alc@compent.net>
2017-10-09 13:31:55 +0300

    Revert refactoring.

--- src/uIntra.Groups/Core/AutoMapperProfiles/GroupsAutoMapperProfile.cs
+++ src/uIntra.Groups/Core/AutoMapperProfiles/GroupsAutoMapperProfile.cs
@@ -11,7 +11,6 @@ namespace uIntra.Groups
     {
         protected override void Configure()
         {
-            Mapper.CreateMap<GroupMemberEntity, GroupMember>();
 
             Mapper.CreateMap<Group, GroupModel>();
             Mapper.CreateMap<GroupModel, Group>();
