60e1fe107fb38d67d9ff8e0b7942310c0f0cc5a2
Dmitry Bondar <dbo@compent.net>
2017-10-17 12:01:00 +0300

    Increment package version

--- src/uIntra.Navigation/Properties/AssemblyInfo.cs
+++ src/uIntra.Navigation/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("The navigation is positioned in the left side of uIntra and serves as a overview of the pages and their sub-pages. It is also possible to add" +
                                " links to the navigation for quick access.")]
 
-[assembly: AssemblyVersion("1.2.2.3")]
+[assembly: AssemblyVersion("1.2.3.0")]
 [assembly: ComVisible(false)]
