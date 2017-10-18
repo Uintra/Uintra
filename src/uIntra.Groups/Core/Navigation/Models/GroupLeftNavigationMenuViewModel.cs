97d30321780ccd5165b95ac362df4425b8d96d51
Dmitry Bondar <dbo@compent.net>
2017-10-18 17:28:28 +0300

    UC-912: Add link to the groups list page

---
+++ src/uIntra.Groups/Core/Navigation/Models/GroupLeftNavigationMenuViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace uIntra.Groups
+{
+    public class GroupLeftNavigationMenuViewModel
+    {
+        public string GroupOverviewPageUrl { get; set; }
+        public IEnumerable<GroupLeftNavigationItemViewModel> Items { get; set; } = Enumerable.Empty<GroupLeftNavigationItemViewModel>();
+    }
+}
