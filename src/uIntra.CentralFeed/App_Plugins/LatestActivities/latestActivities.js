069c1c8efefb8ce110983e94a06caeac7024613e
Denis Prodan <dpr@compent.net>
2017-10-18 17:23:44 +0300

    fixes in central feed

--- src/uIntra.CentralFeed/App_Plugins/LatestActivities/latestActivities.js
+++ src/uIntra.CentralFeed/App_Plugins/LatestActivities/latestActivities.js
@@ -3,11 +3,11 @@
 var holders;
 
 function init() {
-    holders = document.querySelectorAll('.js-latest-activities').toArray();
+    holders = document.querySelectorAll('.js-latest-activities');
     if (!holders.length) return;
     holders.forEach(function (holder) {
         var seeAll = holder.querySelector('.js-see-all');
-        var activityType = holder.querySelector("input[name='typeId']");
+        var activityType = holder.querySelector('input[name="typeId"]');
         var activityTypeId = activityType.value;
         if (seeAll && activityTypeId) {
             seeAll.addEventListener('click', function () {
