84736e20790d44eb7cf115f87f6abd3fc2424571
Julia Shulumba <jsh@compent.net>
2017-10-30 19:52:46 +0200

    Update packages Core

--- src/Compent.uIntra/App_Plugins/Core/Content/scripts/Polyfill.js
+++ src/Compent.uIntra/App_Plugins/Core/Content/scripts/Polyfill.js
@@ -160,3 +160,16 @@ if (!Array.prototype.includes) {
 Array.prototype.except = function (a) {
     return this.filter(el => a.indexOf(el) === -1);
 };
+
+/**
+ * NodeList forEach
+ */
+
+if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = function (callback, thisArg) {
+        thisArg = thisArg || window;
+        for (var i = 0; i < this.length; i++) {
+            callback.call(thisArg, this[i], i, this);
+        }
+    };
+}
