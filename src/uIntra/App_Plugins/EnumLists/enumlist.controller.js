bddbcc3b8c9c7a551476417577607119031442cc
Mykhailo Kilianovskyi <mki@compent.dk>
2017-10-31 18:54:49 +0200

    UC-1123 - update EnumLists package

--- src/uIntra/App_Plugins/EnumLists/enumlist.controller.js
+++ src/uIntra/App_Plugins/EnumLists/enumlist.controller.js
@@ -2,7 +2,7 @@ angular.module("umbraco").controller("EnumList",
     function ($scope, enumResource) {
 
         enumResource.getAll($scope.model.config.assembly, $scope.model.config.enum).then(function (response) {
-            $scope.enumItems = response.data; debugger
+            $scope.enumItems = response.data;
             if ($scope.model.value === "") {
                 var i = parseInt($scope.model.config.defaultValueIndex);
                 if (!isNaN(i)) {
