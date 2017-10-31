16b926e8a268281a4440cae14b682fcba4f90578
Julia Shulumba <jsh@compent.net>
2017-10-31 10:39:55 +0200

    Link added in comment should have related styles and be linkable.

--- src/uIntra.Core/Properties/AssemblyInfo.cs
+++ src/uIntra.Core/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.Core")]
 [assembly: AssemblyDescription("Core activity package.")]
 
-[assembly: AssemblyVersion("1.3.14.16")]
+[assembly: AssemblyVersion("1.3.14.17")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3f8c785a-48d0-41f5-9257-6f73afca41eb")]
