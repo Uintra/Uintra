134fba800eaecbdf98098bf2db92b17c681a5cd3
Julia Shulumba <jsh@compent.net>
2017-10-31 16:55:22 +0200

    UC-874 Add design to groups

--- src/uIntra.Groups/Properties/AssemblyInfo.cs
+++ src/uIntra.Groups/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.Groups")]
 [assembly: AssemblyDescription("Some kind of group description.")]
 
-[assembly: AssemblyVersion("0.1.2.17")]
+[assembly: AssemblyVersion("0.1.2.18")]
 [assembly: ComVisible(false)]
 [assembly: Guid("85aa3a0a-738e-4e0d-9448-ea34aa3a537c")]
