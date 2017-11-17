0831bc6c5586f30ddc374e2f5fc4cfa41585f737
Dmitry Bondar <dbo@compent.net>
2017-11-17 10:03:33 +0200

    Add notification settings config

---
+++ src/Compent.uIntra/App_Plugins/NotificationSettings/backoffice/NotificationSettingsTree/notification-settings.config.js
@@ -0,0 +1,17 @@
+﻿(function (angular) {
+    'use strict';
+
+    angular.module('umbraco')
+        .constant("notificationSettingsConfig", {
+            description: {
+                toolbar: "styleselect | bold | italic | underline | linksPicker | numlist | bullist | removeformat",
+                style_formats: [
+                    { title: 'Heading 1', block: 'h1' },
+                    { title: 'Heading 2', block: 'h2' },
+                    { title: 'Heading 3', block: 'h3' },
+                    { title: 'Normal', block: 'p' }
+                ]
+            }
+        });
+
+})(angular);
