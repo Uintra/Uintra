1a4d2d056ac61b3922c27572a66d84230b0af3ea
Dmitry Bondar <dbo@compent.net>
2017-11-27 10:18:13 +0200

    Add js-active-edit-control class for editing control

--- src/uIntra.BaseControls/Properties/AssemblyInfo.cs
+++ src/uIntra.BaseControls/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.BaseControls")]
 [assembly: AssemblyDescription("Base activity package.")]
 
-[assembly: AssemblyVersion("2.4.2.2")]
+[assembly: AssemblyVersion("2.4.2.3")]
 [assembly: ComVisible(false)]
 [assembly: Guid("2e4a8b55-37f6-46a8-b108-30e346870720")]
