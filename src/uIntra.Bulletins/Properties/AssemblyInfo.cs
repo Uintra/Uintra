176cc20493af13f1f6618c468f09b93a2315b232
aleksandr.chekanyn <alc@compent.net>
2017-11-28 14:23:41 +0200

    Update packages.

--- src/uIntra.Bulletins/Properties/AssemblyInfo.cs
+++ src/uIntra.Bulletins/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("uIntra.Bulletins")]
 [assembly: AssemblyDescription("Bulletins are a simple type of post that are used for more casual purposes than Event and News posts. All types of user roles has access to writing Bulletins.")]
 
-[assembly: AssemblyVersion("0.3.0.3")]
+[assembly: AssemblyVersion("0.3.0.1")]
 [assembly: ComVisible(false)]
 [assembly: Guid("9cdb39aa-aa5a-4691-b942-8f2ee3297e64")]
