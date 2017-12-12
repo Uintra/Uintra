945e859f292e0b2479445a2977a1d21adc3d8eac
m.kilianovskyi <mki@compent.net>
2017-12-12 10:26:18 +0200

    Move Tree<T> class into separate file

--- src/uIntra.Core/Core/Extensions/TreeExtensions.cs
+++ src/uIntra.Core/Core/Extensions/TreeExtensions.cs
@@ -4,25 +4,14 @@ using System.Linq;
 
 namespace uIntra.Core.Extensions
 {
-
-    public class Tree<T> 
-    {
-        public Tree(T value, IEnumerable<Tree<T>> children)
-        {
-            Value = value;
-            Children = children;
-        }
-
-        public Tree(T value, params Tree<T>[] children) : this(value, children.AsEnumerable())
-        {
-        }
-
-        public T Value { get; }
-        public IEnumerable<Tree<T>> Children { get; }
-    }
-
     public static class TreeExtensions
     {
+        public static Tree<T> WithChildren<T>(this Tree<T> tree, params Tree<T>[] children) => 
+            WithChildren(tree, children.AsEnumerable());
+
+        public static Tree<T> WithChildren<T>(this Tree<T> tree, IEnumerable<Tree<T>> children) => 
+            new Tree<T>(tree.Value, children);
+
         public static IEnumerable<Tree<T>> Where<T>(this Tree<T> tree, Func<T, bool> predicate)
         {
             IEnumerable<Tree<T>> WhereRec(Tree<T> tr)
