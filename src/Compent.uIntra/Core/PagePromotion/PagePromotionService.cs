5abef3ef1b35fca34f639979de182e506717098c
Dmitry Bondar <dbo@compent.net>
2017-12-13 12:04:33 +0200

    Add PagePromotionService implementation from Imidt

--- src/Compent.uIntra/Core/PagePromotion/PagePromotionService.cs
+++ src/Compent.uIntra/Core/PagePromotion/PagePromotionService.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using uIntra.CentralFeed;
+using uIntra.Comments;
+using uIntra.Core;
 using uIntra.Core.Activity;
 using uIntra.Core.Extensions;
 using uIntra.Core.PagePromotion;
 using uIntra.Core.TypeProviders;
+using uIntra.Core.User;
+using uIntra.Likes;
+using Umbraco.Core.Models;
+using Umbraco.Web;
 
 namespace Compent.uIntra.Core.PagePromotion
 {
@@ -12,11 +19,28 @@ namespace Compent.uIntra.Core.PagePromotion
     {
         private readonly IActivityTypeProvider _activityTypeProvider;
         private readonly IFeedTypeProvider _feedTypeProvider;
+        private readonly UmbracoHelper _umbracoHelper;
+        private readonly IIntranetUserService<IIntranetUser> _userService;
+        private readonly ILikesService _likesService;
+        private readonly ICommentsService _commentsService;
+        private readonly IDocumentTypeAliasProvider _documentTypeAliasProvider;
 
-        public PagePromotionService(IActivityTypeProvider activityTypeProvider, IFeedTypeProvider feedTypeProvider)
+        public PagePromotionService(
+            IActivityTypeProvider activityTypeProvider,
+            IFeedTypeProvider feedTypeProvider,
+            UmbracoHelper umbracoHelper,
+            IIntranetUserService<IIntranetUser> userService,
+            ILikesService likesService,
+            ICommentsService commentsService,
+            IDocumentTypeAliasProvider documentTypeAliasProvider)
         {
             _activityTypeProvider = activityTypeProvider;
             _feedTypeProvider = feedTypeProvider;
+            _umbracoHelper = umbracoHelper;
+            _userService = userService;
+            _likesService = likesService;
+            _commentsService = commentsService;
+            _documentTypeAliasProvider = documentTypeAliasProvider;
         }
 
         public IIntranetType ActivityType => _activityTypeProvider.Get(IntranetActivityTypeEnum.PagePromotion.ToInt());
@@ -34,12 +58,96 @@ namespace Compent.uIntra.Core.PagePromotion
 
         public PagePromotion GetPagePromotion(Guid pageId)
         {
-            throw new NotImplementedException();
+            var content = _umbracoHelper.TypedContent(pageId);
+            var contentAndPagePromotionConfig = GetActualContentAndPagePromotionConfig(content);
+            return MapToPagePromotionModel(contentAndPagePromotionConfig);
         }
 
         public IEnumerable<IFeedItem> GetItems()
         {
-            throw new NotImplementedException();
+            return GetManyActual().OrderByDescending(i => i.PublishDate);
+        }
+
+        private IEnumerable<IFeedItem> GetManyActual()
+        {
+            var homePage = _umbracoHelper.TypedContentAtRoot().Single(pc => pc.DocumentTypeAlias.Equals(_documentTypeAliasProvider.GetHomePage()));
+
+            var contentAndPagePromotionConfigs = homePage
+                .Descendants()
+                .Select(GetActualContentAndPagePromotionConfig);
+
+            return contentAndPagePromotionConfigs.Where(conf => conf != null).Select(MapToCentralFeedItem);
+        }
+
+        private Tuple<IPublishedContent, PagePromotionConfig> GetActualContentAndPagePromotionConfig(IPublishedContent content)
+        {
+            var isContentPromotionPage = false;// content is IPagePromotionComposition;
+            if (!isContentPromotionPage)
+            {
+                return null;
+            }
+
+            var pagePromotionConfig = GetPagePromotionConfig(content);
+            if (pagePromotionConfig != null && pagePromotionConfig.PromoteOnCentralFeed && pagePromotionConfig.PublishDate <= DateTime.Now)
+            {
+                return new Tuple<IPublishedContent, PagePromotionConfig>(content, pagePromotionConfig);
+            }
+
+            return null;
+        }
+
+        private PagePromotion MapToPagePromotionModel(Tuple<IPublishedContent, PagePromotionConfig> contentAndPagePromotionConfig)
+        {
+            var pagePromotionConfig = contentAndPagePromotionConfig.Item2;
+            var pagePromotionModel = contentAndPagePromotionConfig.Item1.Map<PagePromotion>();
+            pagePromotionModel.CreatorId = _userService.Get(pagePromotionModel.UmbracoCreatorId.Value).Id;
+
+            FillPagePromotionConfig(pagePromotionModel, pagePromotionConfig);
+            return pagePromotionModel;
+        }
+
+        private IFeedItem MapToCentralFeedItem(Tuple<IPublishedContent, PagePromotionConfig> contentAndPagePromotionConfig)
+        {
+            var pagePromotionConfig = contentAndPagePromotionConfig.Item2;
+
+            var centralFeedItem = contentAndPagePromotionConfig.Item1.Map<PagePromotion>();
+            FillPagePromotionConfig(centralFeedItem, pagePromotionConfig);
+
+            centralFeedItem.Commentable = pagePromotionConfig.Comentable;
+            centralFeedItem.Likeable = pagePromotionConfig.Likeable;
+            centralFeedItem.CreatorId = _userService.Get(centralFeedItem.UmbracoCreatorId.Value).Id;
+
+            _likesService.FillLikes(centralFeedItem);
+            _commentsService.FillComments(centralFeedItem);
+
+            return centralFeedItem;
+        }
+
+        private void FillPagePromotionConfig(PagePromotion model, PagePromotionConfig pagePromotionConfig)
+        {
+            model.Type = new IntranetType
+            {
+                Id = ActivityType.Id,
+                Name = model.PageAlias
+            };
+            model.Title = pagePromotionConfig.Title;
+            model.Description = pagePromotionConfig.Description;
+            model.PublishDate = pagePromotionConfig.PublishDate;
+            model.MediaIds = pagePromotionConfig.Files.ToIntCollection();
+        }
+
+        private PagePromotionConfig GetPagePromotionConfig(IPublishedContent content)
+        {
+            var config = new PagePromotionConfig();
+            var prop = content.GetPropertyValue<string>("pagePromotionConfig");
+            if (prop.IsNotNullOrEmpty())
+            {
+                config = prop.Deserialize<PagePromotionConfig>();
+               // config.Comentable = content.GridContainsPlugin("Comments");
+               // config.Likeable = content.GridContainsPlugin("Likes");
+                return config;
+            }
+            return config;
         }
     }
 }
