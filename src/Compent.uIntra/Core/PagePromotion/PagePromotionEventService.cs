13c32b4730a8aa3ddff1425fe52374085756bda6
Dmitry Bondar <dbo@compent.net>
2017-12-18 14:37:04 +0200

    UC-1304: Add searching by page promotion media

---
+++ src/Compent.uIntra/Core/PagePromotion/PagePromotionEventService.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using uIntra.Core.PagePromotion;
+using uIntra.Core.UmbracoEventServices;
+using uIntra.Search;
+using Umbraco.Core.Events;
+using Umbraco.Core.Models;
+using Umbraco.Core.Publishing;
+using Umbraco.Core.Services;
+
+namespace Compent.uIntra.Core.PagePromotion
+{
+    public class PagePromotionEventService : IUmbracoEventService<IPublishingStrategy, PublishEventArgs<IContent>>
+    {
+        public void Process(IPublishingStrategy sender, PublishEventArgs<IContent> publishEventArgs)
+        {
+            foreach (var entity in publishEventArgs.PublishedEntities)
+            {
+                UpdatePagePromotionFilesCache(entity);
+            }
+        }
+
+        private static void UpdatePagePromotionFilesCache(IContent content)
+        {
+            if (!PagePromotionHelper.IsPagePromotion(content)) return;
+
+            var contentService = DependencyResolver.Current.GetService<IContentService>();
+            var documentIndexer = DependencyResolver.Current.GetService<IDocumentIndexer>();
+
+            var newsFilesIds = PagePromotionHelper.GetFileIds(content).ToList();
+
+            if (content.Published)
+            {
+                var oldContentVersion = contentService.GetVersions(content.Id).FirstOrDefault(c => c.Version != content.Version);
+                var oldFilesIds = PagePromotionHelper.GetFileIds(oldContentVersion).ToList();
+              
+                documentIndexer.DeleteFromIndex(oldFilesIds.Except(newsFilesIds));
+                documentIndexer.Index(newsFilesIds.Except(oldFilesIds));
+            }
+            else
+            {
+                documentIndexer.DeleteFromIndex(newsFilesIds);
+            }
+        }
+    }
+}
---
+++ src/uIntra.Core/Core/PagePromotion/PagePromotionHelper.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using uIntra.Core.Extensions;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+
+namespace uIntra.Core.PagePromotion
+{
+    public class PagePromotionHelper
+    {
+        public static bool IsPagePromotion(IContent content) => content.HasProperty(PagePromotionConstants.PagePromotionConfigAlias);
+
+        public static PagePromotionConfig GetConfig(IPublishedContent content)
+        {
+            var prop = content.GetPropertyValue<string>(PagePromotionConstants.PagePromotionConfigAlias);
+            return GetConfig(prop);
+        }
+
+        public static PagePromotionConfig GetConfig(IContent content)
+        {
+            var prop = content.GetValue<string>(PagePromotionConstants.PagePromotionConfigAlias);
+            return GetConfig(prop);
+        }
+
+        public static IEnumerable<int> GetFileIds(IContent content)
+        {
+            var config = GetConfig(content);
+            if (config == null) return Enumerable.Empty<int>();
+
+            return config.Files.ToIntCollection();
+        }
+
+        private static PagePromotionConfig GetConfig(string propertyValue)
+        {
+            return propertyValue.IsNullOrEmpty() ? null : propertyValue.Deserialize<PagePromotionConfig>();
+        }
+    }
+}
