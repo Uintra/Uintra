1d4871839bc87a9e12da2f5076674a97db31e6bb
aleksandr.chekanyn <alc@compent.net>
2017-12-20 17:13:31 +0200

    Add user tags to bulletin create and edit.

--- src/Compent.uIntra/Controllers/BulletinsController.cs
+++ src/Compent.uIntra/Controllers/BulletinsController.cs
@@ -15,6 +15,7 @@ using Compent.uIntra.Core.Activity.Models;
 using Compent.uIntra.Core.Feed;
 using uIntra.Core.Feed;
 using uIntra.Groups.Extensions;
+using uIntra.Tagging.UserTags;
 
 namespace Compent.uIntra.Controllers
 {
@@ -22,11 +23,14 @@ namespace Compent.uIntra.Controllers
     {
         protected override string DetailsViewPath => "~/Views/Bulletins/DetailsView.cshtml";
         protected override string ItemViewPath => "~/Views/Bulletins/ItemView.cshtml";
+        protected override string EditViewPath => "~/Views/Bulletins/EditView.cshtml";
+        protected override string CreationFormViewPath { get; } = "~/Views/Bulletins/CreationForm.cshtml";
         protected override string ItemHeaderViewPath { get; } = "~/Views/Bulletins/ItemHeader.cshtml";
 
         private readonly IBulletinsService<Bulletin> _bulletinsService;
         private readonly IMyLinksService _myLinksService;
         private readonly IGroupActivityService _groupActivityService;
+        private readonly UserTagService _userTagService;
 
         public BulletinsController(
             IBulletinsService<Bulletin> bulletinsService,
@@ -34,14 +38,50 @@ namespace Compent.uIntra.Controllers
             IIntranetUserService<IIntranetUser> intranetUserService,
             IActivityTypeProvider activityTypeProvider, 
             IMyLinksService myLinksService,
-            IGroupActivityService groupActivityService)
+            IGroupActivityService groupActivityService,
+            UserTagService userTagService)
             : base(bulletinsService, mediaHelper, intranetUserService, activityTypeProvider)
         {
             _bulletinsService = bulletinsService;
             _myLinksService = myLinksService;
             _groupActivityService = groupActivityService;
+            _userTagService = userTagService;
         }
-    
+
+        [HttpPost]
+        public JsonResult CreateExtended(BulletinExtendedCreateModel model)
+        {
+            return Create(model);
+        }
+
+        protected override BulletinCreateModel GetCreateFormModel(IActivityCreateLinks links)
+        {
+            var model = base.GetCreateFormModel(links);
+            var extendedModel = model.Map<BulletinExtendedCreateModel>();
+            return extendedModel;
+        }
+
+        [HttpPost]
+        public ActionResult EditExtended(BulletinExtendedEditModel model)
+        {
+            return Edit(model);
+        }
+
+        protected override BulletinEditModel GetEditViewModel(BulletinBase bulletin, ActivityLinks links)
+        {
+            var baseModel = base.GetEditViewModel(bulletin, links);
+            var extendedModel = baseModel.Map<BulletinExtendedEditModel>();
+            return extendedModel;
+        }
+
+        protected override void OnBulletinEdited(BulletinBase bulletin, BulletinEditModel model)
+        {
+            if (model is BulletinExtendedEditModel extendedModel)
+            {
+                ReplaceTags(bulletin.Id, extendedModel.TagIdsData);
+            }
+        }
+
         protected override BulletinViewModel GetViewModel(BulletinBase bulletin, ActivityFeedOptions options)
         {
             var extendedBullet = (Bulletin)bulletin;
@@ -84,14 +124,25 @@ namespace Compent.uIntra.Controllers
         protected override void OnBulletinCreated(BulletinBase bulletin, BulletinCreateModel model)
         {
             base.OnBulletinCreated(bulletin, model);
-
             var groupId = Request.QueryString.GetGroupId();
+
             if (groupId.HasValue)
             {
                 _groupActivityService.AddRelation(groupId.Value, bulletin.Id);
                 var extendedBulletin = _bulletinsService.Get(bulletin.Id);
                 extendedBulletin.GroupId = groupId;
             }
+
+            if (model is BulletinExtendedCreateModel extendedModel)
+            {
+                ReplaceTags(bulletin.Id, extendedModel.TagIdsData);
+            }
+        }
+
+        private void ReplaceTags(Guid entityId, string collectionString)
+        {
+            var tagIds = collectionString.ParseStringCollection(Guid.Parse);
+            _userTagService.ReplaceRelations(entityId, tagIds);
         }
     }
 }
