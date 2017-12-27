7e53dabe2d7a5412b7c53c8ec1ba48c99c745b31
Denys Dzhelomanov <dje@compent.net>
2017-12-27 16:05:18 +0200

    1232: Add new design to Content panel

--- src/Compent.uIntra/postcss.config.js
+++ src/Compent.uIntra/postcss.config.js
@@ -18,7 +18,7 @@ module.exports = {
                         '--text-color-light': '#8f8f8f',
                         '--text-color-dark': '#555',
                         '--color-submit': '#261d43',
-                        '--color-submit-hover': '#1f4a93',
+                        '--color-submit-hover': '#5c5573',
                         '--header-bg': '#5776f9',
                         '--font-custom': 'muli, Arial, Helvetica, sans-serif',
                         '--font-general': 'muli, Arial, Helvetica, sans-serif'
