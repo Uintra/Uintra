aa59f25796cd938d14a97c2f3a92f9b0cf2fcf73
aleksandr.chekanyn <alc@compent.net>
2017-12-27 13:14:35 +0200

    Add search urls to tags view.

--- src/Compent.uIntra/Controllers/UserTagsController.cs
+++ src/Compent.uIntra/Controllers/UserTagsController.cs
@@ -2,6 +2,8 @@
 using System.Linq;
 using System.Web.Mvc;
 using Compent.uIntra.Core.UserTags.ViewModels;
+using uIntra.Core.Extensions;
+using uIntra.Search;
 using uIntra.Tagging.UserTags;
 using uIntra.Tagging.UserTags.Models;
 using uIntra.Tagging.Web;
@@ -12,19 +14,25 @@ namespace Compent.uIntra.Controllers
     public class UserTagsController : UserTagsControllerBase
     {
         private readonly string EntityTagsViewPath = "~/Views/UserTags/EntityTags.cshtml";
+        private readonly ISearchUmbracoHelper _searchUmbracoHelper;
 
         private readonly IUserTagService _tagsService;    
 
-        public UserTagsController(IUserTagService tagsService, IUserTagProvider tagProvider) : base(tagsService, tagProvider)
+        public UserTagsController(
+            IUserTagService tagsService,
+            IUserTagProvider tagProvider,
+            ISearchUmbracoHelper searchUmbracoHelper) : base(tagsService, tagProvider)
         {
             _tagsService = tagsService;
+            _searchUmbracoHelper = searchUmbracoHelper;
         }
 
         public ActionResult ForEntity(Guid entityId)
         {
+            var searchPage = _searchUmbracoHelper.GetSearchPage();
             var tags = _tagsService
                 .GetRelatedTags(entityId)
-                .Select(MapToViewModel)
+                .Select(tag => MapToViewModel(tag, searchPage.Url))
                 .ToList();
             return PartialView(EntityTagsViewPath, tags);
         }
@@ -35,12 +43,12 @@ namespace Compent.uIntra.Controllers
             return ForEntity(currentContentPageId);
         }
 
-        private UserTagViewModel MapToViewModel(UserTag tag)
+        private UserTagViewModel MapToViewModel(UserTag tag, string url)
         {
             return new UserTagViewModel()
             {
                 Text = tag.Text,
-                SearchUrl = "potato"
+                SearchUrl = url.AddQueryParameter(tag.Text)
             };
         }
     }
