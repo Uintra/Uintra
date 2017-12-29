e0b6b4e60824a27d38cc095149c20a3a6f1d43dc
Denys Dzhelomanov <dje@compent.net>
2017-12-29 12:11:45 +0200

    rename button's classes, navigation redesign

--- src/Compent.uIntra/App_Plugins/Events/Create/create-events.js
+++ src/Compent.uIntra/App_Plugins/Events/Create/create-events.js
@@ -38,7 +38,7 @@ var initUserSelect = function () {
 var initDescriptionControl = function () {
     var dataStorage = holder.find('#js-hidden-description-container');
     var descriptionElem = holder.find('#description');
-    var btn = holder.find('.form__btn._submit');
+    var btn = holder.find('.js-submit');
     var toolbarOptions = [
         [{ 'header': [1, 2, 3, false] }],
         ['bold', 'italic', 'underline', 'link'],
