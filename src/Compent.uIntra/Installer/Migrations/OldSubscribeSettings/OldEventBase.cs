43d89ff1be773eece58ce29e08353d91748f2bba
Dmitry Bondar <dbo@compent.net>
2018-01-05 15:52:38 +0200

    Add OldSubscribeSettingsMigration

--- src/Compent.uIntra/Installer/Migrations/OldSubscribeSettings/OldEventBase.cs
+++ src/Compent.uIntra/Installer/Migrations/OldSubscribeSettings/OldEventBase.cs
@@ -1,18 +1,17 @@
 ﻿using System;
-using uIntra.Core.Activity;
-using uIntra.Core.User;
 
-namespace uIntra.Events
+namespace Compent.uIntra.Installer.Migrations.OldSubscribeSettings
 {
-    public class EventBase : IntranetActivity, IHaveCreator, IHaveOwner
+    public class OldEventBase
     {
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public DateTime PublishDate { get; set; }
+        public bool CanSubscribe { get; set; }
         public Guid CreatorId { get; set; }
         public Guid OwnerId { get; set; }
         public int? UmbracoCreatorId { get; set; }
         public string LocationTitle { get; set; }
-        public string LocationAddress { get; set; } 
+        public string LocationAddress { get; set; }
     }
 }
