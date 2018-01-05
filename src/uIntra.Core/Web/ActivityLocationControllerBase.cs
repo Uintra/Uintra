79959805b023b3279238358d3f7c2c171ed5a9a5
m.kilianovskyi <mki@compent.net>
2018-01-05 19:35:03 +0200

    Migrate location displaying from Imidt with removing some dupiation

--- src/uIntra.Core/Web/ActivityLocationControllerBase.cs
+++ src/uIntra.Core/Web/ActivityLocationControllerBase.cs
@@ -7,14 +7,20 @@ namespace uIntra.Core.Web
 {
     public abstract class ActivityLocationControllerBase : SurfaceController
     {
-        protected virtual string ActivityLocationEditViewPath { get; } = "~/App_Plugins/Core/ActivityLocation/Edit/EditView.cshtml";
+        protected virtual string LocationEditViewPath { get; } = "~/App_Plugins/Core/ActivityLocation/Edit/EditView.cshtml";
+        protected virtual string LocationHeaderLinkViewPath { get; } = "~/App_Plugins/Core/ActivityLocation/HeaderLink/View.cshtml";
 
         private ActivityLocationEditModel EmptyEditModel => new ActivityLocationEditModel();
 
         public virtual ActionResult Edit(ActivityLocationEditModel location)
         {
             var model = location ?? EmptyEditModel;
-            return PartialView(ActivityLocationEditViewPath, model);
+            return PartialView(LocationEditViewPath, model);
+        }
+
+        public virtual ActionResult HeaderLink(ActivityLocation location)
+        {
+            return PartialView(LocationHeaderLinkViewPath, location);
         }
     }
 }
