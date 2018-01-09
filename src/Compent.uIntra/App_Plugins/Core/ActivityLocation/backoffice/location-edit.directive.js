6f68f6da2213eb9751f1a1a520a1edb92c9c9741
m.kilianovskyi <mki@compent.net>
2018-01-09 17:52:31 +0200

    Bind directive with map and  view

--- src/Compent.uIntra/App_Plugins/Core/ActivityLocation/backoffice/location-edit.directive.js
+++ src/Compent.uIntra/App_Plugins/Core/ActivityLocation/backoffice/location-edit.directive.js
@@ -2,6 +2,19 @@
     'use strict';
 
     var factory = function (assetsService) {
+        function init(scope, elem, attrs) {
+            var find = function (query) { return getRawDomElement(elem, query) };
+            var findModelHolder = function (name) { return find('[ng-model="' + name + '"]') }
+
+            var addressHolder = findModelHolder('address');
+            var shortAddressHolder = findModelHolder('shortAddress');
+            var mapContainer = find('#js-map-container');
+            debugger;
+            initActivityLocationEdit(addressHolder, shortAddressHolder, mapContainer);
+        }
+
+        function getRawDomElement(elem, query) { return elem.find(query)[0]; }
+
         return {
             restrict: 'E',
             templateUrl: '/App_Plugins/Core/ActivityLocation/backoffice/location-edit.html',
@@ -10,11 +23,14 @@
                 configModel: '=config',
                 mode: '@'
             },
-            link: function () {}
+            link: function(scope, elem, attrs) {
+                assetsService.load(['/App_Plugins/Core/ActivityLocation/activityLocationEdit.js'])
+                    .then(function () {init(scope, elem, attrs)});
+            }
         };
     }
-
-    //factory.$inject = ['assetsService'];
+   
+    factory.$inject = ['assetsService'];
 
     angular.module('umbraco').directive('locationEdit', factory);
 })();
