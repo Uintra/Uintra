6d118b5bf5d6f684f843675765c08624c3cc3031
m.kilianovskyi <mki@compent.net>
2018-01-09 15:05:13 +0200

    Refactor location edit and move logic into reusable file

--- src/Compent.uIntra/App_Plugins/Core/ActivityLocation/Edit/edit.js
+++ src/Compent.uIntra/App_Plugins/Core/ActivityLocation/Edit/edit.js
@@ -1,4 +1,5 @@
 ﻿require('./styles.css');
+import initEdit from './../activityLocationEdit';
 
 var init = function () {
     var locationContainer = document.querySelector(".js-location-edit");
@@ -6,92 +7,11 @@ var init = function () {
         return;
     }
 
-    var defaultMapOptions = {
-        center: { lat: 55.676098, lng: 12.568337 },
-        zoom: 10,
-        disableDoubleClickZoom: true
-    };
+    let addressHolder = locationContainer.querySelector("#js-map-address");
+    let shortAddressHolder = locationContainer.querySelector("#js-map-short");
+    let mapContainer = locationContainer.querySelector("#js-map-container");
 
-    const denmarkRegionCode = "dk";
-
-    var dataStorage = locationContainer.querySelector("#js-map-container");
-
-    var map = new google.maps.Map(dataStorage, defaultMapOptions);
-    var marker = new google.maps.Marker();
-
-    var address = locationContainer.querySelector("#js-map-address").value;
-    if (address) {
-        geocodeAddress(address, function (results, status) {
-            if (status == 'OK') {
-                placeMarkerAndPanTo(results[0].geometry.location);
-            }
-        });
-    } else {
-        placeMarkerAndPanTo(defaultMapOptions.center);
-    }
-
-    map.addListener('dblclick', function (e) {
-        placeMarkerAndPanTo(e.latLng);
-        placeNewAddress(e.latLng);
-    });
-
-    function placeMarkerAndPanTo(latLng) {
-        marker.setMap(null);
-        marker = new google.maps.Marker({
-            position: latLng,
-            map: map
-        });
-        map.panTo(latLng);
-    }
-
-    function placeNewAddress(latLng) {
-        var latlng = { lat: parseFloat(latLng.lat()), lng: parseFloat(latLng.lng()) };
-        geocodeLocation(latlng, function (results, status) {
-            if (status == 'OK') {
-                fillAddress(results[0]);
-            }
-        });
-    }
-
-    function geocodeAddress(address, callback) {
-        var geocoder = new google.maps.Geocoder;
-        geocoder.geocode({ 'address': address, 'region': denmarkRegionCode }, callback);
-    }
-
-    function geocodeLocation(latLng, callback) {
-        var geocoder = new google.maps.Geocoder;
-        geocoder.geocode({ 'location': latLng }, callback);
-    }
-
-    function fillAddress(geocoderData) {
-        locationContainer.querySelector("#js-map-address").value = geocoderData.formatted_address;
-
-        for (var i = 0; i < geocoderData.address_components.length; i++) {
-            var address = geocoderData.address_components[i];
-            if (address.types[0] == "locality")
-                locationContainer.querySelector("#js-map-short").value = address.long_name;
-        }
-    }
-
-    locationContainer.querySelector("#js-map-address").addEventListener("keypress", function (event) {
-        if (event.keyCode == 13) {
-            mapAddressChanged();
-        }
-    });
-
-    locationContainer.querySelector("#js-map-address").addEventListener("blur", function () {
-        mapAddressChanged();
-    });
-
-    function mapAddressChanged() {
-        var mapAddress = locationContainer.querySelector("#js-map-address").value;
-        geocodeAddress(mapAddress, function (results, status) {
-            if (status == 'OK') {
-                fillAddress(results[0]);
-                placeMarkerAndPanTo(results[0].geometry.location);
-            }
-        });
-    }
+    initEdit(addressHolder, shortAddressHolder, mapContainer);
 }
 
 export default init;
---
+++ src/Compent.uIntra/App_Plugins/Core/ActivityLocation/activityLocationEdit.js
@@ -0,0 +1,89 @@
+﻿function initActivityLocationEdit(addressHolder, shortAddressHolder, mapContainer) {
+
+    var defaultMapOptions = {
+        center: { lat: 55.676098, lng: 12.568337 },
+        zoom: 10,
+        disableDoubleClickZoom: true
+    };
+
+    const denmarkRegionCode = "dk";
+
+    var map = new google.maps.Map(mapContainer, defaultMapOptions);
+    var marker = new google.maps.Marker();
+
+    var address = addressHolder.value;
+    if (address) {
+        geocodeAddress(address, function (results, status) {
+            if (status == 'OK') {
+                placeMarkerAndPanTo(results[0].geometry.location);
+            }
+        });
+    } else {
+        placeMarkerAndPanTo(defaultMapOptions.center);
+    }
+
+    map.addListener('dblclick', function (e) {
+        placeMarkerAndPanTo(e.latLng);
+        placeNewAddress(e.latLng);
+    });
+
+    function placeMarkerAndPanTo(latLng) {
+        marker.setMap(null);
+        marker = new google.maps.Marker({
+            position: latLng,
+            map: map
+        });
+        map.panTo(latLng);
+    }
+
+    function placeNewAddress(latLng) {
+        var latlng = { lat: parseFloat(latLng.lat()), lng: parseFloat(latLng.lng()) };
+        geocodeLocation(latlng, function (results, status) {
+            if (status == 'OK') {
+                fillAddress(results[0]);
+            }
+        });
+    }
+
+    function geocodeAddress(address, callback) {
+        var geocoder = new google.maps.Geocoder;
+        geocoder.geocode({ 'address': address, 'region': denmarkRegionCode }, callback);
+    }
+
+    function geocodeLocation(latLng, callback) {
+        var geocoder = new google.maps.Geocoder;
+        geocoder.geocode({ 'location': latLng }, callback);
+    }
+
+    function fillAddress(geocoderData) {
+        addressHolder.value = geocoderData.formatted_address;
+
+        for (var i = 0; i < geocoderData.address_components.length; i++) {
+            var address = geocoderData.address_components[i];
+            if (address.types[0] == "locality")
+                shortAddressHolder.value = address.long_name;
+        }
+    }
+
+    addressHolder.addEventListener("keypress", function (event) {
+        if (event.keyCode == 13) {
+            mapAddressChanged();
+        }
+    });
+
+    addressHolder.addEventListener("blur", function () {
+        mapAddressChanged();
+    });
+
+    function mapAddressChanged() {
+        var mapAddress = addressHolder.value;
+        geocodeAddress(mapAddress, function (results, status) {
+            if (status == 'OK') {
+                fillAddress(results[0]);
+                placeMarkerAndPanTo(results[0].geometry.location);
+            }
+        });
+    }
+}
+
+export default initActivityLocationEdit;
