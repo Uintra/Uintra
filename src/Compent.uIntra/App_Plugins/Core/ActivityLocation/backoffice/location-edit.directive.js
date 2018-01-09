6bd3a9ecc1d687400349ceed984f1296ed7c23c0
m.kilianovskyi <mki@compent.net>
2018-01-09 13:58:12 +0200

    Create directive for editing activity location at backoffice

---
+++ src/Compent.uIntra/App_Plugins/Core/ActivityLocation/backoffice/location-edit.directive.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    'use strict';
+
+
+    var factory = function () {
+        return {
+            restrict: 'E',
+            templateUrl: '/App_Plugins/Core/ActivityLocation/backoffice/location-edit.html',
+            scope: {
+                model: '=',
+                configModel: '=config',
+                mode: '@'
+            },
+            link: function ($scope) {
+                //$scope.config = angular.extend({}, defaultConfig, $scope.configModel);
+                //$scope.isSingleMode = isSingleMode($scope.mode);
+
+                //var internalPicker = internalPickerFactory($q, dialogService, contentResource, entityResource, $scope.config.internalPicker);
+                //var mediaPicker = mediaPickerFactory($q, dialogService);
+                //init($scope, internalPicker, mediaPicker);
+            }
+        };
+    }
+
+    //factory.$inject = ['$q', 'dialogService', 'contentResource', 'entityResource'];
+
+    angular.module('umbraco').directive('locationEdit', factory);
+})();
+
+
