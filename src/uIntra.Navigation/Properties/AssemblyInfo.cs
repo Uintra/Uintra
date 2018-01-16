695fd7b5cc79c425e42efecba47acd158f3b16d2
aleksandr.chekanyn <alc@compent.net>
2018-01-16 18:20:11 +0200

    Update packages.

--- src/uIntra.Navigation/Properties/AssemblyInfo.cs
+++ src/uIntra.Navigation/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("The navigation is positioned in the left side of uIntra and serves as a overview of the pages and their sub-pages. It is also possible to add" +
                                " links to the navigation for quick access.")]
 
-[assembly: AssemblyVersion("1.2.10.0")]
+[assembly: AssemblyVersion("1.2.11.0")]
 [assembly: ComVisible(false)]
--- src/uIntra.Search/Properties/AssemblyInfo.cs
+++ src/uIntra.Search/Properties/AssemblyInfo.cs
@@ -5,6 +5,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("The search bar is visible from all pages of uIntra. It is based on elastic search. The search bar will attempt to help the user by auto-suggest" +
                                " search key words.")]
 
-[assembly: AssemblyVersion("0.2.2.0")]
+[assembly: AssemblyVersion("0.2.3.0")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4d9f910e-24e0-43b3-bccb-fef527d5ff2f")]
