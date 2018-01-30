9182046069aac382598e4ee6da261fa3bc10bc29
Dmitry Bondar <dbo@compent.net>
2018-01-30 16:02:06 +0200

    UC-1603: Refactor CustomAuthorizeAttribute

--- src/uIntra.Core/Core/Attributes/CustomAuthorizeAttribute.cs
+++ src/uIntra.Core/Core/Attributes/CustomAuthorizeAttribute.cs
@@ -1,13 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web.Mvc;
-using Umbraco.Core;
+using Umbraco.Core.IO;
 
 namespace uIntra.Core.Attributes
 {
     public class CustomAuthorizeAttribute : AuthorizeAttribute
     {
-        private static readonly string[] UmbracoIdentifier = { "install", "umbraco" };
+        private static List<string> _reservedPaths;
+
+        private static List<string> ReservedPaths
+        {
+            get
+            {
+                if (_reservedPaths != null) return _reservedPaths;
+
+                var umbracoReservedPaths = ConfigurationManager.AppSettings["umbracoReservedPaths"] ?? string.Empty;
+
+                _reservedPaths = umbracoReservedPaths
+                    .Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries)
+                    .Where(path => !string.IsNullOrWhiteSpace(path))
+                    .Select(path => IOHelper.ResolveUrl(path.Trim()).Trim().ToLower())
+                    .Where(path => path.Length > 0)
+                    .Select(path => path + (path.EndsWith("/") ? string.Empty : "/"))
+                    .ToList();
+
+                return _reservedPaths;
+            }
+        }
 
         public override void OnAuthorization(AuthorizationContext filterContext)
         {
@@ -15,6 +37,15 @@ namespace uIntra.Core.Attributes
             base.OnAuthorization(filterContext);
         }
 
-        private static bool IsUriAllowed(Uri uri) => uri.Segments.Any(s => s.ContainsAny(UmbracoIdentifier));
+        private static bool IsUriAllowed(Uri uri)
+        {
+            var pathPart = uri.AbsolutePath.Split('?')[0];
+            if (!pathPart.Contains(".") && !pathPart.EndsWith("/"))
+            {
+                pathPart += "/";
+            }
+
+            return ReservedPaths.Exists(path => path.Equals(pathPart.ToLowerInvariant()));
+        }
     }
 }
