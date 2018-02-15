b29a7d4c694eda932ee929198a23e6845f92ceab
Dmitry Bondar <dbo@compent.net>
2018-02-15 09:55:20 +0200

    Update packages

--- src/uIntra/Properties/AssemblyInfo.cs
+++ src/uIntra/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 //[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyVersion("0.2.30.0")]
+[assembly: AssemblyVersion("0.2.31.0")]
