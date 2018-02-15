314cc2b6379a96aebfaf9ed38589a2e729b23ba6
Dmitry Bondar <dbo@compent.net>
2018-02-15 16:01:50 +0200

    Increase package versions

--- src/uIntra.Notification/Properties/AssemblyInfo.cs
+++ src/uIntra.Notification/Properties/AssemblyInfo.cs
@@ -5,6 +5,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Notifications works as know from other social media, however it is also possible to receive notifications as email. The user can set up" +
                                " which notifications they would like to receive in emails.")]
 
-[assembly: AssemblyVersion("1.3.10.0")]
+[assembly: AssemblyVersion("1.3.11.0")]
 [assembly: ComVisible(false)]
 [assembly: Guid("774eba5a-679c-401d-b3f1-130562785c60")]
