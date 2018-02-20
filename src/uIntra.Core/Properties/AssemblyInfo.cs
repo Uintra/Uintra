c6a980c7e9313d0859b8f9f1e283bcf9aabfbcf1
Dmitry Bondar <dbo@compent.net>
2018-02-20 15:57:51 +0200

    Increase Core assembly version

--- src/uIntra.Core/Properties/AssemblyInfo.cs
+++ src/uIntra.Core/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Uintra.Core")]
 [assembly: AssemblyDescription("Core activity package.")]
 
-[assembly: AssemblyVersion("1.4.17.0")]
+[assembly: AssemblyVersion("1.4.18.0")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3f8c785a-48d0-41f5-9257-6f73afca41eb")]
