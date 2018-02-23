48abd043deecd1564cc03b780eefc5178d6e7a65
Dmitry Bondar <dbo@compent.net>
2018-02-23 15:41:55 +0200

    Add ActivityLinkPreviewService

---
+++ src/uIntra.Core/Core/LinkPreview/ActivityLinkPreviewService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using Uintra.Core.LinkPreview.Sql;
+using Uintra.Core.Persistence;
+
+namespace Uintra.Core.LinkPreview
+{
+    public class ActivityLinkPreviewService : IActivityLinkPreviewService
+    {
+        private readonly ISqlRepository<int, ActivityToLinkPreviewEntity> _previewRelationRepository;
+        private readonly ISqlRepository<int, LinkPreviewEntity> _previewRepository;
+        private readonly LinkPreviewModelMapper _linkPreviewModelMapper;
+
+        public ActivityLinkPreviewService(
+            ISqlRepository<int, ActivityToLinkPreviewEntity> previewRelationRepository,
+            ISqlRepository<int, LinkPreviewEntity> previewRepository,
+            LinkPreviewModelMapper linkPreviewModelMapper)
+        {
+            _previewRelationRepository = previewRelationRepository;
+            _previewRepository = previewRepository;
+            _linkPreviewModelMapper = linkPreviewModelMapper;
+        }
+
+        public LinkPreview GetActivityLinkPreview(Guid activityId)
+        {
+            var previewIds = _previewRelationRepository.FindAll(r => r.ActivityId == activityId).Select(r => r.LinkPreviewId);
+            var preview = _previewRepository.GetMany(previewIds).Select(_linkPreviewModelMapper.MapPreview).SingleOrDefault();
+            return preview;
+        }
+
+        public void UpdateLinkPreview(Guid commentId, int previewId)
+        {
+            RemovePreviewRelations(commentId);
+            AddLinkPreview(commentId, previewId);
+        }
+
+        public void AddLinkPreview(Guid activityId, int previewId)
+        {
+            var entity = new ActivityToLinkPreviewEntity { ActivityId = activityId, LinkPreviewId = previewId };
+            _previewRelationRepository.Add(entity);
+        }
+
+        public void RemovePreviewRelations(Guid activityId)
+        {
+            var relations = _previewRelationRepository.FindAll(r => r.ActivityId == activityId).ToList();
+            var previewIds = relations.Select(r => r.LinkPreviewId).ToList();
+            _previewRelationRepository.Delete(relations);
+            foreach (var previewId in previewIds)
+            {
+                _previewRepository.Delete(previewId);
+            }
+        }
+    }
+}
