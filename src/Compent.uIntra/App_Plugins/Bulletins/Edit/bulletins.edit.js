2182ed763f4ff89a1e9a825ac059c320fc634f70
Dmitry Bondar <dbo@compent.net>
2018-03-01 17:32:39 +0200

    UC-1754: Link preview. Some links look broken. See attached.

--- src/Compent.uIntra/App_Plugins/Bulletins/Edit/bulletins.edit.js
+++ src/Compent.uIntra/App_Plugins/Bulletins/Edit/bulletins.edit.js
@@ -70,15 +70,15 @@ function initEditor() {
 
         divElem.innerHTML =
             `<button type="button" class="link-preview__close js-link-preview-remove-preview">X</button>
-                <div class="link-preview__image">
-                     <img src="${data.imageUri}" />
-                 </div>
+                <div class="link-preview__image">` +
+            (data.imageUri ? `<img src="${data.imageUri}" />` : '') +
+            `</div>
                 <div class="link-preview__text">
                     <h3 class="link-preview__title">
                         <a href="${data.uri}">${data.title}</a>
-                    </h3>
-                    <p>${data.description}</p>
-                </div>`;
+                    </h3>` +
+            (data.description ? `<p>${data.description}</p>` : "") +
+            "</div>";
 
         return divElem;
     }
