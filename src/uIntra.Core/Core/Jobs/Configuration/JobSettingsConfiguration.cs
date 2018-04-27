a5eaa9b815244716e79ab0d56137a6bf2f91f2a7
aleksandr.chekanyn <alc@compent.net>
2018-04-27 09:05:42 +0300

    Fix job transformation naming.

--- src/uIntra.Core/Core/Jobs/Configuration/JobSettingsConfiguration.cs
+++ src/uIntra.Core/Core/Jobs/Configuration/JobSettingsConfiguration.cs
@@ -10,7 +10,7 @@ namespace Uintra.Core.Jobs.Configuration
     public class JobSettingsConfiguration : ConfigurationSection, IJobSettingsConfiguration
     {
         public static JobSettingsConfiguration Configure =>
-            ConfigurationManager.GetSection("UIntraJobs") as JobSettingsConfiguration;
+            ConfigurationManager.GetSection("UintraJobs") as JobSettingsConfiguration;
 
         [ConfigurationProperty("settings", IsRequired = true)]
         public JobSettingsCollection Settings => (JobSettingsCollection)base["settings"];
