0bf5abd9c35e6971a330081c170e37cd0e21bef0
aleksandr.chekanyn <alc@compent.net>
2018-04-30 13:46:08 +0300

    UC-1947 Translations needed.

--- src/Compent.uIntra/Core/Updater/Migrations/0.3.0.0/Steps/AddTranslationsStep.cs
+++ src/Compent.uIntra/Core/Updater/Migrations/0.3.0.0/Steps/AddTranslationsStep.cs
@@ -1,31 +1,30 @@
-﻿using Compent.Uintra.Core.Updater.Migrations._0._0._0._1;
+﻿using System.Collections.Generic;
+using Compent.Uintra.Core.Updater.Migrations._0._0._0._1;
 using Localization.Core;
 
 namespace Compent.Uintra.Core.Updater.Migrations._0._3._0._0.Steps
 {
     public class AddTranslationsStep : IMigrationStep
     {
-        private const string PopupNotificationOkBtnTextKey = "PopupNotification.OkBtn.Text";
-        private const string LightboxGalleryCountOneKey = "LightboxGallery.Count.One.lbl";
-        private const string LightboxGalleryCountManyKey = "LightboxGallery.Count.Many.lbl";
-        private const string ProfileLinkTitleKey = "PopupNotification.ProfileLink.Title";
-        private const string SubscribeSettingsTitleKey = "SubscribeSettings.Title.lbl";
-        private const string SubscribeSettingsSubscribeNotesKey = "SubscribeSettings.SubscribeNotes.lbl";
-        private const string EventsDescriptionPlaceholderKey = "Events.Description.placeholder";
-        private const string PinAcceptPinTextKey = "Pin.AcceptPinText.lbl";
-        private const string GroupRoomNavigationAllKey = "GroupRoom.Navigation.All.lbl";
-        private const string ProfileEmailNotificationsTitleKey = "Profile.EmailNotifications.Title.lbl";
+        private readonly Dictionary<string, string> _translations = new Dictionary<string, string>()
+        {
+            {"PopupNotification.OkBtn.Text", "Close"},
+            {"LightboxGallery.Count.One.lbl", "File added"},
+            {"LightboxGallery.Count.Many.lbl", "Files added"},
+            {"PopupNotification.ProfileLink.Title", "Go to user profile"},
+            {"SubscribeSettings.Title.lbl", "Event participation"},
+            {"Events.Description.placeholder", "Description"},
+            {
+                "Pin.AcceptPinText.lbl",
+                "Note: Enabling this will mark the post as \"Important\". It will be pinned to the top until you manually unpin it. To automatically unpin the post set the date below, the post will remain being marked \"Important\""
+            },
+            {"GroupRoom.Navigation.All.lbl", "Feed"},
+            {"Profile.EmailNotifications.Title.lbl", "Notifications"},
+            {"GroupDocument.Upload.lbl", "Upload new group document"},
+            {"DocumentsList.Empty.lbl", "Documents list is empty"},
+            {"GroupEdit.GroupImage.note", "Group image"}
 
-        private const string PopupNotificationOkBtnTextTranslation = "Close";
-        private const string LightboxGalleryCountOneTranslation = "File added";
-        private const string LightboxGalleryCountManyTranslation = "Files added";
-        private const string ProfileLinkTitleTranslation = "Go to user profile";
-        private const string SubscribeSettingsTitleTranslation = "Event participation";
-        private const string SubscribeSettingsSubscribeNotesTranslation = "SubscribeSettings.SubscribeNotes.lbl";
-        private const string EventsDescriptionPlaceholderTranslation = "Description";
-        private const string PinAcceptPinTextTranslation = "Note: Enabling this will mark the post as \"Important\". It will be pinned to the top until you manually unpin it. To automatically unpin the post set the date below, the post will remain being marked \"Important\"";
-        private const string GroupRoomNavigationAllTranslation= "Feed";
-        private const string ProfileEmailNotificationsTitleTranslation = "Notifications";
+        };
 
         private readonly ILocalizationCoreService _localizationCoreService;
 
@@ -36,32 +35,21 @@ namespace Compent.Uintra.Core.Updater.Migrations._0._3._0._0.Steps
 
         public ExecutionResult Execute()
         {
-            InstallationStepsHelper.AddTranslation(PopupNotificationOkBtnTextKey, PopupNotificationOkBtnTextTranslation);
-            InstallationStepsHelper.AddTranslation(LightboxGalleryCountOneKey, LightboxGalleryCountOneTranslation);
-            InstallationStepsHelper.AddTranslation(LightboxGalleryCountManyKey, LightboxGalleryCountManyTranslation);
-            InstallationStepsHelper.AddTranslation(ProfileLinkTitleKey, ProfileLinkTitleTranslation);
-            InstallationStepsHelper.AddTranslation(SubscribeSettingsTitleKey, SubscribeSettingsTitleTranslation);
-            InstallationStepsHelper.AddTranslation(SubscribeSettingsSubscribeNotesKey, SubscribeSettingsSubscribeNotesTranslation);
-            InstallationStepsHelper.AddTranslation(EventsDescriptionPlaceholderKey, EventsDescriptionPlaceholderTranslation);
-            InstallationStepsHelper.AddTranslation(PinAcceptPinTextKey, PinAcceptPinTextTranslation);
-            InstallationStepsHelper.AddTranslation(GroupRoomNavigationAllKey, GroupRoomNavigationAllTranslation);
-            InstallationStepsHelper.AddTranslation(ProfileEmailNotificationsTitleKey, ProfileEmailNotificationsTitleTranslation);
+            foreach (var translation in _translations)
+            {
+                InstallationStepsHelper.AddTranslation(translation.Key, translation.Value);
+            }
 
             return ExecutionResult.Success;
         }
 
         public void Undo()
         {
-            _localizationCoreService.Delete(PopupNotificationOkBtnTextKey);
-            _localizationCoreService.Delete(LightboxGalleryCountOneKey);
-            _localizationCoreService.Delete(LightboxGalleryCountManyKey);
-            _localizationCoreService.Delete(ProfileLinkTitleKey);
-            _localizationCoreService.Delete(SubscribeSettingsTitleKey);
-            _localizationCoreService.Delete(SubscribeSettingsSubscribeNotesKey);
-            _localizationCoreService.Delete(EventsDescriptionPlaceholderKey);
-            _localizationCoreService.Delete(PinAcceptPinTextKey);
-            _localizationCoreService.Delete(GroupRoomNavigationAllKey);
-            _localizationCoreService.Delete(ProfileEmailNotificationsTitleKey);
+            foreach (var translationKey in _translations.Keys)
+            {
+                _localizationCoreService.Delete(translationKey);
+            }
+
         }
     }
 }
