1e7f4315cf9e12c3da6cace059eeac3a3316c2bb
aleksandr.chekanyn <alc@compent.net>
2018-05-02 16:32:05 +0300

    UC-1977 Activities. Edit activity: thumbnails should be shown to mp4

--- src/uIntra.Core/Properties/AssemblyInfo.cs
+++ src/uIntra.Core/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Uintra.Core")]
 [assembly: AssemblyDescription("Core activity package.")]
 
-[assembly: AssemblyVersion("1.5.1.0")]
+[assembly: AssemblyVersion("1.5.2.0")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3f8c785a-48d0-41f5-9257-6f73afca41eb")]
