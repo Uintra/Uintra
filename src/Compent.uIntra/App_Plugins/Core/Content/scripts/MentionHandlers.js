a013737a281ea55c76c37abafefe76e2e79427a9
Andrey Datsenko <and@compent.net>
2018-09-10 14:55:05 +0300

    UC-2256 Client feedback. Users tag: behaviour should be changed

---
+++ src/Compent.uIntra/App_Plugins/Core/Content/scripts/MentionHandlers.js
@@ -0,0 +1,9 @@
+﻿export default function () {
+    $('.mention').each(function (i, e) {
+        $(this).on('click', function (e) {
+            var id = $(this).data('id');
+            location.href = '/profile?id=' + id;
+            e.preventDefault();
+        })
+    });
+}
