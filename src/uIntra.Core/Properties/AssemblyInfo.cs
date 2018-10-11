8896f1b93397c3972aebc5fda343deefd398ae4f
Maxim Zhigulsky <maz@compent.net>
2018-10-11 16:16:28 +0300

    Updated web config xdt file.

--- src/uIntra.Core/Properties/AssemblyInfo.cs
+++ src/uIntra.Core/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Uintra.Core")]
 [assembly: AssemblyDescription("Core activity package.")]
 
-[assembly: AssemblyVersion("1.7.0.2")]
+[assembly: AssemblyVersion("1.7.0.3")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3f8c785a-48d0-41f5-9257-6f73afca41eb")]
