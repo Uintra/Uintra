d47aa7a83f38162721864fec6deb7d8bce1a6eaf
Maxim Zhigulsky <maz@compent.net>
2018-10-17 14:25:37 +0300

    Added build tasks.

---
+++ src/uIntra/RemoveInvalidAssemblyBindings.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Uintra
+{
+    public class RemoveInvalidAssemblyBindings : Task
+    {
+        public override bool Execute()
+        {
+            Log.LogMessage(MessageImportance.High, "Removing invalid assembly dependency bindings!");
+            return true;
+        }
+    }
+}
