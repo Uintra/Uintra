093b1262997d6741a81f15d86759631c2d936507
Maxim Zhigulsky <maz@compent.net>
2018-10-17 18:14:30 +0300

    Build tasks moved to separate project.

---
+++ src/Uintra.Tasks/RemoveInvalidAssemblyBindings.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Microsoft.Web.XmlTransform;
+using System;
+
+namespace Uintra.Tasks
+{
+    public class RemoveInvalidAssemblyBindings : Task
+    {
+        private string xdtFilePath;
+        private string webConfigPath;
+
+        [Required]
+        public string XdtFilePath
+        {
+            get
+            {
+                return xdtFilePath;
+            }
+            set
+            {
+                xdtFilePath = value;
+            }
+        }
+
+        [Required]
+        public string WebConfigPath
+        {
+            get
+            {
+                return webConfigPath;
+            }
+            set
+            {
+                webConfigPath = value;
+            }
+        }
+
+        public override bool Execute()
+        {
+            try
+            {
+                XmlTransformableDocument transformableDocument = new XmlTransformableDocument();
+                transformableDocument.PreserveWhitespace = true;
+                transformableDocument.Load(WebConfigPath);
+
+                using (XmlTransformation xmlTransformation = new XmlTransformation(XdtFilePath))
+                    xmlTransformation.Apply(transformableDocument);
+
+                transformableDocument.Save(WebConfigPath);
+                transformableDocument.Dispose();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            //Log.LogMessage(MessageImportance.High, "Removed invalid assembly dependency bindings! " + this.XdtFilePath + "\r\n" + this.WebConfigPath);
+            return true;
+        }
+    }
+}
