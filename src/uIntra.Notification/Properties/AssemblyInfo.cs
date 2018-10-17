5f602f9f0724cd20d37f2500c0f118bca42471c8
Maxim Zhigulsky <maz@compent.net>
2018-10-17 22:54:40 +0300

    Uintra.Notifications (Removed invalid reference)

--- src/uIntra.Notification/Properties/AssemblyInfo.cs
+++ src/uIntra.Notification/Properties/AssemblyInfo.cs
@@ -5,6 +5,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Notifications works as know from other social media, however it is also possible to receive notifications as email. The user can set up" +
                                " which notifications they would like to receive in emails.")]
 
-[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.5.0.1")]
 [assembly: ComVisible(false)]
 [assembly: Guid("774eba5a-679c-401d-b3f1-130562785c60")]
