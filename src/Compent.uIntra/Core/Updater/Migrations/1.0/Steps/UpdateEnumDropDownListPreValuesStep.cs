43af8b498ab0658bad7142257f492c93a5832284
Maxim Zhigulsky <maz@compent.net>
2018-11-14 19:12:48 +0200

    UC-2418: Add migrations for fixing enum (folders), related user property (members) properties.

---
+++ src/Compent.uIntra/Core/Updater/Migrations/1.0/Steps/UpdateEnumDropDownListPreValuesStep.cs
@@ -0,0 +1,41 @@
+﻿using Uintra.Core.Media;
+using Uintra.Notification.Configuration;
+using Umbraco.Core;
+using Umbraco.Core.Services;
+using static Compent.Uintra.Core.Updater.Migrations._0._0._0._1.Constants.NotificationInstallationConstants;
+
+namespace Compent.Uintra.Core.Updater.Migrations._1._0.Steps
+{
+    public class UpdateEnumDropDownListPreValuesStep : IMigrationStep
+    {
+        public ExecutionResult Execute()
+        {
+            var dataTypeService = ApplicationContext.Current.Services.DataTypeService;
+
+            UpdateDatatypePreValues(dataTypeService, FolderConstants.DataTypeName, 
+                FolderConstants.EnumAssemblyDll, typeof(MediaFolderTypeEnum).FullName);
+            UpdateDatatypePreValues(dataTypeService, DataTypeNames.NotificationTypeEnum,
+                DataTypePropertyPreValues.Assembly, typeof(NotificationTypeEnum).FullName); //NotificationInstallationConstants.DataTypePropertyPreValues.Enum
+
+            return ExecutionResult.Success;
+        }
+
+        public void Undo()
+        {
+        }
+        private void UpdateDatatypePreValues(IDataTypeService dataTypeService, string dataTypeName,
+            string assemblyFullNamePreValue, string enumFullNamePreValue)
+        {
+            var dataType = dataTypeService.GetDataTypeDefinitionByName(dataTypeName);
+            if (dataType != null)
+            {
+                var preValues = dataTypeService.GetPreValuesCollectionByDataTypeId(dataType.Id);
+                var dictionary = preValues.FormatAsDictionary();
+                dictionary[FolderConstants.PreValueAssemblyAlias].Value = assemblyFullNamePreValue;
+                dictionary[FolderConstants.PreValueEnumAlias].Value = enumFullNamePreValue;
+
+                dataTypeService.SavePreValues(dataType, dictionary);
+            }
+        }
+    }
+}
---
+++ src/Compent.uIntra/Core/Updater/Migrations/1.0/Steps/UpdateRelatedUserPropertyStep.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core;
+using Umbraco.Core.Models;
+using static Compent.Uintra.Core.Updater.Migrations._0._0._0._1.Constants.UsersInstallationConstants;
+
+namespace Compent.Uintra.Core.Updater.Migrations._1._0.Steps
+{
+    public class UpdateRelatedUserPropertyStep : IMigrationStep
+    {
+        public ExecutionResult Execute()
+        {
+            var dataTypeService = ApplicationContext.Current.Services.DataTypeService;
+            var relatedUserDataType =
+                dataTypeService.GetDataTypeDefinitionByName(DataTypeNames.MemberUserPicker);
+            if (relatedUserDataType != null && !relatedUserDataType.PropertyEditorAlias.Equals(
+                DataTypePropertyEditors.MemberUserPicker))
+            {
+                relatedUserDataType.PropertyEditorAlias = DataTypePropertyEditors.MemberUserPicker;
+                dataTypeService.Save(relatedUserDataType);
+            }
+            return ExecutionResult.Success;
+        }
+
+        public void Undo()
+        {
+        }
+    }
+}
