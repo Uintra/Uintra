52bb669ef51fb365e8232ccc6445af37190286f0
aleksandr.chekanyn <alc@compent.net>
2018-11-15 12:49:29 +0200

    UC-2395 [API] Create member request. [400]Empty body

--- src/uIntra.Core/Core/User/DTO/UpdateUserDto.cs
+++ src/uIntra.Core/Core/User/DTO/UpdateUserDto.cs
@@ -1,13 +1,20 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Uintra.Core.User.DTO
 {
     public class UpdateUserDto
     {
         public Guid Id { get; set; }
+
+        [StringLength(128, MinimumLength = 2, ErrorMessage = "Allowed length 2 - 128")]
         public string FirstName { get; set; }
+
+        [StringLength(128, MinimumLength = 2, ErrorMessage = "Allowed length 2 - 128")]
         public string LastName { get; set; }
+
         public int? NewMedia { get; set; }
+
         public bool DeleteMedia { get; set; }
     }
 }
