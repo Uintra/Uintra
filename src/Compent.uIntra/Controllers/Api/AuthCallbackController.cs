90beeabf73715368a7b1e5b497fdb3ec5f2c41f1
Maxim Zhigulsky <maz@compent.net>
2018-11-22 16:34:44 +0200

    Moved attribute to controller location

--- src/Compent.uIntra/Controllers/Api/AuthCallbackController.cs
+++ src/Compent.uIntra/Controllers/Api/AuthCallbackController.cs
@@ -1,5 +1,4 @@
 ﻿using Compent.Uintra.Core.Sync;
-using Compent.Uintra.Core.Sync.Attributes;
 using Compent.Uintra.Core.Sync.Models;
 using System;
 using System.Web.Mvc;
@@ -10,7 +9,7 @@ namespace Compent.Uintra.Controllers.Api
     public class AuthCallbackController : Google.Apis.Auth.OAuth2.Mvc.Controllers.AuthCallbackController
     {
         private GoogleSyncSettingsModel _settingsModel;
-       
+
         protected override Google.Apis.Auth.OAuth2.Mvc.FlowMetadata FlowData
         {
             get
@@ -23,6 +22,16 @@ namespace Compent.Uintra.Controllers.Api
         public ActionResult Error(Exception e)
         {
             return View(Constants.GoogleAuthWindowViewPath, e);
+        }
+    }
+
+    public class AuthCallbackExceptionFilterAttribute : FilterAttribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {
+            filterContext.ExceptionHandled = true;
+            var controller = (AuthCallbackController)filterContext.Controller;
+            filterContext.Result = controller.Error(filterContext.Exception);
         }
     }
 }
--- src/Compent.uIntra/Core/Sync/Attributes/AuthCallbackExceptionFilterAttribute.cs
+++ src/Compent.uIntra/Core/Sync/Attributes/AuthCallbackExceptionFilterAttribute.cs
@@ -1,15 +1,1 @@
-﻿using Compent.Uintra.Controllers.Api;
-using System.Web.Mvc;
-
-namespace Compent.Uintra.Core.Sync.Attributes
-{
-    public class AuthCallbackExceptionFilterAttribute : FilterAttribute, IExceptionFilter
-    {
-        public void OnException(ExceptionContext filterContext)
-        {
-            filterContext.ExceptionHandled = true;
-            var controller = (AuthCallbackController)filterContext.Controller;
-            filterContext.Result = controller.Error(filterContext.Exception);
-        }
-    }
-}
+﻿
