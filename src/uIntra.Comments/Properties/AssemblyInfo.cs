689ec26438a170191e7db68b77f9eab5d7bbbe1b
Maxim Zhigulsky <maz@compent.net>
2018-11-22 13:04:32 +0200

    Increment packages version.

--- src/uIntra.Comments/Properties/AssemblyInfo.cs
+++ src/uIntra.Comments/Properties/AssemblyInfo.cs
@@ -5,6 +5,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Comments as known from other social media platforms can be added to all types of posts and other comments as well. Commenting on a comment" +
                                " will result in a nested comment. All user roles can comment.")]
 
-[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
 [assembly: ComVisible(false)]
 [assembly: Guid("54e782d4-8298-4b78-8d7a-cde398521374")]
