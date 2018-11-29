52ae975cd577ec943ac7ddaa43e0d4653dc1a3a7
Denis Prodan <dpr@compent.net>
2018-11-29 14:03:32 +0200

    UC-2446 Search by tag. Full compatibility should be on the top

--- src/uIntra.Search/Core/Entities/Mappings/SearchableBaseMap.cs
+++ src/uIntra.Search/Core/Entities/Mappings/SearchableBaseMap.cs
@@ -6,10 +6,10 @@ namespace Uintra.Search
     {
         public SearchableBaseMap()
         {
-            Text(t => t.Name(n => n.Id));
             Text(t => t.Name(n => n.Title).Analyzer(ElasticHelpers.ReplaceNgram));
             Number(t => t.Name(n => n.Type).Type(NumberType.Integer));
-            Text(t => t.Name(n => n.Url));
+            Keyword(t => t.Name(n => n.Id));            
+            Keyword(t => t.Name(n => n.Url));
         }
     }
 }
