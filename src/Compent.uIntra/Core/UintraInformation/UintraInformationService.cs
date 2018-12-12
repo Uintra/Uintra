eb5133ce271aa2ad6d214ab26ab3e2c08e30c17b
aleksandr.chekanyn <alc@compent.net>
2018-12-12 14:49:35 +0200

    UC-1816 show version number in product

---
+++ src/Compent.uIntra/Core/UintraInformation/UintraInformationService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Uintra.Core
+{
+    public class UintraInformationService : IUintraInformationService
+    {
+        public Version Version { get; }
+        public Uri DocumentationLink { get; }
+
+        public UintraInformationService()
+        {
+            Version = GetVersion();
+            DocumentationLink = GetDocumentationLink();
+        }
+
+        private Version GetVersion()
+        {
+            var executingAssembly = Assembly.GetExecutingAssembly();
+            var fileVersionInfo = FileVersionInfo.GetVersionInfo(executingAssembly.Location);
+            return new Version(fileVersionInfo.ProductVersion);
+        }
+
+        private Uri GetDocumentationLink()
+        {
+            return new Uri("");
+        }
+    }
+}
