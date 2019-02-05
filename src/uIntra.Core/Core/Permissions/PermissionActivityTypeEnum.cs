21da560778842b9658ba78bac90e3cc32ada004a
Denis Prodan <dpr@compent.net>
2019-02-05 18:43:08 +0200

    fill activity types table

---
+++ src/uIntra.Core/Core/Permissions/PermissionActivityTypeEnum.cs
@@ -0,0 +1,12 @@
+﻿using Uintra.Core.Activity;
+
+namespace Uintra.Core.Permissions
+{
+    public enum PermissionActivityTypeEnum
+    {
+        News = IntranetActivityTypeEnum.News,
+        Events = IntranetActivityTypeEnum.Events,
+        Bulletins = IntranetActivityTypeEnum.Bulletins,
+        Common = 100
+    }
+}
