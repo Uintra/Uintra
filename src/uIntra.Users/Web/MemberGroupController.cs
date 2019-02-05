b5d79706c7fe21694deb94aa0c5e65a3bec9c98f
Maxim Zhigulsky <maz@compent.net>
2019-02-05 17:53:23 +0200

    Init.

---
+++ src/uIntra.Users/Web/MemberGroupController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Uintra.Users.Core.Models;
+using Umbraco.Core.Models;
+using Umbraco.Web.WebApi;
+
+namespace Uintra.Users.Web
+{
+    public class MemberGroupController : UmbracoAuthorizedApiController
+    {
+        public MemberGroupViewModel Get(int id)
+        {
+            var memberGroup = Services.MemberGroupService.GetById(id);
+            return new MemberGroupViewModel() { Id = memberGroup.Id, Name = memberGroup.Name };
+        }
+
+        public bool Create(string name)
+        {
+            Services.MemberGroupService.Save(new MemberGroup() { Name = name });
+            return true;
+        }
+
+        [HttpPost]
+        public bool Save(MemberGroupViewModel model)
+        {
+
+            var memberGroup = Services.MemberGroupService.GetById(model.Id);
+            memberGroup.Name = model.Name;
+            Services.MemberGroupService.Save(memberGroup);
+            return true;
+        }
+
+        public bool Delete(int id)
+        {
+            var memberGroup = Services.MemberGroupService.GetById(id);
+            Services.MemberGroupService.Delete(memberGroup);
+            return true;
+        }
+    }
+}
---
+++ src/uIntra.Users/Web/MemberGroupTreeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Threading.Tasks;
+using umbraco.BusinessLogic.Actions;
+using Umbraco.Web.Models.Trees;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.Trees;
+
+namespace Uintra.Users.Web
+{
+    [PluginController("uintra")]
+    [Tree("member", "memberGroups", "Member Groups", ".sprTreeFolder", ".sprTreeFolder_o", true, 1)]
+    public class MemberGroupTreeController : TreeController
+    {
+        protected override MenuItemCollection GetMenuForNode(string id, FormDataCollection queryStrings)
+        {
+            var menu = new MenuItemCollection();
+
+            if (id == "-1")
+            {
+                menu.Items.Add<CreateChildEntity, ActionNew>(Services.TextService.Localize(ActionNew.Instance.Alias, CultureInfo.CurrentCulture));
+                menu.Items.Add<RefreshNode, ActionRefresh>(Services.TextService.Localize(ActionRefresh.Instance.Alias, CultureInfo.CurrentCulture), true);
+                return menu;
+            }
+
+            menu.Items.Add<ActionDelete>(Services.TextService.Localize(ActionDelete.Instance.Alias, CultureInfo.CurrentCulture));
+            return menu;
+        }
+
+        protected override TreeNodeCollection GetTreeNodes(string id, FormDataCollection queryStrings)
+        {
+            var memberGroups = Services.MemberGroupService.GetAll();
+            var nodeCollection = new TreeNodeCollection();
+            foreach (var memberGroup in memberGroups)
+            {
+                var node = CreateTreeNode(memberGroup.Id.ToString(), id, queryStrings, memberGroup.Name, "icon-users", false);
+                nodeCollection.Add(node);
+            }
+            return nodeCollection;
+        }
+    }
+}
