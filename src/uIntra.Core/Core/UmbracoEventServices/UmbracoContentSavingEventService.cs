20d4268887d3fb442e5a3130ec5f326181ad237e
aleksandr.chekanyn <alc@compent.net>
2019-03-05 13:39:21 +0200

    UC-2651 [Demo] Search autosuggest and results are incorrect. Look at attachment

--- src/uIntra.Core/Core/UmbracoEventServices/UmbracoContentSavingEventService.cs
+++ src/uIntra.Core/Core/UmbracoEventServices/UmbracoContentSavingEventService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using Compent.Extensions;
+using LanguageExt;
 using Newtonsoft.Json.Linq;
 using Uintra.Core.Constants;
 using Uintra.Core.Core.UmbracoEventServices;
@@ -7,6 +8,7 @@ using Uintra.Core.Grid;
 using Umbraco.Core.Events;
 using Umbraco.Core.Models;
 using Umbraco.Core.Services;
+using static LanguageExt.Prelude;
 
 namespace Uintra.Core.UmbracoEventServices
 {
@@ -22,25 +24,99 @@ namespace Uintra.Core.UmbracoEventServices
 
         public void ProcessContentSaving(IContentService sender, SaveEventArgs<IContent> args)
         {
-            var contentPanels = args.SavedEntities
+            var panels = args.SavedEntities
                 .SelectMany(content => _gridHelper
-                    .GetValues(content, GridEditorConstants.ContentPanelAlias)
-                    .Select(x => x.value));
+                    .GetValues(content, 
+                        GridEditorConstants.ContentPanelAlias,
+                        GridEditorConstants.DocumentLibraryPanelAlias,
+                        GridEditorConstants.FaqPanelAlias,
+                        GridEditorConstants.GlobalPanelPickerAlias));
 
-            var result = contentPanels.All(IsValidContentPanel);
+            var validationResult = panels
+                .Select<(string alias, dynamic value), Validation<string, Unit>>(panel => IsValidPanel(panel.alias, panel.value))
+                .Sequence();
 
-            if (!result)
+            validationResult.IfFail(errors => errors
+                .Select(error => new EventMessage("Panel error", error, EventMessageType.Error))
+                .Iter(args.CancelOperation));
+        }
+
+
+        private static Validation<string, Unit> IsValidPanel(string gridEditorAlias, dynamic panel)
+        {
+            switch (gridEditorAlias)
             {
-                var message = new EventMessage("Content panel error", "empty content", EventMessageType.Error);
-                args.CancelOperation(message);
+                case GridEditorConstants.ContentPanelAlias:
+                    return IsValidContentPanel(panel);
+                case GridEditorConstants.DocumentLibraryPanelAlias:
+                    return IsValidDocumentPanel(panel);
+                case GridEditorConstants.FaqPanelAlias:
+                    return IsValidFaqPanel(panel);
+                case GridEditorConstants.GlobalPanelPickerAlias:
+                    return IsValidGlobalPanel(panel);
+                default:
+                    return unit;
             }
         }
 
-        private static bool IsValidContentPanel(dynamic contentPanel)
+        private static Validation<string, Unit> IsValidContentPanel(dynamic contentPanel)
         {
             var contentPanelObject = contentPanel as JObject;
             var contentPanelTitle = contentPanelObject?["title"]?.Value<string>();
-            return contentPanelObject != null && contentPanelTitle.HasValue();
+
+            if (contentPanelObject != null && contentPanelTitle.HasValue())
+            {
+                return unit;
+            }
+            else
+            {
+                return "Empty content panel";
+            }
+        }
+
+        private static Validation<string, Unit> IsValidDocumentPanel(dynamic documentPanel)
+        {
+            var documentPanelObject = documentPanel as JObject;
+            var documentPanelTitle = documentPanelObject?["title"]?.Value<string>();
+
+            if (documentPanelObject != null && documentPanelTitle.HasValue())
+            {
+                return unit;
+            }
+            else
+            {
+                return "Empty document panel";
+            }
+        }
+
+        private static Validation<string, Unit> IsValidFaqPanel(dynamic faqPanel)
+        {
+            var faqPanelObject = faqPanel as JObject;
+            var faqPanelTitle = faqPanelObject?["title"]?.Value<string>();
+
+            if (faqPanelObject != null && faqPanelTitle.HasValue())
+            {
+                return unit;
+            }
+            else
+            {
+                return "Empty FAQ panel";
+            }
+        }
+
+        private static Validation<string, Unit> IsValidGlobalPanel(dynamic faqPanel)
+        {
+            var faqPanelObject = faqPanel as JObject;
+            var faqPanelTitle = faqPanelObject?["id"]?.Value<string>();
+
+            if (faqPanelObject != null && faqPanelTitle.HasValue())
+            {
+                return unit;
+            }
+            else
+            {
+                return "Empty global panel";
+            }
         }
     }
 }
