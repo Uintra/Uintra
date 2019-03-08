21006b2a68ed30695e71b233a554e469f55dd8f8
Andrey Datsenko <and@compent.net>
2019-03-08 15:17:25 +0200

    UC-2640  Groups. Documents. Ddelete button was updated

---
+++ src/Compent.uIntra/Core/Extensions/AjaxHelperExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace Compent.Uintra.Core.Extensions
+{
+    public static class AjaxHelperExtensions
+    {
+        public static MvcHtmlString RawActionLink(this AjaxHelper ajaxHelper, string linkText, string actionName, string controllerName, object routeValues, AjaxOptions ajaxOptions, object htmlAttributes)
+        {
+            var repID = Guid.NewGuid().ToString();
+            var lnk = ajaxHelper.ActionLink(repID, actionName, controllerName, routeValues, ajaxOptions, htmlAttributes);
+            return MvcHtmlString.Create(lnk.ToString().Replace(repID, linkText));
+        }
+    }
+}
