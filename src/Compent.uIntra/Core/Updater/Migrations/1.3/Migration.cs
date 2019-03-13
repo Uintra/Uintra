acbba1a7a6931ca09ffa5dfbad94436e454519c0
aleksandr.chekanyn <alc@compent.net>
2019-03-13 16:18:15 +0200

    UC-2708 Add localization keys migration Creator should be replaced on Owner

--- src/Compent.uIntra/Core/Updater/Migrations/1.3/Migration.cs
+++ src/Compent.uIntra/Core/Updater/Migrations/1.3/Migration.cs
@@ -13,14 +13,39 @@ namespace Compent.Uintra.Core.Updater.Migrations._1._3
         public Migration(IMigrationStepsResolver migrationStepsResolver)
         {
             _migrationStepsResolver = migrationStepsResolver;
-        }        
+        }
+
+        private TranslationUpdateData TranslationUpdateData { get; } = new TranslationUpdateData
+        {
+            Add = new Dictionary<string, string>
+            {
+                { "EventsCreate.Owner.lbl","Owner"},
+                { "EventsEdit.Owner.lbl","Owner"},
+                { "GroupInfo.OwnerName.lbl","Owner"},
+                { "NewsEdit.Owner.lbl","Owner"},
+                { "NewsCreate.Owner.lbl","owner"}
+            },
+            Update = new Dictionary<string, (string old, string update)>
+            {
+
+            },
+            Remove = new List<string>
+            {
+                 "EventsCreate.Creator.lbl",
+                 "EventsEdit.Creator.lbl",
+                 "GroupInfo.CreatorName",
+                 "NewsEdit.Creator.lbl",
+                 "NewsCreate.Creator.lbl"
+            }
+        };
 
         private T Resolve<T>() where T : class => _migrationStepsResolver.Resolve<T>();
 
         public IEnumerable<IMigrationStep> Steps
         {
             get
-            {                 
+            {
+                yield return new TranslationsUpdateStep(TranslationUpdateData);
                 yield return Resolve<CreateForbiddenErrorPageStep>();
                 yield return Resolve<SetupDefaultMemberGroupsPermissionsStep>();
             }
