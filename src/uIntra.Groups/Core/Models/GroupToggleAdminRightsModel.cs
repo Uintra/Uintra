6b680ac44417d4957da83f4b79a83160d8096aa5
Maxim Gridin <mgr@compent.net>
2019-09-25 13:43:02 +0300

    Removed redundant attributes

--- src/uIntra.Groups/Core/Models/GroupToggleAdminRightsModel.cs
+++ src/uIntra.Groups/Core/Models/GroupToggleAdminRightsModel.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
 
 namespace Uintra.Groups
 {
     public class GroupToggleAdminRightsModel
     {
-        [Required(AllowEmptyStrings = false)]
         public Guid MemberId { get; set; }
-        [Required(AllowEmptyStrings = false)]
         public Guid GroupId { get; set; }
     }
 }
