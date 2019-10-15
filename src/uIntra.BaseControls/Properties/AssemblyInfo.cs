3b129b53a9d90853c8e29581d0671688af9c0d0f
Maxim Zhigulsky <maz@compent.net>
2019-10-15 14:30:43 +0300

    Increment packages versions

--- src/uIntra.BaseControls/Properties/AssemblyInfo.cs
+++ src/uIntra.BaseControls/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Uintra.BaseControls")]
 [assembly: AssemblyDescription("Base activity package.")]
 
-[assembly: AssemblyVersion("3.3.0.0")]
+[assembly: AssemblyVersion("3.4.0.0")]
 [assembly: ComVisible(false)]
 [assembly: Guid("2e4a8b55-37f6-46a8-b108-30e346870720")]
