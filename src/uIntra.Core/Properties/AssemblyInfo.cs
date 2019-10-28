c774111680eb1eee377ac917bc93450473fcd43e
Maxim Zhigulsky <maz@compent.net>
2019-10-28 15:28:12 +0200

    Increment packages versions

--- src/uIntra.Core/Properties/AssemblyInfo.cs
+++ src/uIntra.Core/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Uintra.Core")]
 [assembly: AssemblyDescription("Core activity package.")]
 
-[assembly: AssemblyVersion("2.4.0.3")]
+[assembly: AssemblyVersion("2.4.0.4")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3f8c785a-48d0-41f5-9257-6f73afca41eb")]
