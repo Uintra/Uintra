58aba1d355acd584c7e5fb1969ec581f3215bee7
Maxim Zhigulsky <maz@compent.net>
2019-11-12 12:52:41 +0200

    Increment package versions

--- src/uIntra.Users/Properties/AssemblyInfo.cs
+++ src/uIntra.Users/Properties/AssemblyInfo.cs
@@ -7,6 +7,6 @@ using System.Runtime.InteropServices;
                                " to all functionality including Umbraco.")]
 [assembly: AssemblyConfiguration("")]
 
-[assembly: AssemblyVersion("2.4.0.5")]
+[assembly: AssemblyVersion("2.4.0.6")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3e868f15-ac47-4962-a6a5-c9084752dde9")]
