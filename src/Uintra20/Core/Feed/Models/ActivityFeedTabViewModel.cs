afc24e662dc58d64da813656cb3ad0e88a8a6864
Sergey Vinnichenko <svi@compent.net>
2019-12-11 11:43:31 +0200

    Change tab filter model

--- src/Uintra20/Core/Feed/Models/ActivityFeedTabViewModel.cs
+++ src/Uintra20/Core/Feed/Models/ActivityFeedTabViewModel.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Uintra20.Features.Links.Models;
 
 namespace Uintra20.Core.Feed.Models
 {
     public class ActivityFeedTabViewModel
     {
-        public Enum Type { get; set; }       
+        public Enum Type { get; set; }
         public bool IsActive { get; set; }
         public IActivityCreateLinks Links { get; set; }
         public string Title { get; set; }
-        public Dictionary<string,string> Filters { get; set; } = new Dictionary<string, string>();
+        public ActivityFeedTabFiltersViewModel[] Filters { get; set; } = new ActivityFeedTabFiltersViewModel[]{} ;
     }
 }
