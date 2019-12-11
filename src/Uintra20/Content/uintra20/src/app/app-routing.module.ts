854f5eee26fbaceda26954744e52688c24fb47ca
Taras Gordienko <tgo@compent.net>
2019-12-11 14:40:15 +0200

    fix routing

--- src/Uintra20/Content/uintra20/src/app/app-routing.module.ts
+++ src/Uintra20/Content/uintra20/src/app/app-routing.module.ts
@@ -4,24 +4,22 @@ import { RouterModule } from '@angular/router';
 import { ResolveComponent, ResolveService } from '@ubaseline/next';
 import { pages } from './ui/pages/pages';
 import { panels } from './ui/panels/panels';
-import { DeveloperUIKitPage } from './ui/pages/developer-ui-kit/developer-ui-kit.component';
-import { DeveloperUIKitModule } from './ui/pages/developer-ui-kit/developer-ui-ki.module';
 
 const routes = [
   // remove it
   {
-    path: "developer-ui-kit", component: DeveloperUIKitPage
+    path: "developer-ui-kit", loadChildren: './ui/pages/developer-ui-kit/developer-ui-ki.module#DeveloperUIKitModule'
   },
   {
     path: "**", component: ResolveComponent, resolve: { data: ResolveService },
   },
+
   ...pages, ...panels
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes),
-    DeveloperUIKitModule
   ],
   exports: [RouterModule]
 })
