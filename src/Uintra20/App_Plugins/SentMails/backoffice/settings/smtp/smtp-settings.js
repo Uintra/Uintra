13aec01e27d22a3672cf62301ec18b963ddb6243
Maksim.Gridin <mgr@compent.net>
2019-12-13 12:38:56 +0200

    Installed nuget package

---
+++ src/Uintra20/App_Plugins/SentMails/backoffice/settings/smtp/smtp-settings.js
@@ -0,0 +1,35 @@
+﻿angular
+    .module('umbraco')
+    .controller('smtpSettingsController', ['$scope', 'sentMailsService',
+        function ($scope, sentMailsService) {
+
+            $scope.isSaveBtnDisable = false;
+            $scope.smtpSettings = {};
+
+            sentMailsService.getSmtpSettings()
+                .then(
+                    function (resolve) {
+                        $scope.smtpSettings = resolve.data;
+                    },
+                    function (reject) {
+                        alert('Fail. See details in console. ');
+                        console.log(reject);
+                    });
+
+            $scope.save = function () {
+                $scope.isSaveBtnDisable = true;
+
+                sentMailsService.saveSmtpSettings($scope.smtpSettings)
+                    .then(
+                        function (resolve) {
+                            $scope.isSaveBtnDisable = false;
+                            alert(resolve.data);
+                        },
+                        function (reject) {
+                            $scope.isSaveBtnDisable = false;
+                            alert('Fail. See details in console. ');
+                            console.log(reject);
+                        });
+            };
+        }
+    ]);
