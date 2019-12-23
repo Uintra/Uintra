31ca164dbf7dfca34bbc082c5c0e9cb97678d08c
Taras Gordienko <tgo@compent.net>
2019-12-23 14:17:18 +0200

    UINTRA2-98 UINTRA2-106 UINTRA2-105

--- src/Uintra20/Content/uintra20/src/app/ui/panels/activity-create/activity-create-panel.component.ts
+++ src/Uintra20/Content/uintra20/src/app/ui/panels/activity-create/activity-create-panel.component.ts
@@ -1,17 +1,18 @@
-import { Component, ViewEncapsulation, ViewChild } from '@angular/core';
-import { IActivityCreatePanel } from './activity-create-panel.interface';
-import { ITagData } from 'src/app/feature/project/reusable/inputs/tag-multiselect/tag-multiselect.interface';
-import { DropzoneComponent } from 'ngx-dropzone-wrapper';
-import { CreateSocialContentService } from './services/create-social-content.service';
+import { Component, ViewEncapsulation, ViewChild } from "@angular/core";
+import { IActivityCreatePanel } from "./activity-create-panel.interface";
+import { ITagData } from "src/app/feature/project/reusable/inputs/tag-multiselect/tag-multiselect.interface";
+import { DropzoneComponent } from "ngx-dropzone-wrapper";
+import { CreateSocialContentService } from "./services/create-social-content.service";
+import { MAX_LENGTH } from "./_constants.js";
 
 @Component({
-  selector: 'activity-create-panel',
-  templateUrl: './activity-create-panel.html',
-  styleUrls: ['./activity-create-panel.less'],
+  selector: "activity-create-panel",
+  templateUrl: "./activity-create-panel.html",
+  styleUrls: ["./activity-create-panel.less"],
   encapsulation: ViewEncapsulation.None
 })
 export class ActivityCreatePanel {
-  @ViewChild('dropdownRef', { static: false }) dropdownRef: DropzoneComponent;
+  @ViewChild("dropdownRef", { static: false }) dropdownRef: DropzoneComponent;
 
   data: IActivityCreatePanel;
   availableTags: Array<ITagData> = [];
@@ -19,12 +20,21 @@ export class ActivityCreatePanel {
 
   files: Array<any> = [];
   tags: Array<ITagData> = [];
-  description: string;
+  description: string = "";
+
+  get isSubmitDisabled() {
+    if (this.description && this.description.length > MAX_LENGTH) {
+      return true;
+    }
+    return !this.description && this.files.length === 0;
+  }
 
   constructor(private socialContentService: CreateSocialContentService) {}
 
   ngOnInit() {
-    this.availableTags = Object.values(JSON.parse(JSON.stringify(this.data.tags.get().userTagCollection)));
+    this.availableTags = Object.values(
+      JSON.parse(JSON.stringify(this.data.tags.get().userTagCollection))
+    );
   }
 
   onShowPopUp() {
@@ -33,6 +43,7 @@ export class ActivityCreatePanel {
   onHidePopUp() {
     if (this.description || this.tags.length || this.files.length) {
       if (confirm("Are you sure?")) {
+        this.resetForm();
         this.hidePopUp();
       }
     } else {
@@ -63,22 +74,34 @@ export class ActivityCreatePanel {
   }
 
   getMediaIdsForResponse() {
-    return this.files.map(file => file[1]).join(';');
+    return this.files.map(file => file[1]).join(";");
   }
   getTagsForResponse() {
     return this.tags.map(tag => tag.id);
   }
 
+  resetForm(): void {
+    this.files = [];
+    this.tags = [];
+    this.description = '';
+  }
+
   onSubmit() {
-    this.socialContentService.submitSocialContent({
-      description: this.description,
-      OwnerId: 'cb6969e1-ac68-4cae-88a3-8b1cbc453ef7',
-      NewMedia: this.getMediaIdsForResponse(),
-      TagIdsData: this.getTagsForResponse()
-    }).then(response => {
-      this.hidePopUp();
-    }).catch(err => {
-      this.hidePopUp();
-    });
+    this.socialContentService
+      .submitSocialContent({
+        description: this.description,
+        OwnerId: "cb6969e1-ac68-4cae-88a3-8b1cbc453ef7",
+        NewMedia: this.getMediaIdsForResponse(),
+        TagIdsData: this.getTagsForResponse()
+      })
+      .then(response => {
+        this.hidePopUp();
+      })
+      .catch(err => {
+        this.hidePopUp();
+      })
+      .finally(() => {
+        this.resetForm();
+      });
   }
 }
