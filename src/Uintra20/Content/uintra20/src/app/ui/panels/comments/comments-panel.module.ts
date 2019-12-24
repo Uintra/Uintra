5afdb70e3f82790305f4d3932ce3f21abefca117
eugene_sahach <esa@compent.net>
2019-12-24 19:46:29 +0200

    started with comment item

--- src/Uintra20/Content/uintra20/src/app/ui/panels/comments/comments-panel.module.ts
+++ src/Uintra20/Content/uintra20/src/app/ui/panels/comments/comments-panel.module.ts
@@ -4,13 +4,17 @@ import { CommonModule } from '@angular/common';
 import { AS_DYNAMIC_COMPONENT, NotImplementedModule } from '@ubaseline/next';
 import { CommentsPanel } from './comments-panel.component';
 import { RichTextEditorModule } from 'src/app/feature/project/reusable/inputs/rich-text-editor/rich-text-editor.module';
+import { UserAvatarModule } from 'src/app/feature/project/reusable/ui-elements/user-avatar/user-avatar.module';
+import { LikeButtonModule } from 'src/app/feature/project/reusable/ui-elements/like-button/like-button.module';
 
 @NgModule({
   declarations: [CommentsPanel],
   imports: [
     CommonModule,
     NotImplementedModule,
-    RichTextEditorModule
+    RichTextEditorModule,
+    UserAvatarModule,
+    LikeButtonModule
   ],
   providers: [{provide: AS_DYNAMIC_COMPONENT, useValue: CommentsPanel}],
   entryComponents: [CommentsPanel]
