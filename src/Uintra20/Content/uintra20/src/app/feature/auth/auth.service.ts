7e7f67a633ff4da08c23bb289dfa4dfdc5ff8c8f
eugene_sahach <esa@compent.net>
2019-12-24 14:22:17 +0200

    merge

--- src/Uintra20/Content/uintra20/src/app/feature/auth/auth.service.ts
+++ src/Uintra20/Content/uintra20/src/app/feature/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { LoginModel } from '../login.model';
+import { LoginModel } from '../login/models/login.model';
 import { Observable } from 'rxjs';
 
 @Injectable({
--- src/Uintra20/Content/uintra20/src/app/ui/pages/login/login-page.component.ts
+++ src/Uintra20/Content/uintra20/src/app/ui/pages/login/login-page.component.ts
@@ -1,15 +1,15 @@
 import { Component, ViewEncapsulation, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { LoginModel } from 'src/app/ui/pages/login/login.model';
+import { LoginModel } from 'src/app/feature/login/models/login.model';
 import { Subscription } from 'rxjs';
 import { finalize } from 'rxjs/operators';
-import { AuthService } from 'src/app/ui/pages/login/services/auth.service';
+import { AuthService } from 'src/app/feature/auth/auth.service';
 
 @Component({
   selector: 'login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.less'],
+  templateUrl: './login-page.html',
+  styleUrls: ['./login-page.less'],
   encapsulation: ViewEncapsulation.None
 })
 export class LoginPage implements OnDestroy {
