f6e5f9b547267fca98e955c06310af4f8db2ade4
eugene_sahach <esa@compent.net>
2019-12-30 11:45:09 +0200

    minor changes

--- src/Uintra20/Content/uintra20/src/app/ui/panels/comments/components/comment-item/comment-item.component.ts
+++ src/Uintra20/Content/uintra20/src/app/ui/panels/comments/components/comment-item/comment-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { CommentsService } from '../../helpers/comments.service';
 
 @Component({
