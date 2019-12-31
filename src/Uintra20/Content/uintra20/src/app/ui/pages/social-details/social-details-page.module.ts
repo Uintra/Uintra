529f92f30dfc9bf3fae5ca6a9325126e4f8e55e5
eugene_sahach <esa@compent.net>
2019-12-31 16:05:26 +0200

    started with activity details page

--- src/Uintra20/Content/uintra20/src/app/ui/pages/social-details/social-details-page.module.ts
+++ src/Uintra20/Content/uintra20/src/app/ui/pages/social-details/social-details-page.module.ts
@@ -5,16 +5,20 @@ import { RouterModule } from '@angular/router';
 import { UbaselineCoreModule } from '@ubaseline/next';
 import { SocialDetailsPanelComponent } from './social-details-page.component';
 import { LikeButtonModule } from 'src/app/feature/project/reusable/ui-elements/like-button/like-button.module';
+import { UserAvatarModule } from 'src/app/feature/project/reusable/ui-elements/user-avatar/user-avatar.module';
+import { PostHeaderComponent } from './components/post-header/post-header.component';
 
 @NgModule({
   declarations: [
-    SocialDetailsPanelComponent
+    SocialDetailsPanelComponent,
+    PostHeaderComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild([{ path: '', component: SocialDetailsPanelComponent }]),
     UbaselineCoreModule,
     LikeButtonModule,
+    UserAvatarModule,
   ],
   providers: [
     { provide: AS_DYNAMIC_COMPONENT, useValue: SocialDetailsPanelComponent }
