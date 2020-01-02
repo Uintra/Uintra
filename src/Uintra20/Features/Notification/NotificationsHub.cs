fcf528d486d18b4eb631b89dd4488b0189029732
Denis Prodan <dpr@compent.net>
2020-01-02 16:41:30 +0200

    add notifications hub

---
+++ src/Uintra20/Features/Notification/NotificationsHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Uintra20.Features.Notification.Services;
+
+namespace Uintra20.Features.Notification
+{
+	public class NotificationsHub:Hub
+	{
+		private readonly IUiNotificationService _uiNotificationService;
+
+		public NotificationsHub(IUiNotificationService uiNotificationService)
+		{
+			_uiNotificationService = uiNotificationService;
+		}
+		public async Task<int> GetNotNotifiedCount(Guid memberId)
+		{
+			return await _uiNotificationService.GetNotNotifiedCountAsync(memberId);
+		}
+	}
+}
