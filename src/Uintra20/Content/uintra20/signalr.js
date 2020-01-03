746e07a1a5382ae4f0be0d1a99ed612a4cb59328
Taras Gordienko <tgo@compent.net>
2020-01-03 11:24:10 +0200

    add hub connection

---
+++ src/Uintra20/Content/uintra20/signalr.js
@@ -0,0 +1,97 @@
+(function ($, window, undefined) {
+  /// <param name="$" type="jQuery" />
+  "use strict";
+
+  if (typeof ($.signalR) !== "function") {
+      throw new Error("SignalR: SignalR is not loaded. Please ensure jquery.signalR-x.js is referenced before ~/signalr/js.");
+  }
+
+  var signalR = $.signalR;
+
+  function makeProxyCallback(hub, callback) {
+      return function () {
+          // Call the client hub method
+          callback.apply(hub, $.makeArray(arguments));
+      };
+  }
+
+  function registerHubProxies(instance, shouldSubscribe) {
+      var key, hub, memberKey, memberValue, subscriptionMethod;
+
+      for (key in instance) {
+          if (instance.hasOwnProperty(key)) {
+              hub = instance[key];
+
+              if (!(hub.hubName)) {
+                  // Not a client hub
+                  continue;
+              }
+
+              if (shouldSubscribe) {
+                  // We want to subscribe to the hub events
+                  subscriptionMethod = hub.on;
+              } else {
+                  // We want to unsubscribe from the hub events
+                  subscriptionMethod = hub.off;
+              }
+
+              // Loop through all members on the hub and find client hub functions to subscribe/unsubscribe
+              for (memberKey in hub.client) {
+                  if (hub.client.hasOwnProperty(memberKey)) {
+                      memberValue = hub.client[memberKey];
+
+                      if (!$.isFunction(memberValue)) {
+                          // Not a client hub function
+                          continue;
+                      }
+
+                      // Use the actual user-provided callback as the "identity" value for the registration.
+                      subscriptionMethod.call(hub, memberKey, makeProxyCallback(hub, memberValue), memberValue);
+                  }
+              }
+          }
+      }
+  }
+
+  $.hubConnection.prototype.createHubProxies = function () {
+      var proxies = {};
+      this.starting(function () {
+          // Register the hub proxies as subscribed
+          // (instance, shouldSubscribe)
+          registerHubProxies(proxies, true);
+
+          this._registerSubscribedHubs();
+      }).disconnected(function () {
+          // Unsubscribe all hub proxies when we "disconnect".  This is to ensure that we do not re-add functional call backs.
+          // (instance, shouldSubscribe)
+          registerHubProxies(proxies, false);
+      });
+
+      proxies['notificationsHub'] = this.createHubProxy('notificationsHub');
+      proxies['notificationsHub'].client = { };
+      proxies['notificationsHub'].server = {
+          getNotNotifiedCount: function (memberId) {
+              return proxies['notificationsHub'].invoke.apply(proxies['notificationsHub'], $.merge(["GetNotNotifiedCount"], $.makeArray(arguments)));
+           }
+      };
+
+      proxies['previewHub'] = this.createHubProxy('previewHub');
+      proxies['previewHub'].client = { };
+      proxies['previewHub'].server = {
+      };
+
+      proxies['uSyncHub'] = this.createHubProxy('uSyncHub');
+      proxies['uSyncHub'].client = { };
+      proxies['uSyncHub'].server = {
+          getTime: function () {
+              return proxies['uSyncHub'].invoke.apply(proxies['uSyncHub'], $.merge(["GetTime"], $.makeArray(arguments)));
+           }
+      };
+
+      return proxies;
+  };
+
+  signalR.hub = $.hubConnection("/umbraco/BackOffice/signalr", { useDefaultPath: false });
+  $.extend(signalR, signalR.hub.createHubProxies());
+
+}(window.jQuery, window));
--- src/Uintra20/Content/uintra20/src/app/feature/project/specific/nav-notifications/nav-notifications.component.ts
+++ src/Uintra20/Content/uintra20/src/app/feature/project/specific/nav-notifications/nav-notifications.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { NavNotificationsService, INotificationsData } from './nav-notifications.service';
 
+declare var $: any;
+
 @Component({
   selector: 'app-nav-notifications',
   templateUrl: './nav-notifications.component.html',
   styleUrls: ['./nav-notifications.component.less']
 })
 export class NavNotificationsComponent implements OnInit {
+
+  private connection: any;
+  private proxy: any;
+  private ulr: any;
+
+
   notifications: INotificationsData[];
   notificationCount: number;
   isShow: boolean = false;
@@ -17,7 +25,34 @@ export class NavNotificationsComponent implements OnInit {
   ngOnInit() {
     this.navNotificationsService.getNotifiedCount().subscribe(count => {
       this.notificationCount = count;
-    })
+    });
+    // this.connection = $.hubConnection('/umbraco/backoffice/signalr/hubs');
+    // this.proxy = this.connection.createHubProxy('ProcessingHub');
+
+    const notificationsHub = $.connection.notificationsHub;
+    this.proxy = $.connection.notificationsHub;
+
+    $.connection.hub.start().done(r => {
+      debugger;
+      var count=$.connection.notificationsHub.server.getNotNotifiedCount('28241e2b-e993-4f60-9357-577b5d42eb63');
+      }).catch(r => {
+        debugger;
+
+      });
+    // .done(r => {
+    //   debugger;
+    // }).catch(r => {
+    //   debugger;
+
+    // })
+
+
+
+    //   .start().done((data: any) => {
+    //     console.log('Connected to Processing Hub');
+    // }).catch((error: any) => {
+    //     console.log('Hub error -> ' + error);
+    // });
   }
 
   onShow() {
