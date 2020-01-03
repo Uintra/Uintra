b645eda9fbf19b5e3206daa173dc7381cf0d7432
Maksim.Gridin <mgr@compent.net>
2020-01-03 10:55:55 +0200

    Reset

--- src/Uintra20/Content/uintra20/src/app/ui/panels/likes/likes-panel.module.ts
+++ src/Uintra20/Content/uintra20/src/app/ui/panels/likes/likes-panel.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AS_DYNAMIC_COMPONENT } from '@ubaseline/next';
+
+import { AS_DYNAMIC_COMPONENT, NotImplementedModule } from '@ubaseline/next';
 import { LikesPanel } from './likes-panel.component';
-import { LikeButtonModule } from 'src/app/feature/project/reusable/ui-elements/like-button/like-button.module';
 
 @NgModule({
   declarations: [LikesPanel],
   imports: [
     CommonModule,
-    LikeButtonModule
+    NotImplementedModule
   ],
   providers: [{provide: AS_DYNAMIC_COMPONENT, useValue: LikesPanel}],
   entryComponents: [LikesPanel]
