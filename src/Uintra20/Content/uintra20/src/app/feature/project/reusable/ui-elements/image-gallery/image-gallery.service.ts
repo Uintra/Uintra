7ac3963c6653ebbd44724404b2b0d9226b599384
eugene_sahach <esa@compent.net>
2020-01-03 16:59:03 +0200

    RTE styling fix and image gallery

--- src/Uintra20/Content/uintra20/src/app/feature/project/reusable/ui-elements/image-gallery/image-gallery.service.ts
+++ src/Uintra20/Content/uintra20/src/app/feature/project/reusable/ui-elements/image-gallery/image-gallery.service.ts
@@ -9,28 +9,28 @@ export class ImageGalleryService {
 
   constructor() { }
 
-  open() {
+  open(items, index) {
     let pswpElement = document.querySelectorAll('.pswp')[0];
 
     // build items array
-    let items = [
-        {
-            src: 'https://placekitten.com/600/400',
-            w: 600,
-            h: 400
-        },
-        {
-            src: 'https://placekitten.com/1200/900',
-            w: 1200,
-            h: 900
-        }
-    ];
+    // let items = [
+    //     {
+    //         src: 'https://placekitten.com/600/400',
+    //         w: 600,
+    //         h: 400
+    //     },
+    //     {
+    //         src: 'https://placekitten.com/1200/900',
+    //         w: 1200,
+    //         h: 900
+    //     }
+    // ];
 
     // define options (if needed)
     let options = {
         // optionName: 'option value'
         // for example:
-        index: 0 // start at first slide
+        index: index // start at first slide
     };
 
     // Initializes and opens PhotoSwipe
