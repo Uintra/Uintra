a315b4b5384f10ca4da543b7b1cbe40cd07dc4e8
Maksim.Gridin <mgr@compent.net>
2020-01-13 13:12:33 +0200

    Fixes: - duplication in pages.ts - markup in social details component - navigation to social-edit page
    (cherry picked from commit 5d7e5b03862cb7de95ed3694051f0207570dd178)

--- src/Uintra20/Content/uintra20/src/app/ui/pages/pages.ts
+++ src/Uintra20/Content/uintra20/src/app/ui/pages/pages.ts
@@ -1,40 +1,23 @@
 export const pages = [
   {
-    id: "homePage",
-    path: "__dynamic__",
-    loadChildren: "./ui/pages/home/home-page.module#HomePageModule"
+    id: 'homePage',
+    path: '__dynamic__',
+    loadChildren: './ui/pages/home/home-page.module#HomePageModule'
   },
   {
-    id: "socialDetailsPage",
-    path: "__dynamic__",
-    loadChildren:
-      "./ui/pages/social-details/social-details-page.module#SocialDetailsPageModule"
-  },
-  {
-    id: "articlePage",
-    path: "__dynamic__",
-    loadChildren: "./ui/pages/article/article-page.module#ArticlePageModule"
+    id: 'articlePage',
+    path: '__dynamic__',
+    loadChildren: './ui/pages/article/article-page.module#ArticlePageModule'
   },
   {
     id: 'notificationsPage',
     path: '__dynamic__',
     loadChildren: './ui/pages/notifications/notifications-page.module#NotificationsPageModule'
   },
-    {
-        id: 'homePage',
-        path: '__dynamic__',
-        loadChildren: './ui/pages/home/home-page.module#HomePageModule'
-    },
-    {
-        id: 'socialDetailsPage',
-        path: '__dynamic__',
-        loadChildren: './ui/pages/social-details/social-details-page.module#SocialDetailsPageModule',
-        cache: false
-    },
-    {
-        id: 'articlePage',
-        path: '__dynamic__',
-        loadChildren: './ui/pages/article/article-page.module#ArticlePageModule'
-    }
+  {
+    id: 'socialDetailsPage',
+    path: '__dynamic__',
+    loadChildren: './ui/pages/social-details/social-details-page.module#SocialDetailsPageModule',
+    cache: false
+  }
 ];
-
