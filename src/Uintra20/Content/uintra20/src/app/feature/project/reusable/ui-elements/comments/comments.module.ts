f56e7fd192e2d16ea9894feb30f2fac7d34bef0a
eugene.sahach <esa@gmail.com>
2020-01-16 11:37:52 +0200

    moved comments to shared

--- src/Uintra20/Content/uintra20/src/app/feature/project/reusable/ui-elements/comments/comments.module.ts
+++ src/Uintra20/Content/uintra20/src/app/feature/project/reusable/ui-elements/comments/comments.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CommentsComponent } from './comments.component';
 import { RichTextEditorModule } from '../../inputs/rich-text-editor/rich-text-editor.module';
-import { CommentItemComponent } from 'src/app/ui/panels/comments/components/comment-item/comment-item.component';
 import { UserAvatarModule } from '../user-avatar/user-avatar.module';
 import { LikeButtonModule } from '../like-button/like-button.module';
 import { FormsModule } from '@angular/forms';
-import { SubcommentItemComponent } from 'src/app/ui/panels/comments/components/subcomment-item/subcomment-item.component';
-import { CommentHeaderComponent } from 'src/app/ui/panels/comments/components/comment-header/comment-header.component';
+import { CommentItemComponent } from './components/comment-item/comment-item.component';
+import { SubcommentItemComponent } from './components/subcomment-item/subcomment-item.component';
+import { CommentHeaderComponent } from './components/comment-header/comment-header.component';
 
 @NgModule({
   declarations: [
