65fd9581bc9b99a060b49f34da5ae5debf684ec3
eugene.sahach <esa@gmail.com>
2020-01-16 17:53:38 +0200

    started with emoji

---
+++ src/Uintra20/Content/uintra20/src/app/feature/project/reusable/inputs/rich-text-editor/rich-text-editor-emoji/rich-text-editor-emoji.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rich-text-editor-emoji',
+  templateUrl: './rich-text-editor-emoji.component.html',
+  styleUrls: ['./rich-text-editor-emoji.component.css']
+})
+export class RichTextEditorEmojiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
