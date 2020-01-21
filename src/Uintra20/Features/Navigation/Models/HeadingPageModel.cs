2524612fde493c63eb3224b7fbfa61aeb9cd744f
Denis Prodan <dpr@compent.net>
2020-01-21 16:56:23 +0200

    UINTRA2-218 Create Umbraco Navigation Composition UINTRA2-219 Create Umbraco Heading Document Type

---
+++ src/Uintra20/Features/Navigation/Models/HeadingPageModel.cs
@@ -0,0 +1,13 @@
+﻿using UBaseline.Shared.Navigation;
+using UBaseline.Shared.Node;
+using UBaseline.Shared.Property;
+using Uintra20.Features.Navigation.Models;
+
+namespace Uintra20.Core.UbaselineModels
+{
+    public class HeadingPageModel: NodeModel, IUintraNavigationComposition
+    {
+        public NavigationCompositionModel Navigation { get; set; }
+        public PropertyModel<bool> ShowInSubMenu { get; set; }
+    }
+}
