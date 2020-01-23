86a761f87a628fc5c1585f978d2f650890f66dc7
Taras Gordienko <tgo@compent.net>
2020-01-23 15:17:23 +0200

    minor refactor

--- src/Uintra20/Content/uintra20/src/app/ui/pages/social/services/social.service.ts
+++ src/Uintra20/Content/uintra20/src/app/ui/pages/social/services/social.service.ts
@@ -1,29 +1,26 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class SocialService {
-  private prefix = 'api/Social/';
-  private routeTree = {
-    update: `${this.prefix}Update`,
-    delete: `${this.prefix}Delete`
+  private readonly apiPrefix = "api/Social";
+  private readonly apiRouteTree = {
+    update: `${this.apiPrefix}/Update`,
+    delete: `${this.apiPrefix}/Delete`
   };
 
-  constructor(
-    private httpClient: HttpClient
-  ) { }
+  constructor(private httpClient: HttpClient) {}
 
   public update(model: any): Observable<any> {
-    return this.httpClient.put(this.routeTree.update, model);
+    return this.httpClient.put(this.apiRouteTree.update, model);
   }
 
   public delete(id: string): Observable<any> {
-    return this.httpClient.delete(
-      this.routeTree.delete,
-      { params: new HttpParams().set('id', id), }
-    );
+    return this.httpClient.delete(this.apiRouteTree.delete, {
+      params: new HttpParams().set("id", id)
+    });
   }
 }
