852d1094c09985975b25411a04e388698f584aca
Taras Gordienko <tgo@compent.net>
2020-01-24 17:30:55 +0200

    add simple datapicker

--- src/Uintra20/Content/uintra20/src/app/ui/pages/developer-ui-kit/developer-ui-kit.component.ts
+++ src/Uintra20/Content/uintra20/src/app/ui/pages/developer-ui-kit/developer-ui-kit.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'developer-ui-kit',
@@ -6,9 +7,48 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./developer-ui-kit.less'],
   encapsulation: ViewEncapsulation.None
 })
-export class DeveloperUIKitPage {
+export class DeveloperUIKitPage implements OnInit{
   testValue: string = 'test value';
   testBoolean: boolean = false;
 
-  constructor() {}
+  reactiveForm = null;
+  field1 = null;
+  field2 = null;
+
+  options1:any = {
+    // defaultDate: new Date(),
+    // minDate: new Date(),
+    // maxDate: null
+
+  };
+
+  options2: any = {
+
+  };
+
+  constructor() {
+
+  }
+      ngOnInit(): void {
+        this.field1 = new Date();
+      }
+
+  onSubmit() {
+    this.field1.format();
+    this.field2.format();
+  }
+
+  dp1Change() {
+    this.options2 = {
+      ...this.options2,
+      minDate: this.field1,
+    }
+  }
+
+  dp2Change() {
+    this.options1 = {
+      ...this.options1,
+      maxDate: this.field2,
+    }
+  }
 }
