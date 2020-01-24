299c41926c71862608d73943e5ac8cbb2c1ec719
Denis Prodan <dpr@compent.net>
2020-01-24 18:44:00 +0200

    fix top menu automapper profile

--- src/Uintra20/Features/Navigation/Models/TopNavigationItemViewModel.cs
+++ src/Uintra20/Features/Navigation/Models/TopNavigationItemViewModel.cs
@@ -1,14 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using Uintra20.Features.Links.Models;
 
 namespace Uintra20.Features.Navigation.Models
 {
     public class TopNavigationItemViewModel
     {
         public string Name { get; set; }
-        public string Url { get; set; }
+        public UintraLinkModel Url { get; set; }
         public Enum Type { get; set; }
     }
 }
