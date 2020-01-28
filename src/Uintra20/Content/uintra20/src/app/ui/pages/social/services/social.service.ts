65d5bd9877be7b65b234babb6a013ef74676384a
Maksim.Gridin <mgr@compent.net>
2020-01-28 12:11:27 +0200

    UINTRA2-61 Changes: - Replaced any type in social service

--- src/Uintra20/Content/uintra20/src/app/ui/pages/social/services/social.service.ts
+++ src/Uintra20/Content/uintra20/src/app/ui/pages/social/services/social.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpParams } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ISocialEdit } from '../edit/social-edit-page.interface';
 
 @Injectable({
   providedIn: "root"
@@ -12,14 +13,14 @@ export class SocialService {
     delete: `${this.apiPrefix}/Delete`
   };
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) { }
 
-  public update(model: any): Observable<any> {
-    return this.httpClient.put(this.apiRouteTree.update, model);
+  public update(model: ISocialEdit): Observable<Response> {
+    return this.httpClient.put<Response>(this.apiRouteTree.update, model);
   }
 
-  public delete(id: string): Observable<any> {
-    return this.httpClient.delete(this.apiRouteTree.delete, {
+  public delete(id: string): Observable<Response> {
+    return this.httpClient.delete<Response>(this.apiRouteTree.delete, {
       params: new HttpParams().set("id", id)
     });
   }
