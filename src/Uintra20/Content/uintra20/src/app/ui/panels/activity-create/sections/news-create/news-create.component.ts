b7c6bb6c11cdd6fe6c83bfcd3486b3f38d24ae05
Sergiy Bojdenko <sbo@compent.net>
2020-01-29 11:56:43 +0200

    Fix owners

--- src/Uintra20/Content/uintra20/src/app/ui/panels/activity-create/sections/news-create/news-create.component.ts
+++ src/Uintra20/Content/uintra20/src/app/ui/panels/activity-create/sections/news-create/news-create.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IActivityCreatePanel } from '../../activity-create-panel.interface';
 import ParseHelper from 'src/app/feature/shared/helpers/parse.helper';
+import { ISelectItem } from 'src/app/feature/project/reusable/inputs/select/select.component';
+
+export interface INewsOwner{
+  id: string;
+  displayedName: string;
+}
 
 @Component({
   selector: 'app-news-create',
@@ -13,11 +19,17 @@ export class NewsCreateComponent implements OnInit {
   files: Array<any> = [];
   isPinCheked: boolean;
   tags: any[];
+  owners: ISelectItem[];
+  defaultOwner: ISelectItem;
+
   constructor() { }
 
   ngOnInit() {
     this.panelData = ParseHelper.parseUbaselineData(this.data);
     this.tags = Object.values(this.panelData.tags.userTagCollection);
+    this.owners = this.getOwners();
+    this.defaultOwner = this.owners.find(x => x.id == this.panelData.creator.id);
+    debugger
   }
 
   onUploadSuccess(fileArray: Array<any> = []): void {
@@ -37,5 +49,18 @@ export class NewsCreateComponent implements OnInit {
 
   onSubmit() {
     // debugger;
+  }
+
+  private getOwners(): ISelectItem[] {
+    const result = new Array<ISelectItem>();
+    if (this.panelData.members) {
+      const members = Object.values(this.panelData.members) as Array<any>;
+      members.forEach(element => {
+        result.push({id: element.id, text: element.displayedName});
+      });
+      return result;
+    }
+    result.push({id: this.panelData.creator.id, text: this.panelData.creator.displayedName});
+    return result;
   }
 }
