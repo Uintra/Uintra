0c4299365f8613900dd61b65e6adaefb3f3b7d74
Maksim.Gridin <mgr@compent.net>
2020-01-30 17:02:05 +0200

    UINTRA-2-225 - Added tags to profile page

--- src/Uintra20/Core/Member/Profile/Converters/ProfilePageViewModelConverter.cs
+++ src/Uintra20/Core/Member/Profile/Converters/ProfilePageViewModelConverter.cs
@@ -4,6 +4,7 @@ using UBaseline.Core.Node;
 using Uintra20.Core.Member.Entities;
 using Uintra20.Core.Member.Profile.Models;
 using Uintra20.Core.Member.Services;
+using Uintra20.Features.Tagging.UserTags.Services;
 using Uintra20.Infrastructure.Extensions;
 
 namespace Uintra20.Core.Member.Profile.Converters
@@ -12,10 +13,17 @@ namespace Uintra20.Core.Member.Profile.Converters
         INodeViewModelConverter<ProfilePageModel, ProfilePageViewModel>
     {
         private readonly IIntranetMemberService<IntranetMember> _memberService;
+        private readonly IUserTagService _userTagService;
+        private readonly IUserTagProvider _userTagProvider;
 
-        public ProfilePageViewModelConverter(IIntranetMemberService<IntranetMember> memberService)
+        public ProfilePageViewModelConverter(
+            IIntranetMemberService<IntranetMember> memberService, 
+            IUserTagService userTagService, 
+            IUserTagProvider userTagProvider)
         {
             _memberService = memberService;
+            _userTagService = userTagService;
+            _userTagProvider = userTagProvider;
         }
 
         public void Map(
@@ -29,6 +37,7 @@ namespace Uintra20.Core.Member.Profile.Converters
             var member = _memberService.Get(parseId);
 
             viewModel.Profile = member.Map<ProfileViewModel>();
+            viewModel.Tags = _userTagService.Get(member.Id);
         }
     }
 }
