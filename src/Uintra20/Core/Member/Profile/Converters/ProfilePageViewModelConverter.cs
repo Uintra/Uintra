78e01accda0c04d4c4d2988c6c83db14cdc08e71
Sergey Vinnichenko <svi@compent.net>
2020-02-04 16:25:57 +0200

    Add edit profile link to profile model

--- src/Uintra20/Core/Member/Profile/Converters/ProfilePageViewModelConverter.cs
+++ src/Uintra20/Core/Member/Profile/Converters/ProfilePageViewModelConverter.cs
@@ -4,6 +4,7 @@ using UBaseline.Core.Node;
 using Uintra20.Core.Member.Entities;
 using Uintra20.Core.Member.Profile.Models;
 using Uintra20.Core.Member.Services;
+using Uintra20.Core.User;
 using Uintra20.Features.Tagging.UserTags.Services;
 using Uintra20.Infrastructure.Extensions;
 
@@ -15,15 +16,18 @@ namespace Uintra20.Core.Member.Profile.Converters
         private readonly IIntranetMemberService<IntranetMember> _memberService;
         private readonly IUserTagService _userTagService;
         private readonly IUserTagProvider _userTagProvider;
+        private readonly IIntranetUserContentProvider _intranetUserContentProvider;
 
         public ProfilePageViewModelConverter(
             IIntranetMemberService<IntranetMember> memberService, 
             IUserTagService userTagService, 
-            IUserTagProvider userTagProvider)
+            IUserTagProvider userTagProvider,
+            IIntranetUserContentProvider intranetUserContentProvider)
         {
             _memberService = memberService;
             _userTagService = userTagService;
             _userTagProvider = userTagProvider;
+            _intranetUserContentProvider = intranetUserContentProvider;
         }
 
         public void Map(
@@ -35,6 +39,13 @@ namespace Uintra20.Core.Member.Profile.Converters
             if (!Guid.TryParse(id, out var parseId))  return;
 
             var member = _memberService.Get(parseId);
+
+            var currentMemberId = _memberService.GetCurrentMemberId();
+
+            if (member.Id == currentMemberId)
+            {
+                viewModel.EditProfileLink = _intranetUserContentProvider.GetEditPage().Url.ToLinkModel();
+            }
 
             viewModel.Profile = member.Map<ProfileViewModel>();
             viewModel.Tags = _userTagService.Get(member.Id);
--- src/Uintra20/Core/Member/Profile/Models/ProfilePageViewModel.cs
+++ src/Uintra20/Core/Member/Profile/Models/ProfilePageViewModel.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using UBaseline.Shared.Node;
 using UBaseline.Shared.PageSettings;
 using UBaseline.Shared.Property;
+using Uintra20.Features.Links.Models;
 using Uintra20.Features.Tagging.UserTags.Models;
 
 namespace Uintra20.Core.Member.Profile.Models
@@ -13,5 +14,6 @@ namespace Uintra20.Core.Member.Profile.Models
         public PageSettingsCompositionViewModel PageSettings { get; set; }
         public ProfileViewModel Profile { get; set; }
         public IEnumerable<UserTag> Tags { get; set; } = Enumerable.Empty<UserTag>();
+        public UintraLinkModel EditProfileLink { get; set; }
     }
 }
