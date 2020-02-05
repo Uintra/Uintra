b47a5709db5c2d1963170b5714fe1a6eee756e3b
eugene_sahach <esa@compent.net>
2020-02-05 17:26:11 +0200

    no closing left navigation yet

--- src/Uintra20/Content/uintra20/src/app/feature/project/reusable/ui-elements/comments/components/comment-header/comment-header.component.ts
+++ src/Uintra20/Content/uintra20/src/app/feature/project/reusable/ui-elements/comments/components/comment-header/comment-header.component.ts
@@ -15,7 +15,6 @@ export class CommentHeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    console.log(this.data);
   }
 
   onToggleReply() {
--- src/Uintra20/Content/uintra20/src/app/ui/main-layout/left-navigation/components/user-nav-mobile/user-nav-mobile.component.ts
+++ src/Uintra20/Content/uintra20/src/app/ui/main-layout/left-navigation/components/user-nav-mobile/user-nav-mobile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IMobileUserNavigation } from '../../left-navigation.interface';
 import { Router } from '@angular/router';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user-nav-mobile',
@@ -10,21 +11,42 @@ import { Router } from '@angular/router';
 })
 export class UserNavMobileComponent implements OnInit {
   data: IMobileUserNavigation;
+  inProgress: boolean;
 
   constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
-    this.http.get('ubaseline/api/intranetNavigation/mobileNavigation').subscribe(
-      (res: any) => {
-        this.data = res;
-        console.log(this.data)
-      } 
-    );
+    this.http.get('/ubaseline/api/IntranetNavigation/TopNavigation')
+    .subscribe((res: any) => {
+      this.data = res;
+    });
   }
 
-  handleRequest(type, url) {
+  redirect(url, type) {
+    this.inProgress = true;
+  
+    if (type == 1) {
+      this.http.post(url.originalUrl, null).pipe(
+        finalize(() => this.inProgress = false)
+      ).subscribe(
+        (next) => {
+          window.open(window.location.origin + "/umbraco", "_blank");
+        },
+        (error) => {
+          if (error.status === 403) {
+            console.error(error.message);
+          }
+        },
+      );
+    }
+  
     if (type == 4) {
-      this.http.post(url, null).subscribe(res => this.router.navigate(['/login']));
+      this.http.post(url.originalUrl, null).pipe(
+        finalize(() => this.inProgress = false)
+      ).subscribe(
+        (next) => { this.router.navigate(['/login']); }
+      )
     }
   }
 }
+
