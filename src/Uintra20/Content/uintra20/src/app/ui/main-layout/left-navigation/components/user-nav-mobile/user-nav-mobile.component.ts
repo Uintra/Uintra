9b115d110253c332e2d9e74bfc380fa7cfcf8929
eugene_sahach <esa@compent.net>
2020-02-05 13:45:13 +0200

    still needs link to profile page

--- src/Uintra20/Content/uintra20/src/app/ui/main-layout/left-navigation/components/user-nav-mobile/user-nav-mobile.component.ts
+++ src/Uintra20/Content/uintra20/src/app/ui/main-layout/left-navigation/components/user-nav-mobile/user-nav-mobile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IMobileUserNavigation } from '../../left-navigation.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-nav-mobile',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-nav-mobile.component.less']
 })
 export class UserNavMobileComponent implements OnInit {
+  data: IMobileUserNavigation;
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
+    this.http.get('ubaseline/api/intranetNavigation/mobileNavigation').subscribe(
+      (res: any) => {
+        this.data = res;
+        console.log(this.data)
+      } 
+    );
   }
 
+  handleRequest(type, url) {
+    if (type == 4) {
+      this.http.post(url, null).subscribe(res => this.router.navigate(['/login']));
+    }
+  }
 }
--- src/Uintra20/Content/uintra20/src/app/ui/main-layout/left-navigation/left-navigation.interface.ts
+++ src/Uintra20/Content/uintra20/src/app/ui/main-layout/left-navigation/left-navigation.interface.ts
@@ -1,3 +1,7 @@
+import { IOwner } from 'src/app/feature/shared/interfaces/Owner';
+import { IMyLink } from './components/my-links/my-links.service';
+import { ISharedLink } from './components/shared-links/shared-links.service';
+
 export interface INavigationData {
   menuItems: INavigationItem[];
 }
@@ -12,4 +16,17 @@ export interface INavigationItem {
   children: INavigationItem[];
   level: number;
   isSelected: boolean;
+}
+export interface IMobileUserNavigation {
+  currentMember: IOwner,
+  items: ITopNavigationLink[]
+}
+export interface ITopNavigationLink {
+  name: string;
+  url: {
+    originalUrl: string;
+    baseUrl: string;
+    params: Array<object>;
+  };
+  type: number;
 }
