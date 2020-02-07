c4b3e499584230d51594edfea5d8b6900d66c650
Maksim.Gridin <mgr@compent.net>
2020-02-07 17:05:53 +0200

    post fix related to media in gitignore

---
+++ src/Uintra20/Features/Media/Strategies/Preset/MemberProfilePresetStrategy.cs
@@ -0,0 +1,10 @@
+﻿namespace Uintra20.Features.Media.Strategies.Preset
+{
+    public class MemberProfilePresetStrategy : IPresetStrategy
+    {
+        public string ThumbnailPreset { get; } = "preset=profile";
+        public string PreviewPreset { get; } = "preset=preview";
+        public string PreviewTwoPreset { get; } = "preset=previewTwo";
+        public int MediaFilesToDisplay { get; } = int.MaxValue; //Means that all files should render
+    }
+}
---
+++ src/Uintra20/Features/Media/Strategies/Preset/PresetStrategies.cs
@@ -0,0 +1,9 @@
+﻿namespace Uintra20.Features.Media.Strategies.Preset
+{
+    public static class PresetStrategies
+    {
+        public static CentralFeedPresetStrategy ForCentralFeed = new CentralFeedPresetStrategy();
+        public static ActivityDetailsPresetStrategy ForActivityDetails = new ActivityDetailsPresetStrategy();
+        public static MemberProfilePresetStrategy ForMemberProfile = new MemberProfilePresetStrategy();
+    }
+}
