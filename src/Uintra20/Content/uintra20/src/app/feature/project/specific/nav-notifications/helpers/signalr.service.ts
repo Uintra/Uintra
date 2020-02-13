e980b1150cbe915daa3c30ff6fb4857e9c189e11
Taras Gordienko <tgo@compent.net>
2020-02-13 17:53:01 +0200

    fix ts error

--- src/Uintra20/Content/uintra20/src/app/feature/project/specific/nav-notifications/helpers/signalr.service.ts
+++ src/Uintra20/Content/uintra20/src/app/feature/project/specific/nav-notifications/helpers/signalr.service.ts
@@ -1,42 +1,47 @@
-import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { Observable, Subject } from "rxjs";
 
 declare var $: any;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class SignalrService {
-  private uintraHub;  
+  private uintraHub;
 
-  constructor() { }
+  constructor() {}
 
-  public startHub() {    
+  public startHub() {
     this.uintraHub = $.connection.uintraHub;
     this.uintraHub.notificationSubject = new Subject<any>();
     this.uintraHub.centralFeedSubject = new Subject<any>();
-    this.uintraHub.client.updateNotifications = this.broadcastUpdateNotifications;    
+    this.uintraHub.client.updateNotifications = this.broadcastUpdateNotifications;
     this.uintraHub.client.reloadFeed = this.broadcastReloadFeed;
 
     $.connection.hub.disconnected(() => {
-      if ($.connection.hub.lastError) { console.log('UintraHub connection: disconnected.reason: ' + $.connection.hub.lastError.message); }
+      if ($.connection.hub.lastError) {
+        console.log(
+          "UintraHub connection: disconnected.reason: " +
+            $.connection.hub.lastError.message
+        );
+      }
     });
     $.connection.hub.reconnected(() => {
       this.hubConnectionStart("reconnected");
     });
 
     this.hubConnectionStart("started");
-  }  
+  }
 
-  private hubConnectionStart(logmessage: string) {    
+  private hubConnectionStart(logmessage: string) {
     if ($.connection.hub.state === $.signalR.connectionState.disconnected) {
       $.connection.hub
         .start()
         .done(r => {
-          console.log("UintraHub connection:" + logmessage);          
+          console.log("UintraHub connection:" + logmessage);
         })
         .catch(r => console.log(r));
-    }   
+    }
   }
 
   public getUpdateNotificationsSubjects(): Observable<any> {
@@ -47,16 +52,17 @@ export class SignalrService {
     return this.uintraHub.centralFeedSubject.asObservable();
   }
 
-  private broadcastUpdateNotifications(notifications = []) {    
+  private broadcastUpdateNotifications(notifications = []) {
+    // @ts-ignore: Unreachable code error: 'this' variable is uintraHub context
     this.notificationSubject.next(notifications);
   }
 
-  private broadcastReloadFeed() {    
+  private broadcastReloadFeed() {
+    // @ts-ignore: Unreachable code error: 'this' variable is uintraHub context
     this.centralFeedSubject.next();
   }
 
   public hubConnectionStop() {
-    debugger;
     $.connection.centralFeedHub.server.onDisconnected();
     console.log("UintraHub connection: closed");
   }
