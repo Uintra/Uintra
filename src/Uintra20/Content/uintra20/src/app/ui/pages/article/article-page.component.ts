487974136bec27756ae1abd4ef2729520d775365
Taras Gordienko <tgo@compent.net>
2020-02-14 16:47:18 +0200

    add confirm popup

--- src/Uintra20/Content/uintra20/src/app/ui/pages/article/article-page.component.ts
+++ src/Uintra20/Content/uintra20/src/app/ui/pages/article/article-page.component.ts
@@ -1,33 +1,53 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { DeactivationGuarded } from 'src/app/services/general/can-deactivate.service';
-import { HasDataChangedService } from 'src/app/services/general/has-data-changed.service';
+import { Component, ViewEncapsulation } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { DeactivationGuarded } from "src/app/services/general/can-deactivate.service";
+import { HasDataChangedService } from "src/app/services/general/has-data-changed.service";
+import { Observable, Subject, BehaviorSubject, Subscriber } from "rxjs";
 
 @Component({
-  selector: 'article-page',
-  templateUrl: './article-page.html',
-  styleUrls: ['./article-page.less'],
+  selector: "article-page",
+  templateUrl: "./article-page.html",
+  styleUrls: ["./article-page.less"],
   encapsulation: ViewEncapsulation.None
 })
 export class ArticlePage implements DeactivationGuarded {
   data: any;
+  isShow: boolean;
+
+  subscriber: Subscriber<boolean>;
+  isConfirm: boolean;
 
   constructor(
     private route: ActivatedRoute,
     private hasDataChangedService: HasDataChangedService
   ) {
-    this.route.data.subscribe(data => this.data = data);
+    this.route.data.subscribe(data => (this.data = data));
   }
 
-  canDeactivate(): boolean {
+  canDeactivate(): Observable<boolean> | boolean {
     if (this.hasDataChangedService.hasDataChanged) {
-      if(confirm('Are you sure, bla bla bla?')) {
+      this.isShow = true;
+
+      if (this.isConfirm) {
         return true;
-      } else {
-        return false;
       }
+      return new Observable<boolean>(subs => {
+        this.subscriber = subs;
+      });
     }
 
     return true;
+  }
+
+  onSubmit() {
+    this.subscriber.next(true);
+    this.subscriber.complete();
+    this.isConfirm = true;
+  }
+
+  onCancel() {
+    this.isConfirm = false;
+    this.subscriber.next(false);
+    this.isShow = false;
   }
 }
