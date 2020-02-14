daee3427691e4c56ee31d5f5f62f883666f6938f
Sergey Vinnichenko <svi@compent.net>
2020-02-14 11:40:13 +0200

    add group room link to header model

--- src/Uintra20/Features/Groups/Models/GroupHeaderViewModel.cs
+++ src/Uintra20/Features/Groups/Models/GroupHeaderViewModel.cs
@@ -1,8 +1,11 @@
-﻿namespace Uintra20.Features.Groups.Models
+﻿using Uintra20.Features.Links.Models;
+
+namespace Uintra20.Features.Groups.Models
 {
     public class GroupHeaderViewModel
     {
         public string Title { get; set; }
+        public UintraLinkModel RoomPageLink { get; set; }
         public GroupLinksModel GroupLinks { get; set; }
     }
 }
