577875854227a4a5a71346958d8b92d00cf1cc40
Taras Gordienko <tgo@compent.net>
2020-02-17 16:11:05 +0200

    add wrapper to table for mobile

--- src/Uintra20/Content/uintra20/src/app/feature/project/specific/groups/document-table/document-table.module.ts
+++ src/Uintra20/Content/uintra20/src/app/feature/project/specific/groups/document-table/document-table.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { DocumentTableComponent } from "./document-table.component";
+import { UserAvatarModule } from "../../../reusable/ui-elements/user-avatar/user-avatar.module";
 
 @NgModule({
   declarations: [DocumentTableComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, UserAvatarModule],
   exports: [DocumentTableComponent]
 })
 export class DocumentTableModule {}
