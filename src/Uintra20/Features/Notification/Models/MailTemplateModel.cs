10f147cd68584a889882b1a451b33d1ec00e9e01
Sergey Vinnichenko <svi@compent.net>
2020-02-20 14:10:51 +0200

    temp commit for jobs

---
+++ src/Uintra20/Features/Notification/Models/MailTemplateModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UBaseline.Shared.Node;
+using UBaseline.Shared.Property;
+
+namespace Uintra20.Features.Notification.Models
+{
+    public class MailTemplateModel : NodeModel
+    {
+        public PropertyModel<string> Subject { get; set; }
+        public PropertyModel<string> Body { get; set; }
+        public PropertyModel<string> ExtraTokens { get; set; }
+        public PropertyModel<string> EmailType { get; set; }
+        public PropertyModel<INodeModel> To { get; set; }
+    }
+}
