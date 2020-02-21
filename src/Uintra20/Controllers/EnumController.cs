8c2b541f5f6be0c21c3c540bfae3d24b6ca1eed5
Sergey Vinnichenko <svi@compent.net>
2020-02-21 14:22:58 +0200

    Chenge return type of enum controller

--- src/Uintra20/Controllers/EnumController.cs
+++ src/Uintra20/Controllers/EnumController.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http;
 using Uintra20.Features.Media;
+using Uintra20.Infrastructure.Extensions;
 
 namespace Uintra20.Controllers
 {
     public class EnumController : ApiController
     {
         [HttpGet]
-        public IEnumerable<string> MediaFolderType()
+        public IEnumerable<KeyValuePair<string, string>> MediaFolderType()
         {
-            return Enum.GetNames(typeof(MediaFolderTypeEnum));
+            return Enum.GetValues(typeof(MediaFolderTypeEnum)).OfType<Enum>().Select(x => new KeyValuePair<string, string>(x.ToString(), x.GetDisplayName()));
         }
     }
 }
