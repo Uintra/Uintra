7f47e4a0eff9005b2f191d243c4dfa420b95ab2b
Sergey Vinnichenko <svi@compent.net>
2020-02-25 11:35:33 +0200

    Permission fix

--- src/Uintra20/Features/CentralFeed/Services/CentralFeedService.cs
+++ src/Uintra20/Features/CentralFeed/Services/CentralFeedService.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using Uintra20.Core.Feed.Models;
 using Uintra20.Core.Feed.Services;
 using Uintra20.Features.Groups;
+using Uintra20.Features.Permissions;
+using Uintra20.Features.Permissions.Interfaces;
 using Uintra20.Infrastructure.Caching;
 using Uintra20.Infrastructure.Extensions;
 
@@ -12,17 +14,25 @@ namespace Uintra20.Features.CentralFeed.Services
     public class CentralFeedService : FeedService, ICentralFeedService
     {
         private readonly IEnumerable<IFeedItemService> _feedItemServices;
+        private readonly IPermissionsService _permissionsService;
 
         public CentralFeedService(
             IEnumerable<IFeedItemService> feedItemServices,
-            ICacheService cacheService)
+            ICacheService cacheService,
+            IPermissionsService permissionsService)
             : base(feedItemServices, cacheService)
         {
             _feedItemServices = feedItemServices;
+            _permissionsService = permissionsService;
         }
 
         public IEnumerable<IFeedItem> GetFeed(Enum type)
         {
+            if (!_permissionsService.Check((PermissionResourceTypeEnum) type.ToInt(), PermissionActionEnum.View))
+            {
+                return Enumerable.Empty<IFeedItem>();
+            }
+
             var service = _feedItemServices.SingleOrDefault(s => s.Type.ToInt() == type.ToInt());
 
             return service == null
@@ -32,7 +42,9 @@ namespace Uintra20.Features.CentralFeed.Services
 
         public IEnumerable<IFeedItem> GetFeed()
         {
-            var items = _feedItemServices.SelectMany(service => service.GetItems());
+            var items = _feedItemServices
+                .Where(service => _permissionsService.Check((PermissionResourceTypeEnum)service.Type.ToInt(), PermissionActionEnum.View))
+                .SelectMany(service => service.GetItems());
             //return items.Where(IsCentralFeedActivity);
             return items;
         }
