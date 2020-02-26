190276c502ec4a59aaea4ae8f0e843652aaf15a9
Taras Gordienko <goldsteals@gmail.com>
2020-02-26 15:27:32 +0200

    UINTRA2-413 CF. Filter doesn't clear settings.

--- src/Uintra20/Content/uintra20/src/app/feature/project/reusable/ui-elements/spoiler-section/spoiler-section.component.ts
+++ src/Uintra20/Content/uintra20/src/app/feature/project/reusable/ui-elements/spoiler-section/spoiler-section.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-spoiler-section',
   templateUrl: './spoiler-section.component.html',
-  styleUrls: ['./spoiler-section.component.less']
+  styleUrls: ['./spoiler-section.component.less'],
+  providers: [
+    { provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => SpoilerSectionComponent), multi: true}
+  ]
 })
-export class SpoilerSectionComponent {
+export class SpoilerSectionComponent  implements ControlValueAccessor {
   @Input() title: string;
   @Input() isSpoilerShowed: boolean = false;
 
@@ -13,5 +17,28 @@ export class SpoilerSectionComponent {
 
   onToggleSpoiler() {
     this.isSpoilerShowed = !this.isSpoilerShowed;
+    this.writeValue(this.isSpoilerShowed);
+  }
+
+  propagateChange: any = () => {
+  }
+
+  writeValue(value) {
+    this.isSpoilerShowed = value;
+    this.onChange(value);
+  }
+
+  onChange: any = () => {
+  }
+
+  onTouched: any = () => {
+  }
+
+  registerOnChange(fn) {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn) {
+    this.onTouched = fn;
   }
 }
--- src/Uintra20/Content/uintra20/src/app/ui/panels/central-feed/central-feed-filters/central-feed-filters.component.ts
+++ src/Uintra20/Content/uintra20/src/app/ui/panels/central-feed/central-feed-filters/central-feed-filters.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { UmbracoFlatPropertyModel } from '@ubaseline/next';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { UmbracoFlatPropertyModel } from "@ubaseline/next";
+import { CentralFeedFiltersService } from "./central-feed-filters.service";
 
 @Component({
-  selector: 'app-central-feed-filters',
-  templateUrl: './central-feed-filters.component.html',
-  styleUrls: ['./central-feed-filters.component.less']
+  selector: "app-central-feed-filters",
+  templateUrl: "./central-feed-filters.component.html",
+  styleUrls: ["./central-feed-filters.component.less"]
 })
 export class CentralFeedFiltersComponent implements OnInit {
   @Input() tabs: Array<UmbracoFlatPropertyModel>;
@@ -13,39 +14,125 @@ export class CentralFeedFiltersComponent implements OnInit {
   selectedTab: UmbracoFlatPropertyModel = null;
   selectTabFilters: Array<any>;
   selectedTabType: number;
+  isOpen: boolean = false;
+  filtersState: Array<any> = [];
 
-  constructor() { }
+  constructor(private centralFeedFiltersService: CentralFeedFiltersService) {}
 
   ngOnInit() {
     this.setInitValues();
-    this.selectFilters.emit({ selectedTabType: this.selectedTabType, selectTabFilters: this.selectTabFilters } );
+    this.emitFilterState();
+  }
+
+  handleOpen(value: boolean) {
+    this.centralFeedFiltersService.setOpeningState(this.isOpen);
   }
 
   setSelectedTab(event) {
     this.selectedTabType = event;
     this.selectedTab = this.tabs.find(tab => tab.get().type.get() === event);
     this.selectTabFilters = this.getTabFilters();
-    this.selectFilters.emit({ selectedTabType: this.selectedTabType, selectTabFilters: this.selectTabFilters } );
+    this.setSelectedFiltersFromCookie();
+    this.emitFilterState();
   }
 
   changeFilters() {
-    this.selectFilters.emit({ selectedTabType: this.selectedTabType, selectTabFilters: this.selectTabFilters } );
+    this.setFiltersState();
+    this.emitFilterState();
   }
 
   clearFilters() {
+    this.centralFeedFiltersService.setFilteringState("");
     this.setInitValues();
-    this.selectTabFilters = this.selectTabFilters
-      .map(filter => ({ ...filter, isActive: false }));
-    this.setSelectedTab(this.tabs.find(tab => tab.get().isActive.get() === true).get().type.get());
+    this.selectTabFilters = this.selectTabFilters.map(filter => ({
+      ...filter,
+      isActive: false
+    }));
+    this.setSelectedTab(
+      this.tabs.find(tab => tab.get().isActive.get() === true).get().type.get()
+    );
   }
 
-  setInitValues() {
-    this.selectedTab = this.tabs.find(tab => tab.get().isActive);
-    this.selectedTabType = this.selectedTab.get().type.get();
+  private setInitValues(): void {
+    this.isOpen = this.centralFeedFiltersService.getOpeningState();
+
+    this.setUniqueFiltersState();
+    this.setSelectedTabFromCookie();
+
     this.selectTabFilters = this.getTabFilters();
+    this.setSelectedFiltersFromCookie();
   }
 
-  getTabFilters() {
-    return Object.values(JSON.parse(JSON.stringify(this.selectedTab.get().filters.get())));
+  private setUniqueFiltersState() {
+    const filtersState = this.tabs.map(tab => {
+      return Object.values(JSON.parse(JSON.stringify(tab.get().filters.get())));
+    });
+    filtersState.forEach(filter => {
+      filter.forEach((item: any) => {
+        if (!this.filtersState.find(cur => cur.key === item.key)) {
+          this.filtersState.push(item);
+        }
+      });
+    });
+  }
+
+  private setSelectedTabFromCookie() {
+    const selectedTabType = this.centralFeedFiltersService.getTabState();
+
+    if (selectedTabType) {
+      this.selectedTabType = selectedTabType;
+      this.selectedTab = this.tabs.find(
+        tab => tab.get().type == selectedTabType
+      );
+    } else {
+      this.selectedTab = this.tabs.find(tab => tab.get().isActive);
+      this.selectedTabType = this.selectedTab.get().type.get();
+    }
+  }
+
+  private emitFilterState() {
+    this.centralFeedFiltersService.setTabState(this.selectedTabType);
+    this.centralFeedFiltersService.setFilteringState(this.filtersState);
+
+    this.selectFilters.emit({
+      selectedTabType: this.selectedTabType,
+      selectTabFilters: this.selectTabFilters
+    });
+  }
+
+  private setSelectedFiltersFromCookie() {
+    const selectTabFilters = this.centralFeedFiltersService.getFilteringState();
+
+    if (selectTabFilters && selectTabFilters.length) {
+      this.selectTabFilters = this.selectTabFilters.map(filter => {
+        const foundFilter = selectTabFilters.find(
+          cur => cur.key === filter.key
+        );
+
+        return foundFilter
+          ? { ...filter, isActive: foundFilter.isActive }
+          : filter;
+      });
+    }
+
+    this.setFiltersState();
+  }
+
+  private getTabFilters() {
+    return Object.values(
+      JSON.parse(JSON.stringify(this.selectedTab.get().filters.get()))
+    );
+  }
+
+  private setFiltersState() {
+    this.filtersState = this.filtersState.map(filter => {
+      const foundFilter = this.selectTabFilters.find(
+        cur => cur.key === filter.key
+      );
+      if (foundFilter) {
+        filter.isActive = foundFilter.isActive;
+      }
+      return filter;
+    });
   }
 }
