b2330eba6632648239a7db8b8240e8e8099926c2
Sergey Vinnichenko <svi@compent.net>
2020-03-02 13:21:19 +0200

    Temp commit

---
+++ src/Uintra20/Features/News/Models/NewsCreateDataViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Uintra20.Features.News.Models
+{
+    public class NewsCreateDataViewModel
+    {
+    }
+}
---
+++ src/Uintra20/Features/News/Models/UintraNewsCreatePageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using UBaseline.Shared.Node;
+using UBaseline.Shared.PageSettings;
+using UBaseline.Shared.Property;
+
+namespace Uintra20.Features.News.Models
+{
+    public class UintraNewsCreatePageViewModel : NodeViewModel
+    {
+        public PropertyViewModel<INodeViewModel[]> Panels { get; set; }
+        public PageSettingsCompositionViewModel PageSettings { get; set; }
+    }
+}
