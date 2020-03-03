7d08a0a57d6f56733afde26544ab6f050ed31a59
Sergey Vinnichenko <svi@compent.net>
2020-03-03 16:53:42 +0200

    temp commit move group header

--- src/Uintra20/Features/Social/Models/SocialEditPageViewModel.cs
+++ src/Uintra20/Features/Social/Models/SocialEditPageViewModel.cs
@@ -5,12 +5,14 @@ using UBaseline.Shared.Node;
 using UBaseline.Shared.PageSettings;
 using UBaseline.Shared.Property;
 using Uintra20.Core.Controls.LightboxGallery;
+using Uintra20.Features.Groups;
+using Uintra20.Features.Groups.Models;
 using Uintra20.Features.Links.Models;
 using Uintra20.Features.Tagging.UserTags.Models;
 
 namespace Uintra20.Features.Social.Models
 {
-    public class SocialEditPageViewModel : NodeViewModel
+    public class SocialEditPageViewModel : NodeViewModel, IGroupHeader
     {
         public Guid OwnerId { get; set; }
         public PropertyViewModel<INodeViewModel[]> Panels { get; set; }
@@ -21,10 +23,9 @@ namespace Uintra20.Features.Social.Models
         public IEnumerable<UserTag> AvailableTags { get; set; } = Enumerable.Empty<UserTag>();
         public Guid Id { get; set; }
         public string AllowedMediaExtensions { get; set; }
-        public Guid? GroupId { get; set; }
-        public bool RequiresGroupHeader { get; set; }
         public bool CanEdit { get; set; }
         public bool CanDelete { get; set; }
         public IActivityLinks Links { get; set; }
+        public GroupHeaderViewModel GroupHeader { get; set; }
     }
 }
