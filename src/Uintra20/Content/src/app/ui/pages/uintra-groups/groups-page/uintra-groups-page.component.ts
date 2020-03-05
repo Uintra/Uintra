a69f6343c2f1e7fa3f6204ad648c332e7459f251
eugene.sahach <esa@gmail.com>
2020-03-05 18:26:50 +0200

    group header

--- src/Uintra20/Content/src/app/ui/pages/uintra-groups/groups-page/uintra-groups-page.component.ts
+++ src/Uintra20/Content/src/app/ui/pages/uintra-groups/groups-page/uintra-groups-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AddButtonService } from '../../../main-layout/left-navigation/components/my-links/add-button.service';
+import ParseHelper from 'src/app/shared/utils/parse.helper';
 
 @Component({
   selector: 'uintra-groups-page',
@@ -16,7 +17,7 @@ export class UintraGroupsPage {
     private addButtonService: AddButtonService
   ) {
     this.route.data.subscribe(data => {
-      this.data = data;
+      this.data = ParseHelper.parseUbaselineData(data);
       this.addButtonService.setPageId(data.id);
     });
   }
