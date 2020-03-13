95f8c2a71aeabf10e7a89195b41fec819e6d9ee8
Sergey Vinnichenko <svi@compent.net>
2020-03-13 14:16:32 +0200

    Coming events

---
+++ src/Uintra20/Features/Events/Models/ComingEventViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Uintra20.Core.Member.Models;
+using Uintra20.Features.Links.Models;
+
+namespace Uintra20.Features.Events.Models
+{
+    public class ComingEventViewModel
+    {
+        public string Title { get; set; }
+        public DateTime StartDate { get; set; }
+        public IEnumerable<string> Dates { get; set; } = Enumerable.Empty<string>();
+        public MemberViewModel Owner { get; set; }
+        public IActivityLinks Links { get; set; }
+    }
+}
