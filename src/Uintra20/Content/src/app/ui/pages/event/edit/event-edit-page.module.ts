75c79c8ffb1777692e6484a0d7e3d23181bb16f2
eugene.sahach <esa@gmail.com>
2020-03-17 12:01:23 +0200

    event edit module

--- src/Uintra20/Content/src/app/ui/pages/event/edit/event-edit-page.module.ts
+++ src/Uintra20/Content/src/app/ui/pages/event/edit/event-edit-page.module.ts
@@ -4,13 +4,18 @@ import { RouterModule } from '@angular/router';
 
 import { UbaselineCoreModule } from '@ubaseline/next';
 import { EventEditPage } from './event-edit-page.component';
+import { GroupDetailsWrapperModule } from 'src/app/feature/specific/groups/group-details-wrapper/group-details-wrapper.module';
+import { EventCreatePage } from '../create/event-create-page.component';
+import { EventCreateModule } from 'src/app/feature/specific/activity/create/event-create/event-create.module';
 
 @NgModule({
   declarations: [EventEditPage],
   imports: [
     CommonModule,
-    RouterModule.forChild([{path: "", component: EventEditPage}]),
+    RouterModule.forChild([{path: "", component: EventCreatePage}]),
     UbaselineCoreModule,
+    GroupDetailsWrapperModule,
+    EventCreateModule,
   ],
   entryComponents: [EventEditPage]
 })
