a8b110ce9da67a48a5590245f0a359da37aea608
Maksim.Gridin <mgr@compent.net>
2020-03-19 16:30:48 +0200

    UINTRA2-482 - added alert dismiss behavior

--- src/Uintra20/App_Plugins/Utils/Search/search-controller.js
+++ src/Uintra20/App_Plugins/Utils/Search/search-controller.js
@@ -5,21 +5,28 @@
             function ($scope, searchService) {
 
                 $scope.inProgress = false;
+                $scope.showResult = false;
+                $scope.class = '';
 
                 $scope.rebuild = function () {
                     $scope.inProgress = true;
                     searchService.rebuildIndex()
                         .then(
                             function (resolve) {
-                                alert('Ok');
+                                $scope.class = 'alert-success';
                             },
                             function (reject) {
-                                alert('No Ok');
+                                $scope.class = 'alert-dander';
                             })
                         .finally(
                             function () {
+                                $scope.showResult = true;
                                 $scope.inProgress = false;
                             });
+                };
+
+                $scope.hideAlert = function () {
+                    $scope.showResult = false;
                 };
             }
         ]
