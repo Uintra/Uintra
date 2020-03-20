29640114d8f05ed6241acc01c5dcd613acf508d1
eugene.sahach <esa@gmail.com>
2020-03-20 23:38:35 +0200

    hot fix

--- src/Uintra20/Content/src/app/ui/pages/pages.ts
+++ src/Uintra20/Content/src/app/ui/pages/pages.ts
@@ -116,23 +116,34 @@ export const pages = [
     cache: false
   },
   {
-    id: 'searchPage',
+    id: 'eventCreatePage',
     path: '__dynamic__',
-    loadChildren: './ui/pages/search/search-page.module#SearchPageModule',
+    loadChildren: './ui/pages/event/create/event-create-page.module#EventCreatePageModule',
+    cache: false
+  },
+  {
+    id: 'eventEditPage',
+    path: '__dynamic__',
+    loadChildren: './ui/pages/event/edit/event-edit-page.module#EventEditPageModule',
+    cache: false
+  },
+  {
+    id: 'eventDetailsPage',
+    path: '__dynamic__',
+    loadChildren: './ui/pages/event/details/event-details-page.module#EventDetailsPageModule',
     cache: false
   },
   {
     id: 'pageNotFoundPage',
     path: '__dynamic__',
-    loadChildren: './ui/pages/page-not-found/page-not-found-page.module#PageNotFoundPageModule'
+    loadChildren: './ui/pages/page-not-found/page-not-found-page.module#PageNotFoundPageModule',
+    cache: false
   },
   {
     id: 'forbiddenPage',
     path: '__dynamic__',
-    loadChildren: './ui/pages/forbidden/forbidden-page.module#ForbiddenPageModule'
+    loadChildren: './ui/pages/forbidden/forbidden-page.module#ForbiddenPageModule',
+    cache: false
   },
 ];
-
-
-
 
