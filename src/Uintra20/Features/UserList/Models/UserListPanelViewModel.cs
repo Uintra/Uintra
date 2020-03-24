dcc2d6c016c74affde9d60a9159438f756cafcba
Sergey Vinnichenko <svi@compent.net>
2020-03-24 13:08:17 +0200

    Add groupId to userlist panel

--- src/Uintra20/Features/UserList/Models/UserListPanelViewModel.cs
+++ src/Uintra20/Features/UserList/Models/UserListPanelViewModel.cs
@@ -1,9 +1,11 @@
-﻿using UBaseline.Shared.Node;
+﻿using System;
+using UBaseline.Shared.Node;
 
 namespace Uintra20.Features.UserList.Models
 {
     public class UserListPanelViewModel : NodeViewModel
     {
         public MembersRowsViewModel Details { get; set; }
+        public Guid? GroupId { get; set; }
     }
 }
