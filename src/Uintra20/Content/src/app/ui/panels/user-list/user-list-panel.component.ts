1348f561b8f1cd0c2acdb04b4f7eccba7b95f7f6
eugene.sahach <esa@gmail.com>
2020-03-24 15:51:45 +0200

    more of user list panel

--- src/Uintra20/Content/src/app/ui/panels/user-list/user-list-panel.component.ts
+++ src/Uintra20/Content/src/app/ui/panels/user-list/user-list-panel.component.ts
@@ -1,8 +1,9 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { IUserListPanel } from './user-list-panel.interface';
 import { Subject } from 'rxjs';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, finalize } from 'rxjs/operators';
 import { SearchService } from 'src/app/feature/specific/search/search.service';
+import ParseHelper from 'src/app/shared/utils/parse.helper';
 
 @Component({
   selector: 'user-list-panel',
@@ -12,8 +13,15 @@ import { SearchService } from 'src/app/feature/specific/search/search.service';
 })
 export class UserListPanel {
   data: IUserListPanel;
+  parsedData: any;
   inputValue: string = "";
   currentPage: number = 1;
+  members: any[] = [];
+  selectedColumns: any[] = [];
+  canLoadMore: boolean;
+  isName: boolean;
+  isInfo: boolean;
+  isLoadMoreDisabled: boolean;
 
   _query = new Subject<string>();
 
@@ -24,14 +32,49 @@ export class UserListPanel {
       debounceTime(200),
       distinctUntilChanged(),
     ).subscribe((value: string) => {
-      this.searchService.userListSearch(this.requestDataBuilder()).subscribe((res: any[]) => {
-        // this.autocompleteList = res.map(suggestion => ({
-        //   ...suggestion,
-        //   isActive: false
-        // }));
-        // this.hasResults = res.length > 0;
-      })
+      this.currentPage = 1;
+      this.members = [];
+      this.canLoadMore = false;
+      this.getMembers();
     })
+  }
+
+  ngOnInit() {
+    this.parsedData = ParseHelper.parseUbaselineData(this.data);
+    this.members = Object.values(this.parsedData.details.members);
+    this.selectedColumns = Object.values(this.parsedData.details.selectedColumns);
+    this.getColumns();
+    this.canLoadMore = !this.parsedData.details.isLastRequest;
+  }
+
+  onQueryChange(val) {
+    this.inputValue = val;
+    this._query.next(val);
+  }
+
+  getMembers() {
+    this.isLoadMoreDisabled = true;
+
+    this.searchService.userListSearch(this.requestDataBuilder()).pipe(
+      finalize(() => {
+        this.isLoadMoreDisabled = false;
+      })
+    ).subscribe((res: any) => {
+      this.members = this.members.concat(res.members);
+      this.selectedColumns = res.selectedColumns;
+      this.getColumns();
+      this.canLoadMore = !res.isLastRequest;
+    })
+  }
+
+  loadMore() {
+    this.currentPage += 1;
+    this.getMembers();
+  }
+
+  getColumns() {
+    this.isName = this.selectedColumns.indexOf(column => column.name == 'Name') !== -1;
+    this.isInfo = this.selectedColumns.indexOf(column => column.name == 'Info') !== -1;
   }
 
   requestDataBuilder() {
--- src/Uintra20/Content/src/app/ui/panels/user-list/user-list-panel.module.ts
+++ src/Uintra20/Content/src/app/ui/panels/user-list/user-list-panel.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { AS_DYNAMIC_COMPONENT } from '@ubaseline/next';
 import { UserListPanel } from './user-list-panel.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { UserAvatarModule } from 'src/app/feature/reusable/ui-elements/user-avatar/user-avatar.module';
 
 @NgModule({
   declarations: [UserListPanel],
   imports: [
     CommonModule,
     TranslateModule,
+    UserAvatarModule,
   ],
   providers: [{provide: AS_DYNAMIC_COMPONENT, useValue: UserListPanel}],
   entryComponents: [UserListPanel]
