11389151f08e9c463bbaaf675c99d4e3d31bd9d5
eugene.sahach <esa@gmail.com>
2020-03-26 14:47:47 +0200

    photoswipe fix for videos

--- src/Uintra20/Content/src/app/feature/reusable/ui-elements/image-gallery/image-gallery.service.ts
+++ src/Uintra20/Content/src/app/feature/reusable/ui-elements/image-gallery/image-gallery.service.ts
@@ -31,11 +31,18 @@ export class ImageGalleryService {
     let options = {
         // optionName: 'option value'
         // for example:
-        index: index // start at first slide
+        index: index, // start at first slide
+        history: false
     };
 
     // Initializes and opens PhotoSwipe
     let gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
     gallery.init();
+    gallery.listen('close', () => {
+      const test = document.querySelectorAll('.pswp__video');
+      document.querySelectorAll('.pswp__video').forEach(element => {
+        element.pause();
+      });
+    })
   }
 }
