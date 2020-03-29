565f63c6d9227e6124da7bf0b45eaf334b569c78
Sergey Vinnichenko <svi@compent.net>
2020-03-30 01:54:27 +0300

    Fix length location

--- src/Uintra20/Features/Location/Models/ActivityLocationEditModel.cs
+++ src/Uintra20/Features/Location/Models/ActivityLocationEditModel.cs
@@ -1,8 +1,11 @@
-﻿namespace Uintra20.Features.Location.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Uintra20.Features.Location.Models
 {
     public class ActivityLocationEditModel
     {
         public string Address { get; set; }
+        [MaxLength(200)]
         public string ShortAddress { get; set; }
     }
 }
