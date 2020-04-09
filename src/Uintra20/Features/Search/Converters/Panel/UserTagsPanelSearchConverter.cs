8bc02aa95d4fc6299284cfef5d9983ae239a6d76
Denis Prodan <darkden88@gmail.com>
2020-04-09 13:05:51 +0300

    add search converter for user tag panel

---
+++ src/Uintra20/Features/Search/Converters/Panel/UserTagsPanelSearchConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using Compent.Shared.Extensions.Bcl;
+using Uintra20.Core.Search.Converters.SearchDocumentPanelConverter;
+using Uintra20.Core.Search.Entities;
+using Uintra20.Features.Tagging.UserTags.Models;
+
+namespace Uintra20.Features.Search.Converters.Panel
+{
+    public class UserTagsPanelSearchConverter:SearchDocumentPanelConverter<UserTagsPanelViewModel>
+    {
+        protected override SearchablePanel OnConvert(UserTagsPanelViewModel panel)
+        {
+            return new SearchablePanel()
+            {
+                Content = panel.Tags.Select(t => t.Text).JoinWith()
+            };
+        }
+    }
+}
