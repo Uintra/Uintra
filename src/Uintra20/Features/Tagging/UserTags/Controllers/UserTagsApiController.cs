5564a98e998a04bf7a7fa6d056c9ac095ffa134a
Maxim Gridin <gridinmaxx@gmail.com>
2020-04-09 12:07:39 +0300

    UINTRA2-562 - Create user tags panel for article page - Added endpoint

--- src/Uintra20/Features/Tagging/UserTags/Controllers/UserTagsApiController.cs
+++ src/Uintra20/Features/Tagging/UserTags/Controllers/UserTagsApiController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using UBaseline.Core.Controllers;
+using UBaseline.Core.Node;
 using Uintra20.Features.Tagging.UserTags.Models;
 using Uintra20.Features.Tagging.UserTags.Services;
 
@@ -11,13 +13,16 @@ namespace Uintra20.Features.Tagging.UserTags.Controllers
     {
         private readonly IUserTagService _tagsService;
         private readonly IUserTagProvider _tagProvider;
+        private readonly INodeModelService _modelService;
 
         public UserTagsApiController(
             IUserTagService tagsService,
-            IUserTagProvider tagProvider)
+            IUserTagProvider tagProvider,
+            INodeModelService modelService)
         {
             _tagsService = tagsService;
             _tagProvider = tagProvider;
+            _modelService = modelService;
         }
 
         [HttpGet]
@@ -32,6 +37,27 @@ namespace Uintra20.Features.Tagging.UserTags.Controllers
             };
 
             return pickerViewModel;
+        }
+
+        [HttpGet]
+        public IEnumerable<UserTagBackofficeViewModel> GetAll(int pageId)
+        {
+            var nodeModel = _modelService.Get(pageId);
+
+            var allTags = _tagProvider.GetAll();
+
+            var selectedTagsDictionary = _tagsService
+                .Get(nodeModel.Key)
+                .ToDictionary(tag => tag.Id);
+
+            var result = allTags.Select(tag => new UserTagBackofficeViewModel
+            {
+                Id = tag.Id,
+                Text = tag.Text,
+                Selected = selectedTagsDictionary.ContainsKey(tag.Id)
+            });
+
+            return result;
         }
     }
 }
