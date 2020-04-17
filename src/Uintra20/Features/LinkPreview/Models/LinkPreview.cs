db368c0b6ce88a31d1db66f2b918efb837814ca8
Maxim Gridin <gridinmaxx@gmail.com>
2020-04-17 16:19:31 +0300

    UINTRA2-510 -  Implement/adapt fwiront-end logic for paste/render/delete links with preview - - Removed url, added uri parsing (without port)

--- src/Uintra20/Features/LinkPreview/Models/LinkPreview.cs
+++ src/Uintra20/Features/LinkPreview/Models/LinkPreview.cs
@@ -5,16 +5,10 @@ namespace Uintra20.Features.LinkPreview.Models
     public class LinkPreview
     {
         public int Id { get; set; }
-
-        public Uri Uri { get; set; }
-        public string Url { get; set; }
-
+        public string Uri { get; set; }
         public string Title { get; set; }
-
         public string Description { get; set; }
-
         public Uri ImageUri { get; set; }
-
         public Uri FaviconUri { get; set; }
     }
 }
