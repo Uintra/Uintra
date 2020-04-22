1cad4ec21564b21ccd553c004fe38215f1143a84
Maxim Gridin <gridinmaxx@gmail.com>
2020-04-22 16:05:45 +0300

    UINTRA2-646 - Get rid of the Umbraco flat property parser - extracted event create interface, changed config in tslint(due to warnings in whole project)

---
+++ src/Uintra20/Content/src/app/shared/interfaces/pages/event/create/event-create-page.ts
@@ -0,0 +1,15 @@
+export interface EventCreateInterface {
+    addToSitemap?: boolean;
+    contentTypeAlias?: string;
+    data?: any;
+    errorLink?: any;
+    groupHeader?: any;
+    id?: number;
+    name?: string;
+    nodeType?: number;
+    pageSettings?: any;
+    panels?: any;
+    requiresRedirect?: boolean;
+    statusCode?: number;
+    url?: string;
+}
--- src/Uintra20/Content/src/app/ui/pages/pages.ts
+++ src/Uintra20/Content/src/app/ui/pages/pages.ts
@@ -1,122 +1,122 @@
 export const pages = [
   {
-    id: "homePage",
-    path: "__dynamic__",
-    loadChildren: "./ui/pages/home/home-page.module#HomePageModule",
+    id: 'homePage',
+    path: '__dynamic__',
+    loadChildren: './ui/pages/home/home-page.module#HomePageModule',
     cache: false,
     legacy: true
   },
   {
-    id: "articlePage",
-    path: "__dynamic__",
-    loadChildren: "./ui/pages/article/article-page.module#ArticlePageModule",
+    id: 'articlePage',
+    path: '__dynamic__',
+    loadChildren: './ui/pages/article/article-page.module#ArticlePageModule',
     cache: false,
     legacy: true
   },
   {
-    id: "notificationsPage",
-    path: "__dynamic__",
+    id: 'notificationsPage',
+    path: '__dynamic__',
     loadChildren:
-      "./ui/pages/notifications/notifications-page.module#NotificationsPageModule",
+      './ui/pages/notifications/notifications-page.module#NotificationsPageModule',
     cache: false,
     legacy: true
   },
   {
-    id: "socialDetailsPage",
-    path: "__dynamic__",
+    id: 'socialDetailsPage',
+    path: '__dynamic__',
     loadChildren:
-      "./ui/pages/social/details/social-details-page.module#SocialDetailsPageModule",
+      './ui/pages/social/details/social-details-page.module#SocialDetailsPageModule',
     cache: false,
     legacy: true
   },
   {
-    id: "socialEditPage",
-    path: "__dynamic__",
+    id: 'socialEditPage',
+    path: '__dynamic__',
     loadChildren:
-      "./ui/pages/social/edit/social-edit-page.module#SocialEditPageModule",
+      './ui/pages/social/edit/social-edit-page.module#SocialEditPageModule',
     cache: false,
     legacy: true
   },
   {
-    id: "profilePage",
-    path: "__dynamic__",
+    id: 'profilePage',
+    path: '__dynamic__',
     loadChildren:
-      "./ui/pages/profile/profile-details/profile-page.module#ProfilePageModule",
+      './ui/pages/profile/profile-details/profile-page.module#ProfilePageModule',
     cache: false,
     legacy: true
   },
   {
-    id: "profileEditPage",
-    path: "__dynamic__",
+    id: 'profileEditPage',
+    path: '__dynamic__',
     loadChildren:
-      "./ui/pages/profile/profile-edit/profile-edit-page.module#ProfileEditPageModule",
+      './ui/pages/profile/profile-edit/profile-edit-page.module#ProfileEditPageModule',
     cache: false,
     legacy: true
   },
   {
-    id: "uintraNewsEditPage",
-    path: "__dynamic__",
+    id: 'uintraNewsEditPage',
+    path: '__dynamic__',
     loadChildren:
-      "./ui/pages/news/uintra-news-edit/uintra-news-edit-page.module#UintraNewsEditPageModule",
+      './ui/pages/news/uintra-news-edit/uintra-news-edit-page.module#UintraNewsEditPageModule',
     cache: false,
     legacy: true
   },
   {
-    id: "uintraNewsDetailsPage",
-    path: "__dynamic__",
+    id: 'uintraNewsDetailsPage',
+    path: '__dynamic__',
     loadChildren:
-      "./ui/pages/news/uintra-news-details/uintra-news-details-page.module#UintraNewsDetailsPageModule",
+      './ui/pages/news/uintra-news-details/uintra-news-details-page.module#UintraNewsDetailsPageModule',
     cache: false,
     legacy: true
   },
   {
-    id: "uintraGroupsCreatePage",
-    path: "__dynamic__",
+    id: 'uintraGroupsCreatePage',
+    path: '__dynamic__',
     loadChildren:
-      "./ui/pages/uintra-groups/create/uintra-groups-create-page.module#UintraGroupsCreatePageModule",
+      './ui/pages/uintra-groups/create/uintra-groups-create-page.module#UintraGroupsCreatePageModule',
     cache: false,
   },
   {
-    id: "uintraGroupsEditPage",
-    path: "__dynamic__",
+    id: 'uintraGroupsEditPage',
+    path: '__dynamic__',
     loadChildren:
-      "./ui/pages/uintra-groups/edit/uintra-groups-edit-page.module#UintraGroupsEditPageModule",
+      './ui/pages/uintra-groups/edit/uintra-groups-edit-page.module#UintraGroupsEditPageModule',
     cache: false,
   },
   {
-    id: "uintraGroupsMembersPage",
-    path: "__dynamic__",
+    id: 'uintraGroupsMembersPage',
+    path: '__dynamic__',
     loadChildren:
-      "./ui/pages/uintra-groups/members/uintra-groups-members-page.module#UintraGroupsMembersPageModule",
+      './ui/pages/uintra-groups/members/uintra-groups-members-page.module#UintraGroupsMembersPageModule',
     cache: false,
   },
   {
-    id: "uintraGroupsRoomPage",
-    path: "__dynamic__",
+    id: 'uintraGroupsRoomPage',
+    path: '__dynamic__',
     loadChildren:
-      "./ui/pages/uintra-groups/room/uintra-groups-room-page.module#UintraGroupsRoomPageModule",
+      './ui/pages/uintra-groups/room/uintra-groups-room-page.module#UintraGroupsRoomPageModule',
     cache: false,
     legacy: true
   },
   {
-    id: "uintraGroupsDocumentsPage",
-    path: "__dynamic__",
+    id: 'uintraGroupsDocumentsPage',
+    path: '__dynamic__',
     loadChildren:
-      "./ui/pages/uintra-groups/documents/uintra-groups-documents-page.module#UintraGroupsDocumentsPageModule",
+      './ui/pages/uintra-groups/documents/uintra-groups-documents-page.module#UintraGroupsDocumentsPageModule',
     cache: false,
   },
   {
-    id: "uintraGroupsPage",
-    path: "__dynamic__",
+    id: 'uintraGroupsPage',
+    path: '__dynamic__',
     loadChildren:
-      "./ui/pages/uintra-groups/groups-page/uintra-groups-page.module#UintraGroupsPageModule",
+      './ui/pages/uintra-groups/groups-page/uintra-groups-page.module#UintraGroupsPageModule',
     cache: false
   },
   {
-    id: "uintraMyGroupsPage",
-    path: "__dynamic__",
+    id: 'uintraMyGroupsPage',
+    path: '__dynamic__',
     loadChildren:
-      "./ui/pages/uintra-groups/my-groups-page/uintra-my-groups-page.module#UintraMyGroupsPageModule",
+      './ui/pages/uintra-groups/my-groups-page/uintra-my-groups-page.module#UintraMyGroupsPageModule',
     cache: false
   },
   {
