5cdf4a52e6f8722f4dda873ac05ec0e04119cb86
Maxim Gridin <gridinmaxx@gmail.com>
2020-05-14 16:05:02 +0300

    UINTRA2-691 - Social with image> 20 mb. Should be added as an attachment - Added checking the size  of file, extracted image size threshold, added extensions for converting to megabytes

---
+++ src/Uintra20/Features/Media/Images/Constants/ImageConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace Uintra20.Features.Media.Images.Constants
+{
+    public class ImageConstants
+    {
+        public static float ImageSizeThreshold = 20.0F;
+    }
+}
---
+++ src/Uintra20/Infrastructure/Extensions/ByteExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Uintra20.Infrastructure.Extensions
+{
+    public static class ByteExtensions
+    {
+        public static float ToMegabytes(this byte[] source) => 
+            source.Length / 1024.0F / 1024.0F;
+    }
+}
