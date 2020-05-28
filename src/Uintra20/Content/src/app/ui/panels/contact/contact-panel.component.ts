648a1c81b1c9caa32e4420a0f6495653c1c1179e
Andrey Datsenko <and@compent.net>
2020-05-28 15:13:29 +0300

    UINTRA2-610 Contact panel

--- src/Uintra20/Content/src/app/ui/panels/contact/contact-panel.component.ts
+++ src/Uintra20/Content/src/app/ui/panels/contact/contact-panel.component.ts
@@ -1,18 +1,19 @@
-import { Component, ViewEncapsulation, HostBinding } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 import { IContactPanel } from '../../../shared/interfaces/panels/contact/contact-panel.interface';
-import { resolveThemeCssClass } from 'src/app/feature/reusable/ui-elements/ubl-ui-kit/core/helpers/panel-settings';
 
 @Component({
   selector: 'contact-panel',
   templateUrl: './contact-panel.html',
-  styleUrls: ['./contact-panel.less'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./contact-panel.less']
 })
 export class ContactPanel {
   data: IContactPanel;
-  @HostBinding('class') hostClasses;
+
+  @HostBinding('class') rootClasses;
 
   ngOnInit() {
-    this.hostClasses = resolveThemeCssClass(this.data.panelSettings);
+    this.rootClasses = `
+      ${ this.data.panelSettings.theme.value.alias || 'default-theme' }
+    `;
   }
 }
