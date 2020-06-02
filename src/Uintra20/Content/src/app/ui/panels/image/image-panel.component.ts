72a37d6e4b41993f287b653283fb60b5dc3eb515
AndreyDatsenko <and@compent.net>
2020-06-02 21:19:21 +0300

    UINTRA2-31 / UINTRA2-601 Responcive for article & group page

--- src/Uintra20/Content/src/app/ui/panels/image/image-panel.component.ts
+++ src/Uintra20/Content/src/app/ui/panels/image/image-panel.component.ts
@@ -1,18 +1,19 @@
-import { Component, ViewEncapsulation, HostBinding } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 import { IImagePanel } from '../../../shared/interfaces/panels/image/image-panel.interface';
 import { resolveThemeCssClass } from 'src/app/feature/reusable/ui-elements/ubl-ui-kit/core/helpers/panel-settings';
 
 @Component({
   selector: 'image-panel',
   templateUrl: './image-panel.html',
-  styleUrls: ['./image-panel.less']//,
-  //encapsulation: ViewEncapsulation.None
+  styleUrls: ['./image-panel.less']
 })
 export class ImagePanel {
   data: IImagePanel;
-  // @HostBinding('class') hostClasses;
+  // @HostBinding('class') rootClasses;
 
-  ngOnInit() {
-    // this.hostClasses = resolveThemeCssClass(this.data.panelSettings);
-  }
+  // ngOnInit() {
+  //   this.rootClasses = `
+  //     ${ this.data.panelSettings.theme.value.alias || 'default-theme' }
+  //   `;
+  // }
 }
