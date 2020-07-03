c389ed91c1ecfe0e0c52ccac9336a17e25edff12
Taras Gordienko <goldsteals@gmail.com>
2020-07-03 16:03:09 +0300

    UINTRA2-843 User profile: Updating image is not reflected in avatar right away

--- src/Uintra20/Content/src/app/feature/specific/user-navigation/user-navigation.component.ts
+++ src/Uintra20/Content/src/app/feature/specific/user-navigation/user-navigation.component.ts
@@ -1,48 +1,67 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import { finalize } from 'rxjs/operators';
-import { IUserNavigation } from '../../reusable/ui-elements/comments/components/user-navigation/user-navigation.interface';
-import { UserNavigationService } from './services/user-navigation.service';
-import { Subscription } from 'rxjs';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Router } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
+import { finalize } from "rxjs/operators";
+import { IUserNavigation } from "../../reusable/ui-elements/comments/components/user-navigation/user-navigation.interface";
+import { UserNavigationService } from "./services/user-navigation.service";
+import { Subscription } from "rxjs";
+import { DownloadedPhotoWatcherService } from "./services/downloaded-photo-watcher.service";
 
 export enum IconType {
-  'icon-umbraco-logo' = 1,
-  'icon-user-profile',
-  'icon-uintra',
-  'icon-logout',
+  "icon-umbraco-logo" = 1,
+  "icon-user-profile",
+  "icon-uintra",
+  "icon-logout",
 }
 
 @Component({
-  selector: 'user-navigation',
-  templateUrl: './user-navigation.component.html',
-  styleUrls: ['./user-navigation.component.less']
+  selector: "user-navigation",
+  templateUrl: "./user-navigation.component.html",
+  styleUrls: ["./user-navigation.component.less"],
 })
 export class UserNavigationComponent implements OnInit, OnDestroy {
-
   private $navigation: Subscription;
   private $redirect: Subscription;
 
   public inProgress: boolean;
   public data: IUserNavigation;
   public navigationExpanded: boolean;
+  public downloadedPhoto: string;
+  public isPhotoRemoved: boolean;
 
   public get isNavigationExpanded() {
     return this.navigationExpanded;
   }
 
-  constructor(private userNavigationService: UserNavigationService) { }
+  constructor(
+    private userNavigationService: UserNavigationService,
+    private downloadedPhotoWatcherService: DownloadedPhotoWatcherService
+  ) {}
 
   public ngOnInit(): void {
-    this.$navigation = this.userNavigationService.topNavigation()
+    this.$navigation = this.userNavigationService
+      .topNavigation()
       .subscribe((next: IUserNavigation) => {
         this.data = next;
       });
+    this.downloadedPhotoWatcherService.getTrigger().subscribe((r: string) => {
+      if (r) {
+        this.downloadedPhoto = r;
+        this.isPhotoRemoved = false;
+      } else {
+        this.downloadedPhoto = '';
+        this.isPhotoRemoved = true;
+      }
+    });
   }
 
   public ngOnDestroy(): void {
-    if (this.$navigation) { this.$navigation.unsubscribe(); }
-    if (this.$redirect) { this.$redirect.unsubscribe(); }
+    if (this.$navigation) {
+      this.$navigation.unsubscribe();
+    }
+    if (this.$redirect) {
+      this.$redirect.unsubscribe();
+    }
   }
 
   public toggleUserNavigation(event): void {
@@ -63,28 +82,36 @@ export class UserNavigationComponent implements OnInit, OnDestroy {
     this.inProgress = true;
 
     if (type === 1) {
-      this.$redirect = this.userNavigationService.redirect(url.originalUrl).pipe(
-        finalize(() => {
-          this.inProgress = false;
-        })
-      ).subscribe(
-        (next) => {
-          window.open(window.location.origin + '/umbraco', '_blank');
-        },
-        (error) => {
-          if (error.status === 403) {
-            console.error(error.message);
+      this.$redirect = this.userNavigationService
+        .redirect(url.originalUrl)
+        .pipe(
+          finalize(() => {
+            this.inProgress = false;
+          })
+        )
+        .subscribe(
+          (next) => {
+            window.open(window.location.origin + "/umbraco", "_blank");
+          },
+          (error) => {
+            if (error.status === 403) {
+              console.error(error.message);
+            }
           }
-        },
-      );
+        );
     }
 
     if (type === 4) {
-      this.$redirect = this.userNavigationService.redirect(url.originalUrl).pipe(
-        finalize(() => {
-          this.inProgress = false;
-        })
-      ).subscribe(() => { window.location.href = '/login'; });
+      this.$redirect = this.userNavigationService
+        .redirect(url.originalUrl)
+        .pipe(
+          finalize(() => {
+            this.inProgress = false;
+          })
+        )
+        .subscribe(() => {
+          window.location.href = "/login";
+        });
     }
   }
 }
