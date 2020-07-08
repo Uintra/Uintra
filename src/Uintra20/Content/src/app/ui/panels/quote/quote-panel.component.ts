10be99a4bf1a42a377d6591f9a099046937c5250
Taras Gordienko <goldsteals@gmail.com>
2020-07-08 12:47:16 +0300

    UINTRA2-856 Quote panel. Remove html tags from UI

--- src/Uintra20/Content/src/app/ui/panels/quote/quote-panel.component.ts
+++ src/Uintra20/Content/src/app/ui/panels/quote/quote-panel.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
 import { IQuotePanel } from '../../../shared/interfaces/panels/quote/quote-panel.interface';
+import { SafeHtml, DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'quote-panel',
@@ -8,10 +9,15 @@ import { IQuotePanel } from '../../../shared/interfaces/panels/quote/quote-panel
 })
 export class QuotePanel implements OnInit{
   data: IQuotePanel;
+  sanitizedContent: SafeHtml;
 
   @HostBinding('class') rootClasses;
 
+  constructor(
+    private sanitizer: DomSanitizer) { }
+
   ngOnInit() {
+    this.sanitizedContent = this.sanitizer.bypassSecurityTrustHtml(this.data.quote.toString());
     this.rootClasses = `
       ${ this.data.panelSettings.theme.value.alias || 'default-theme' }
     `;
